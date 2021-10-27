f3e08bacaddec5675b786961d7bcde13687b522c
Yves Hauser <yves.hauser@gmail.com>
2021-10-27 18:21:33 +0200

    SCP-2881: JSON serialization for ACTUS test cases

--- marlowe-actus/test/Spec/Marlowe/ACTUS/TestFramework.hs
+++ marlowe-actus/test/Spec/Marlowe/ACTUS/TestFramework.hs
@@ -12,17 +12,20 @@
 {-# LANGUAGE TypeSynonymInstances  #-}
 
 module Spec.Marlowe.ACTUS.TestFramework
+  ( tests
+  , testCasesFromFile
+  )
   where
 
 import           Control.Applicative                               ((<|>))
 import           Control.Monad                                     (mzero)
 import           Data.Aeson
-import           Data.ByteString.Lazy.UTF8                         as BLU (fromString)
+import           Data.ByteString.Lazy                              as B (readFile)
 import           Data.Char                                         (toUpper)
 import           Data.List                                         as L (find)
-import           Data.Map                                          as Map (Map, lookup, toList)
+import           Data.Map                                          as Map (Map, elems, lookup)
 import           Data.Maybe                                        (fromJust, fromMaybe)
-import           Data.Time                                         (LocalTime (..), defaultTimeLocale, parseTimeM)
+import           Data.Time                                         (LocalTime (..))
 import           GHC.Generics                                      (Generic)
 import           GHC.Records                                       (getField)
 import           Language.Marlowe.ACTUS.Analysis
@@ -32,91 +35,69 @@ import           Language.Marlowe.ACTUS.Definitions.Schedule
 import           Language.Marlowe.ACTUS.Model.Utility.DateShift    (getFollowingBusinessDay)
 import           Test.Tasty
 import           Test.Tasty.HUnit                                  (Assertion, assertBool, assertFailure, testCase)
+import           Text.Printf                                       (printf)
 
 tests :: String -> [TestCase] -> TestTree
-tests n t = testGroup n $ [ testCase (getField @"identifier" tc) (runTest tc) | tc <- t]
+tests n t = testGroup n [testCase (getField @"identifier" tc) (runTest tc) | tc <- t]
+  where
+    runTest :: TestCase -> Assertion
+    runTest TestCase {..} =
+        let ct = setDefaultContractTermValues terms
 
-runTest :: TestCase -> Assertion
-runTest TestCase {..} =
-    let ct = setDefaultContractTermValues terms
+            getRiskFactors ev date =
+              let rf =
+                    RiskFactorsPoly
+                      { o_rf_CURS = 1.0,
+                        o_rf_RRMO = 1.0,
+                        o_rf_SCMO = 1.0,
+                        pp_payoff = 0.0
+                      }
 
-        getRiskFactors ev date =
-          let riskFactors =
-                RiskFactorsPoly
-                  { o_rf_CURS = 1.0,
-                    o_rf_RRMO = 1.0,
-                    o_rf_SCMO = 1.0,
-                    pp_payoff = 0.0
-                  }
+                  observedKey RR = marketObjectCodeOfRateReset ct
+                  observedKey SC = marketObjectCodeOfScalingIndex ct
+                  observedKey DV = Just (fmap toUpper identifier ++ "_DV")
+                  observedKey XD = Just . marketObjectCode . Prelude.head $ contractStructure ct
+                  observedKey _  = settlementCurrency ct
 
-              observedKey RR = marketObjectCodeOfRateReset ct
-              observedKey SC = marketObjectCodeOfScalingIndex ct
-              observedKey DV = Just (fmap toUpper identifier ++ "_DV")
-              observedKey XD = Just . marketObjectCode . Prelude.head $ contractStructure ct
-              observedKey _  = settlementCurrency ct
+                  v = fromMaybe 1.0 $ do
+                    k <- observedKey ev
+                    DataObserved {values} <- Map.lookup k dataObserved
+                    ValueObserved {value} <-
+                      L.find
+                        ( \ValueObserved {timestamp} ->
+                            getFollowingBusinessDay timestamp (fromJust . calendar $ scheduleConfig ct) == date
+                        )
+                        values
+                    return value
+               in case ev of
+                    RR -> rf {o_rf_RRMO = v}
+                    SC -> rf {o_rf_SCMO = v}
+                    DV -> rf {pp_payoff = v}
+                    XD -> rf {pp_payoff = v}
+                    _  -> rf {o_rf_CURS = v}
 
-              v = fromMaybe 1.0 $ do
-                k <- observedKey ev
-                DataObserved {values} <- Map.lookup k dataObserved
-                ValueObserved {value} <-
-                  L.find
-                    ( \ValueObserved {timestamp} ->
-                        getFollowingBusinessDay timestamp (fromJust . calendar $ scheduleConfig ct) == date
-                    )
-                    values
-                return value
-           in case ev of
-                RR -> riskFactors {o_rf_RRMO = v}
-                SC -> riskFactors {o_rf_SCMO = v}
-                DV -> riskFactors {pp_payoff = v}
-                XD -> riskFactors {pp_payoff = v}
-                _  -> riskFactors {o_rf_CURS = v}
+            cashFlows = genProjectedCashflows getRiskFactors ct
+            cashFlowsTo = maybe cashFlows (\d -> filter ((<= d) . cashCalculationDay) cashFlows) to
+         in assertTestResults cashFlowsTo results
 
-        cashFlows = genProjectedCashflows getRiskFactors ct
-        cashFlowsTo = maybe cashFlows (\d -> filter ((<= d) . cashCalculationDay) cashFlows) (parseDate to)
-     in assertTestResults cashFlowsTo results identifier
+    assertTestResults :: [CashFlow] -> [TestResult] -> IO ()
+    assertTestResults [] []           = return ()
+    assertTestResults (cf:cfs) (r:rs) = assertTestResult cf r >> assertTestResults cfs rs
+    assertTestResults _ _             = assertFailure "Sizes differ"
+
+    assertTestResult :: CashFlow -> TestResult -> IO ()
+    assertTestResult CashFlow {..} TestResult{eventDate, eventType, payoff} = do
+        assertEqual cashEvent eventType
+        assertEqual cashPaymentDay eventDate
+        assertEqual (realToFrac amount :: Float) (realToFrac payoff :: Float)
+      where
+        assertEqual a b = assertBool (err a b) $ a == b
+        err a b = printf "Mismatch: actual %s, expected %s" (show a) (show b)
 
 testCasesFromFile :: [String] -> FilePath -> IO [TestCase]
-testCasesFromFile excludedTestCases fileName = do
-  tcs <- readFile fileName
-  case let tc = fromString tcs in eitherDecode tc :: Either String (Map String TestCase) of
-    (Right decodedTests) ->
-      return $
-        filter (\TestCase {..} -> notElem identifier excludedTestCases) $
-          fmap snd (Map.toList decodedTests)
-    Left e -> assertFailure ("Cannot parse test specification from file: " ++ fileName ++ "\nError: " ++ e) >> return []
-
-assertTestResults :: [CashFlow] -> [TestResult] -> String -> IO ()
-assertTestResults [] [] _ = return ()
-assertTestResults (cashFlow : restCash) (testResult : restTest) identifier' = do
-  assertTestResult cashFlow testResult identifier'
-  assertTestResults restCash restTest identifier'
-assertTestResults _ _ _ = assertFailure "Sizes differ"
-
-assertTestResult :: CashFlow -> TestResult -> String -> IO ()
-assertTestResult
-  CashFlow {cashPaymentDay = date, cashEvent = event, amount = payoff'}
-  testResult@TestResult {eventDate = testDate, eventType = testEvent, payoff = testPayoff}
-  identifier' = do
-
-    assertBool
-      ("[" ++ show identifier' ++ "] Generated event and test event types should be the same: actual " ++ show event ++ ", expected for " ++ show testResult)
-      $ event == (read testEvent :: EventType)
-
-    assertBool
-      ("Generated date and test date should be the same: actual " ++ show date ++ ", expected for " ++ show testResult ++ " in " ++ identifier')
-      $ date == (fromJust $ parseDate testDate)
-
-    assertBool
-      ("[" ++ show identifier' ++ "]  Generated payoff and test payoff should be the same: actual " ++ show payoff' ++ ", expected for " ++ show testResult)
-      $ (realToFrac payoff' :: Float) == (realToFrac testPayoff :: Float)
-
-parseDate :: String -> Maybe LocalTime
-parseDate date =
-  let format
-        | length date == 19 = "%Y-%-m-%-dT%T"
-        | otherwise = "%Y-%-m-%-dT%H:%M"
-   in parseTimeM True defaultTimeLocale format date :: Maybe LocalTime
+testCasesFromFile excluded f = B.readFile f >>= \l -> case eitherDecode l :: Either String (Map String TestCase) of
+    Right tst -> return $ filter (\TestCase {..} -> notElem identifier excluded) $ elems tst
+    Left err  -> assertFailure ("Cannot parse test specification from file: " ++ f ++ "\nError: " ++ err) :: IO [TestCase]
 
 data DataObserved = DataObserved
   { identifier :: String
@@ -145,8 +126,8 @@ instance FromJSON ValueObserved where
   parseJSON _ = mzero
 
 data TestResult = TestResult
-  { eventDate           :: String,
-    eventType           :: String,
+  { eventDate           :: LocalTime,
+    eventType           :: EventType,
     payoff              :: Double,
     currency            :: String,
     notionalPrincipal   :: Double,
@@ -174,10 +155,20 @@ instance FromJSON TestResult where
 data TestCase = TestCase
   { identifier     :: String,
     terms          :: ContractTerms,
-    to             :: String,
+    to             :: Maybe LocalTime,
     dataObserved   :: Map String DataObserved,
     eventsObserved :: Value,
     results        :: [TestResult]
   }
   deriving stock (Show, Generic)
-  deriving anyclass (FromJSON)
+
+instance FromJSON TestCase where
+  parseJSON (Object v) =
+    TestCase
+      <$> v .: "identifier"
+      <*> v .: "terms"
+      <*> (v .:? "to" <|> return Nothing)
+      <*> v .: "dataObserved"
+      <*> v .: "eventsObserved"
+      <*> v .: "results"
+  parseJSON _ = mzero
