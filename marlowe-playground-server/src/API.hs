ae5441025c168304d3685980d62c7e9f3bf519ec
Yves Hauser <yves.hauser@gmail.com>
2021-11-03 09:59:47 +0100

    SCP-3014: Exposing projected cash flows for ACTUS contracts
    Signed-off-by: Yves Hauser <yves.hauser@gmail.com>

--- marlowe-playground-server/src/API.hs
+++ marlowe-playground-server/src/API.hs
@@ -11,13 +11,15 @@ import           Data.Aeson                                  (FromJSON, ToJSON, 
 import           Data.Text                                   (Text)
 import           GHC.Generics                                (Generic)
 import           Language.Haskell.Interpreter                (InterpreterError, InterpreterResult)
-import qualified Language.Marlowe.ACTUS.Domain.ContractTerms as CT
+import qualified Language.Marlowe.ACTUS.Domain.ContractTerms as A (ContractTerms)
+import qualified Language.Marlowe.ACTUS.Domain.Schedule      as A (CashFlow)
 import           Servant.API                                 (Capture, Get, Header, JSON, NoContent, PlainText, Post,
                                                               Raw, ReqBody, (:<|>), (:>))
 import           Webghc.Server                               (CompileRequest)
 
 type API
      = "oracle" :> Capture "exchange" String :> Capture "pair" String :> Get '[JSON] Value
-       :<|> "actus" :> ("generate" :> ReqBody '[ JSON] CT.ContractTerms :> Post '[ JSON] String
-                        :<|> "generate-static" :> ReqBody '[ JSON] CT.ContractTerms :> Post '[ JSON] String)
+       :<|> "actus" :> ("generate" :> ReqBody '[ JSON] A.ContractTerms :> Post '[ JSON] String
+                        :<|> "generate-static" :> ReqBody '[ JSON] A.ContractTerms :> Post '[ JSON] String
+                        :<|> "cashflows" :> ReqBody '[ JSON] A.ContractTerms :> Post '[ JSON] [A.CashFlow])
        :<|> "compile" :> ReqBody '[JSON] CompileRequest :> Post '[JSON] (Either InterpreterError (InterpreterResult String))
