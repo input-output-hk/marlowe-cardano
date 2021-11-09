b2089e9f69a00e0acca6b259829297d7a062fb2f
Shea Levy <shea@shealevy.com>
2021-11-09 14:37:00 -0500

    Redirect marlowe alpha URLs to staging

--- deployment/morph/machines.nix
+++ deployment/morph/machines.nix
@@ -9,16 +9,56 @@
 
   "${tfinfo.marloweDashA.dns}" = mkMachine {
     name = "marloweDashA";
-    config = ./machines/marlowe-dash.nix;
+    config.networking = {
+      firewall.enable = true;
+      firewall.allowedTCPPorts = [ 22 80 9080 ];
+      timeServers = [ "1.amazon.pool.ntp.org" "2.amazon.pool.ntp.org" "3.amazon.pool.ntp.org" ];
+    };
+    config.services.nginx =
+      {
+        enable = true;
+        recommendedGzipSettings = true;
+        recommendedProxySettings = true;
+        recommendedOptimisation = true;
+
+        virtualHosts = {
+          "marlowe-dash" = {
+            listen = [{ addr = "0.0.0.0"; port = 9080; }];
+            globalRedirect = "marlowe-run-staging.plutus.aws.iohkdev.io";
+          };
+        };
+      };
   };
 
   "${tfinfo.playgroundsA.dns}" = mkMachine {
     name = "playgroundsB";
-    config = ./machines/playground.nix;
+    config.networking = {
+      firewall.enable = true;
+      firewall.allowedTCPPorts = [ 22 80 8181 9080 ];
+      timeServers = [ "1.amazon.pool.ntp.org" "2.amazon.pool.ntp.org" "3.amazon.pool.ntp.org" ];
+    };
+    config.services.nginx =
+      {
+        enable = true;
+        recommendedGzipSettings = true;
+        recommendedProxySettings = true;
+        recommendedOptimisation = true;
+
+        virtualHosts = {
+          "marlowe-web" = {
+            listen = [{ addr = "0.0.0.0"; port = 8181; }];
+            globalRedirect = "marlowe-website-staging.plutus.aws.iohkdev.io";
+          };
+          "marlowe-playground" = {
+            listen = [{ addr = "0.0.0.0"; port = 9080; }];
+            globalRedirect = "marlowe-playground-staging.plutus.aws.iohkdev.io";
+          };
+        };
+      };
   };
 
   "${tfinfo.webghcA.dns}" = mkMachine {
     name = "webghcA";
-    config = ./machines/web-ghc.nix;
+    config = { };
   };
 }
