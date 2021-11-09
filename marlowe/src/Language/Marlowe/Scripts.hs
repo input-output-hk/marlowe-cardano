718479874837c0b3bc74fa63da1d305cc1dc98d0
Alexander Nemish <anemish@gmail.com>
2021-11-09 22:46:22 +0200

    Marlowe validator size: 14870

--- marlowe/src/Language/Marlowe/Scripts.hs
+++ marlowe/src/Language/Marlowe/Scripts.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE DerivingVia           #-}
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE ImportQualifiedPost   #-}
 {-# LANGUAGE LambdaCase            #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NamedFieldPuns        #-}
@@ -22,24 +23,24 @@
 {-# OPTIONS_GHC -fno-specialise #-}
 
 module Language.Marlowe.Scripts where
-import           Data.Default                    (Default (def))
+import           Data.Default                     (Default (def))
 import           Language.Marlowe.Semantics
-import           Language.Marlowe.SemanticsTypes hiding (Contract)
-import           Ledger                          (CurrencySymbol, Datum (..), PubKeyHash, ScriptContext (..), Slot (..),
-                                                  TokenName, ValidatorHash, mkValidatorScript, validatorHash,
-                                                  valueSpent)
-import           Ledger.Ada                      (adaSymbol)
+import           Language.Marlowe.SemanticsTypes  hiding (Contract)
+import           Ledger
+import           Ledger.Ada                       (adaSymbol)
 import           Ledger.Constraints
-import qualified Ledger.Interval                 as Interval
-import           Ledger.Scripts                  (Validator)
-import qualified Ledger.TimeSlot                 as TimeSlot
-import qualified Ledger.Typed.Scripts            as Scripts
-import qualified Ledger.Value                    as Val
-import           Plutus.Contract.StateMachine    (StateMachine (..), Void)
-import qualified Plutus.Contract.StateMachine    as SM
+import           Ledger.Constraints.OnChain
+import           Ledger.Constraints.TxConstraints
+import qualified Ledger.Interval                  as Interval
+import           Ledger.Scripts                   (Validator)
+import qualified Ledger.TimeSlot                  as TimeSlot
+import qualified Ledger.Typed.Scripts             as Scripts
+import qualified Ledger.Value                     as Val
+import           Plutus.Contract.StateMachine     (StateMachine (..), Void)
+import qualified Plutus.Contract.StateMachine     as SM
 import qualified PlutusTx
-import qualified PlutusTx.AssocMap               as AssocMap
-import qualified PlutusTx.Prelude                as P
+import qualified PlutusTx.AssocMap                as AssocMap
+import qualified PlutusTx.Prelude                 as P
 
 type MarloweSlotRange = (Slot, Slot)
 type MarloweInput = (MarloweSlotRange, [Input])
@@ -183,49 +184,87 @@ asdf
     -> MarloweInput
     -> ScriptContext
     -> Bool
-asdf params MarloweData{..} (interval@(minSlot, maxSlot), inputs) ctx = do
+asdf params MarloweData{..} (_, inputs) ctx = do
+    let slotConfig = def :: TimeSlot.SlotConfig
+    let txInfo = scriptContextTxInfo ctx
+    let ownInput = case findOwnInput ctx of
+            Just i -> i
+            _      -> P.traceError "S0"
+    let scriptInValue = maybe (P.traceError "S0" {-"Can't find validation input"-}) (txOutValue . txInInfoResolved) (findOwnInput ctx)
+    let (minTime, maxTime) =
+            case txInfoValidRange txInfo of
+                Interval.Interval (Interval.LowerBound (Interval.Finite l) True) (Interval.UpperBound (Interval.Finite h) True) -> (l, h)
+                _ -> P.traceError "Mr"
+    let timeToSlot = TimeSlot.posixTimeToEnclosingSlot slotConfig
+    let minSlot = timeToSlot minTime
+    let maxSlot = timeToSlot maxTime
+    let interval = (minSlot, maxSlot)
+    let positiveBalances = validateBalances marloweState ||
+            -- Avoid creating a too-big string literal
+            P.traceError ("M1")
+
+    {-  We do not check that a transaction contains exact input payments.
+        We only require an evidence from a party, e.g. a signature for PubKey party,
+        or a spend of a 'party role' token.
+        This gives huge flexibility by allowing parties to provide multiple
+        inputs (either other contracts or P2PKH).
+        Then, we check scriptOutput to be correct.
+     -}
+    let inputsOk = validateInputs1 params txInfo inputs
+
+    -- total balance of all accounts in State
+    -- accounts must be positive, and we checked it above
+    let inputBalance = totalBalance (accounts marloweState)
+
+    -- ensure that a contract TxOut has what it suppose to have
+    let balancesOk = inputBalance P.== scriptInValue
+
+    let preconditionsOk = P.traceIfFalse "M2" $ positiveBalances && balancesOk && inputsOk
+
     let txInput = TransactionInput {
             txInterval = interval,
             txInputs = inputs }
 
     let computedResult = computeTransaction txInput marloweState marloweContract
+    -- let computedResult = TransactionOutput [] [] (emptyState minSlot) Close
     case computedResult of
         TransactionOutput {txOutPayments, txOutState, txOutContract} -> do
             let marloweData = MarloweData {
                     marloweContract = txOutContract,
                     marloweState = txOutState }
 
-            let (outputsConstraints, finalBalance) = let
+            let (payoutsOk, finalBalance) = let
                     payoutsByParty = AssocMap.toList $ P.foldMap payoutByParty txOutPayments
                     in case txOutContract of
-                        _ -> (payoutConstraints payoutsByParty, True)
-
-            -- TODO Push this use of time further down the code
-            let range = TimeSlot.slotRangeToPOSIXTimeRange def $ Interval.interval minSlot maxSlot
-            let constraints :: TxConstraints MarloweInput MarloweData
-                constraints = outputsConstraints
-            checkScriptContext constraints ctx
-        Error _ -> False
+                        Close -> (payoutConstraints txInfo payoutsByParty, P.zero)
+                        _ -> let
+                            payoutsOk = payoutConstraints txInfo payoutsByParty
+                            totalIncome = P.foldMap collectDeposits inputs
+                            totalPayouts = P.foldMap snd payoutsByParty
+                            finalBalance = inputBalance P.+ totalIncome P.- totalPayouts
+                            in (payoutsOk, finalBalance)
+            let outConstrs = OutputConstraint
+                                        { ocDatum = marloweData
+                                        , ocValue = finalBalance
+                                        }
+            let checkOutput = checkOwnOutputConstraint ctx outConstrs
+            preconditionsOk && payoutsOk && checkOutput
+        Error _ -> P.traceError "ME"
 
   where
-    validateInputs :: MarloweParams -> [Input] -> TxConstraints Void Void
-    validateInputs MarloweParams{rolesCurrency} inputs = let
-        (keys, roles) = P.foldMap validateInputWitness inputs
-        mustSpendSetOfRoleTokens = P.foldMap mustSpendRoleToken (AssocMap.keys roles)
-        in P.foldMap mustBeSignedBy keys P.<> mustSpendSetOfRoleTokens
+    validateInputs1 :: MarloweParams -> TxInfo -> [Input] -> Bool
+    validateInputs1 MarloweParams{rolesCurrency} scriptContextTxInfo inputs = P.all validateInputWitness inputs
       where
-        validateInputWitness :: Input -> ([PubKeyHash], AssocMap.Map TokenName ())
+        validateInputWitness :: Input -> Bool
         validateInputWitness input =
             case input of
                 IDeposit _ party _ _         -> validatePartyWitness party
                 IChoice (ChoiceId _ party) _ -> validatePartyWitness party
-                INotify                      -> (P.mempty, P.mempty)
+                INotify                      -> True
           where
-            validatePartyWitness (PK pk)     = ([pk], P.mempty)
-            validatePartyWitness (Role role) = ([], AssocMap.singleton role ())
-
-        mustSpendRoleToken :: TokenName -> TxConstraints Void Void
-        mustSpendRoleToken role = mustSpendAtLeast $ Val.singleton rolesCurrency role 1
+            validatePartyWitness (PK pk)     = P.traceIfFalse "L4" $ scriptContextTxInfo `txSignedBy` pk
+            validatePartyWitness (Role role) = P.traceIfFalse "L5" -- "Spent value not OK"
+                                               $ Val.singleton rolesCurrency role 1 `Val.leq` valueSpent scriptContextTxInfo
 
     collectDeposits :: Input -> Val.Value
     collectDeposits (IDeposit _ _ (Token cur tok) amount) = Val.singleton cur tok amount
@@ -235,14 +274,22 @@ asdf params MarloweData{..} (interval@(minSlot, maxSlot), inputs) ctx = do
     payoutByParty (Payment _ (Party party) money) = AssocMap.singleton party money
     payoutByParty (Payment _ (Account _) _)       = AssocMap.empty
 
-    payoutConstraints :: [(Party, Val.Value)] -> TxConstraints i0 o0
-    payoutConstraints payoutsByParty = P.foldMap payoutToTxOut payoutsByParty
+    payoutConstraints :: TxInfo -> [(Party, Val.Value)] -> Bool
+    payoutConstraints scriptContextTxInfo payoutsByParty = P.all payoutToTxOut payoutsByParty
       where
         payoutToTxOut (party, value) = case party of
-            PK pk  -> mustPayToPubKey pk value
+            PK pk  -> P.traceIfFalse "La" $ value `Val.leq` valuePaidTo scriptContextTxInfo pk
             Role role -> let
                 dataValue = Datum $ PlutusTx.toBuiltinData role
-                in mustPayToOtherScript (rolePayoutValidatorHash params) dataValue value
+                payoutScriptHash = rolePayoutValidatorHash params
+                outs = txInfoOutputs scriptContextTxInfo
+                hsh = findDatumHash dataValue scriptContextTxInfo
+                addr = Ledger.scriptHashAddress payoutScriptHash
+                checkOutput TxOut{txOutAddress, txOutValue, txOutDatumHash=Just svh} =
+                    txOutValue P.== value && hsh P.== Just svh && txOutAddress P.== addr
+                checkOutput _ = False
+                in P.traceIfFalse "Lb" -- "MustPayToOtherScript"
+                $ any checkOutput outs
 
 
 
