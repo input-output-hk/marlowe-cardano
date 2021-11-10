a472758b587a52ae6c769a70e0e7e18e90335b87
Brian W Bush <brian.bush@iohk.io>
2021-11-10 14:50:42 -0700

    SCP-3049 finished CLI for "close" contract
    1. Removed all hardwired values.
    2. Implemented CLI using optparse-applicative
    3. Regression test against 870b21262ef87219231cbaa13a091e74803aea56
    
    Remaining work:
    
    4. API documentation
    5. Tutorial
    6. Usage on testnet.

--- marlowe/test-run.sh
+++ marlowe/test-run.sh
@@ -5,3 +5,16 @@ cabal run exe:marlowe-cli -- validator --network-magic 1097911063   \
                                        --print-address              \
                                        --print-hash                 \
                                        --print-stats
+
+cabal run exe:marlowe-cli -- datum --account-hash d7604c51452bf9c135d63c686ba306d268fcae8494c877e12c44c657 \
+                                   --account-value 3000000                                                 \
+                                   --min-slot 10                                                           \
+                                   --datum-file test.datum                                                 \
+                                   --print-hash                                                            \
+                                   --print-stats
+
+cabal run exe:marlowe-cli -- redeemer --min-slot 1000     \
+                                      --max-slot 43000000 \
+                                      --redeemer-file     \
+                                      test.redeemer       \
+                                      --print-stats
