c06c9af930ffc920c49ebe7f4ccadc9318ab392f
Jamie Bertram <jamie.bertram@tweag.io>
2021-11-22 16:34:09 +0000

    Define FUTUR contract terms

---
+++ purescript-actus/src/Data/Actus/Contract/Future.purs
@@ -0,0 +1,36 @@
+module Data.Actus.Contract.Future where
+
+import Data.Actus.Contract (Contract)
+import Data.Actus.Types as Types
+import Data.DateTime (DateTime)
+import Data.Interval.Duration.Iso (IsoDuration)
+import Data.Map.Heterogeneous (HMap)
+import Data.Maybe (Maybe)
+
+type Future = Contract
+  ( calendar :: Calendar
+  , contractPerformance :: Types.ContractPerformance
+  , contractStructure :: Types.ContractStructure
+  , notionalPrincipal :: NotionalPrincipal
+  , settlement :: Maybe Settlement
+  , futuresPrice :: Number
+  )
+
+type Calendar = HMap
+  ( capitalizationEndDate :: Types.Calendar
+  , businessDayConvention :: Types.BusinessDayConvention
+  , endOfMonthConvention :: Types.EndOfMonthConvention
+  )
+
+type NotionalPrincipal =
+  { maturityDate :: DateTime
+  , purchase :: Maybe Types.ContractEndEvent
+  , termination :: Maybe Types.ContractEndEvent
+  }
+
+type Settlement =
+  { exerciseAmount :: Number
+  , exerciseDate :: DateTime
+  , period :: Maybe IsoDuration
+  , deliverySettlement :: Maybe Types.DeliverySettlement
+  }
