70801d860c77ec9d440db6e67824db3a59a622e3
Jamie Bertram <jamie.bertram@tweag.io>
2022-01-13 13:35:50 -0500

    WIP cardano-wallet service

--- marlowe-dashboard-client/arion-compose.nix
+++ marlowe-dashboard-client/arion-compose.nix
@@ -3,41 +3,71 @@
 , ...
 }:
 let
-  inherit (pkgs) coreutils;
-  inherit (pkgs.marlowe) cardano-node;
+  inherit (pkgs) cardano-wallet cardano-node;
   node-port = "3001";
+  wallet-port = "8090";
+
+  cardano-node-service = {
+    image = {
+      name = "cardano-node";
+      contents = [ cardano-node ];
+      command = [
+        "${cardano-node}/bin/cardano-node"
+        "run"
+        "--config"
+        "/var/config/config.json"
+        "--topology"
+        "/var/config/topology.yaml"
+        "--database-path"
+        "/var/db"
+        "--socket-path"
+        "/var/alloc/node.sock"
+        "--port"
+        "8000"
+      ];
+    };
+    service = {
+      useHostStore = true;
+      ports = [ "${node-port}:8000" ];
+      volumes = [
+        "alloc-volume:/var/alloc"
+        "${../bitte/node/config}:/var/config"
+        "/var/db"
+      ];
+    };
+  };
+
+  cardano-wallet-service = {
+    image = {
+      name = "cardano-wallet";
+      contents = [ cardano-wallet ];
+      command = [
+        "${cardano-wallet}/bin/cardano-wallet"
+        "serve"
+        "--listen-address"
+        "*"
+        "--node-socket"
+        "/var/alloc/node.sock"
+        "--testnet"
+        "/var/config/byron-genesis.json"
+        "--port"
+        "8000"
+      ];
+    };
+    service = {
+      useHostStore = true;
+      ports = [ "${wallet-port}:8000" ];
+      volumes = [
+        "alloc-volume:/var/alloc"
+        "${../bitte/node/config}:/var/config"
+      ];
+    };
+  };
+
 in
 {
   config.services = {
-    cardano-node = {
-      image = {
-        name = "cardano-node";
-        contents = [ cardano-node ];
-        command = [
-          "${cardano-node}/bin/cardano-node"
-          "run"
-          "--config"
-          "/var/config/config.json"
-          "--topology"
-          "/var/config/topology.yaml"
-          "--database-path"
-          "/var/db"
-          "--socket-path"
-          "/var/alloc/node.sock"
-          "--port"
-          node-port
-        ];
-      };
-      service = {
-        useHostStore = true;
-        ports = [ "${node-port}:${node-port}" ];
-        volumes = [
-          "alloc-volume:/var/alloc"
-          "${../bitte/node/config}:/var/config"
-          "/var/db"
-        ];
-      };
-    };
+    inherit cardano-node-service cardano-wallet-service;
   };
   config.docker-compose.raw = {
     volumes = {
