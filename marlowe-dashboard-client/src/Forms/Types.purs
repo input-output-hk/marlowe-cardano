a34c23560c929982b8bac6547a9904796e705aba
Tomasz Rybarczyk <tomasz.rybarczyk@iohk.io>
2022-01-31 23:10:45 +0100

    Add basic forms field constructors

--- marlowe-dashboard-client/src/Forms/Types.purs
+++ marlowe-dashboard-client/src/Forms/Types.purs
@@ -1,14 +1,28 @@
 module Forms.Types where
 
+import Prelude
+
 import Data.FormURLEncoded.Query (Query) as FormURLEncoded
+import Data.Generic.Rep (class Generic)
 import Data.Map.Ordered.OMap (OMap)
+import Data.Newtype (class Newtype)
+import Data.Show.Generic (genericShow)
 import Polyform.Batteries.UrlEncoded (Errors) as UrlEncoded
 
 type RenderWidgetFn :: Type -> Type -> Type -> Type
 type RenderWidgetFn state err widget =
   state -> FormURLEncoded.Query -> UrlEncoded.Errors err -> widget
 
-type Widgets widget = OMap String widget
+newtype WidgetId = WidgetId String
+
+derive instance Newtype WidgetId _
+derive instance Generic WidgetId _
+derive newtype instance Eq WidgetId
+derive newtype instance Ord WidgetId
+instance Show WidgetId where
+  show = genericShow
+
+type Widgets widget = OMap WidgetId widget
 
 type RenderFn state err widget =
   state -> FormURLEncoded.Query -> UrlEncoded.Errors err -> Widgets widget
--- marlowe-dashboard-client/src/Forms/Uni.purs
+++ marlowe-dashboard-client/src/Forms/Uni.purs
@@ -26,49 +26,32 @@ module Forms.Uni where
 import Prelude
 
 import Control.Alt (class Alt, (<|>))
-import Control.Bind (bindFlipped)
-import Control.Monad.Maybe.Trans (MaybeT(..), mapMaybeT)
-import Control.Monad.Rec.Class (class MonadRec)
-import Control.Monad.Trans.Class (lift)
-import Control.Monad.Writer (WriterT(..), mapWriterT)
 import Control.Plus (class Plus, empty)
-import Data.Array (head, singleton) as Array
-import Data.Bifunctor (bimap, lmap)
-import Data.Either (Either, hush)
-import Data.Foldable (class Foldable)
-import Data.FoldableWithIndex (class FoldableWithIndex)
-import Data.FormURLEncoded.Query (Query(..))
-import Data.Functor.Contravariant (cmap)
-import Data.FunctorWithIndex (mapWithIndex)
-import Data.Generic.Rep (class Generic)
-import Data.Lens (Lens, Lens', _1, _2, iso, lens, over, preview, set, view)
-import Data.Lens as Lens
-import Data.Lens.Index (class Index, ix)
+import Data.Array (head) as Array
+import Data.FormURLEncoded.Query (FieldId(..), Query)
+import Data.FormURLEncoded.Query (FieldId, alter, lookup, singleton) as Query
+import Data.Lens (Lens')
 import Data.Lens.Iso.Newtype (_Newtype)
 import Data.Lens.Record (prop)
-import Data.Map (SemigroupMap(..))
-import Data.Map (insert, lookup) as Map
-import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Map.Ordered.OMap (singleton) as OMap
+import Data.Maybe (Maybe(..))
 import Data.Newtype (class Newtype)
-import Data.Profunctor (class Profunctor, dimap, lcmap)
+import Data.Profunctor (class Profunctor, dimap)
 import Data.Profunctor.Choice (class Choice, left, right)
-import Data.Profunctor.Star (Star(..))
 import Data.Profunctor.Strong (class Strong, first, second)
-import Data.Show.Generic (genericShow)
-import Data.TraversableWithIndex (class TraversableWithIndex, traverseWithIndex)
-import Data.Tuple (Tuple(..))
-import Data.Validation.Semigroup (V(..))
-import Forms.FormM (FormM(..))
-import Forms.Types (RenderFn)
-import Network.RemoteData (RemoteData(..), fromEither, toMaybe)
-import Polyform (Reporter(..), Validator)
+import Forms.FormM (FormM)
+import Forms.FormM (mapUpdateFn) as FormM
+import Forms.Types (RenderFn, WidgetId(..))
+import Polyform (Validator)
 import Polyform.Batteries.UrlEncoded (Errors) as UrlEncoded
-import Polyform.Reporter (R, liftFnR, lmapReporter)
-import Polyform.Reporter as Reporter
+import Polyform.Batteries.UrlEncoded.Types.Errors (ErrorId(..))
+import Polyform.Batteries.UrlEncoded.Types.Errors (lookup, singleton) as Errors
 import Polyform.Validator (hoist) as Validator
-import Polyform.Validator (liftFn, lmapValidator, runValidator)
-import Type.Prelude (Proxy(..))
-import Unsafe.Coerce (unsafeCoerce)
+import Polyform.Validator (liftFn, lmapValidator)
+import Prim.Row (class Cons) as Row
+import Record (get) as Record
+import Safe.Coerce (coerce) as Safe
+import Type.Prelude (class IsSymbol, Proxy(..), reflectSymbol)
 
 -- | We keep `input` polymorphic here (it will be `Payload`)
 -- | because we want to provide `Profunctor` and `Category`
@@ -82,7 +65,7 @@ newtype Form m state widget err input output = Form
   }
 
 -- | Usual starting point of the validation.
-type Form' m state widget doc output = Form m state widget Query output
+type Form' m state widget err output = Form m state widget err Query output
 
 derive instance Newtype (Form m st wd err i o) _
 derive instance Applicative m => Functor (Form m st wd err i)
@@ -171,3 +154,106 @@ _validator
    . Lens' (Form m st wd err i o)
        (Validator (FormM st Query m) (UrlEncoded.Errors err) i o)
 _validator = _Newtype <<< prop (Proxy :: Proxy "validator")
+
+type FieldErrors err = Array err
+
+type FieldValue = String
+
+type RenderFieldFn err wd = Maybe FieldValue -> FieldErrors err -> wd
+
+type FieldM st = FormM st (Maybe FieldValue)
+
+-- | Helper functions
+flattenValue :: FieldId -> Query -> Maybe FieldValue
+flattenValue id = Array.head <=< Query.lookup id
+
+alterOverFieldValue
+  :: FieldId
+  -> (Maybe FieldValue -> Maybe FieldValue)
+  -> (Query -> Query)
+alterOverFieldValue id f = flip Query.alter id \m -> do
+  r <- f do
+    arr <- m
+    Array.head arr
+  pure [ r ]
+
+-- | Stateless field which accesses only its own data.
+type FieldSpec m st wd err o =
+  { id :: Query.FieldId
+  , validator ::
+      Validator (FormM st (Maybe FieldValue) m) (FieldErrors err)
+        (Maybe FieldValue)
+        o
+  , render :: RenderFieldFn err wd
+  }
+
+mkFieldForm
+  :: forall wd err m o st
+   . Monad m
+  => FieldSpec m st wd err o
+  -> Form' m st wd err o
+mkFieldForm { id: fieldId, render, validator } = do
+  let
+    errId :: ErrorId
+    errId = Safe.coerce fieldId
+
+    wdId :: WidgetId
+    wdId = Safe.coerce fieldId
+  Form
+    { init: const $ mempty
+    , render: \_ payload errs -> do
+        let
+          widget = render (flattenValue fieldId payload)
+            (Errors.lookup errId errs)
+        OMap.singleton wdId widget
+    , validator:
+        validator <<< liftFn (flattenValue fieldId)
+          # lmapValidator (Errors.singleton errId)
+          # Validator.hoist (FormM.mapUpdateFn (alterOverFieldValue fieldId))
+    }
+
+type RenderStFieldFn o err wd = o -> Maybe FieldValue -> FieldErrors err -> wd
+
+type StFieldSpec
+  :: (Type -> Type) -> Type -> Type -> Type -> Type -> Symbol -> Type
+type StFieldSpec m st wd errs o l =
+  { init :: Maybe (o -> FieldValue)
+  , id :: Proxy l
+  , validator ::
+      Validator (FormM st (Maybe FieldValue) m) (FieldErrors errs)
+        (Maybe FieldValue)
+        o
+  , render :: RenderStFieldFn o errs wd
+  }
+
+mkStFieldForm
+  :: forall err l m o st st_ wd
+   . Monad m
+  => IsSymbol l
+  => Row.Cons l o st_ st
+  => StFieldSpec m { | st } wd err o l
+  -> Form' m { | st } wd err o
+mkStFieldForm { id: l, init, render, validator } = do
+  let
+    id = reflectSymbol l
+    fieldId = FieldId id
+    errId = ErrorId id
+    wdId = WidgetId id
+  Form
+    { init: case init of
+        Nothing -> mempty
+        Just f -> \st -> do
+          let
+            v = Record.get l st
+          Query.singleton fieldId [ f v ]
+    , render: \st payload errs -> do
+        let
+          widget = render (Record.get l st) (flattenValue fieldId payload)
+            (Errors.lookup errId errs)
+        OMap.singleton wdId widget
+    , validator:
+        validator <<< liftFn (flattenValue fieldId)
+          # lmapValidator (Errors.singleton errId)
+          # Validator.hoist (FormM.mapUpdateFn (alterOverFieldValue fieldId))
+    }
+
