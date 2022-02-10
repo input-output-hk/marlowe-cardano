6d4050d07d44b28acfa980d5104b47d52b55369f
Jamie Bertram <jamie.bertram@tweag.io>
2022-02-10 18:13:53 -0500

    Add example test case for testing library

--- marlowe-dashboard-client/test/Main.purs
+++ marlowe-dashboard-client/test/Main.purs
@@ -2,8 +2,38 @@ module Test.Main where
 
 import Prologue
 
+import Data.String (toLower)
 import Effect (Effect)
-import Effect.Class.Console as Console
+import Effect.Aff (launchAff_)
+import Effect.Class (liftEffect)
+import Test.Spec (describe, it)
+import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Reporter (consoleReporter)
+import Test.Spec.Runner (runSpec)
+import Test.Web (runTestMInBody)
+import Test.Web.DOM.Query (Match(..), get)
+import Test.Web.Event.User (click, runUserM)
+import Test.Web.Monad (getContainer)
+import Web.ARIA (ARIARole(..))
+import Web.DOM (Element)
+import Web.DOM.Element as Element
+import Web.DOM.Node (textContent)
+
+foreign import setupTestApp :: Element -> Effect Unit
 
 main :: Effect Unit
-main = Console.log "TODO write some tests"
+main = launchAff_ $ runSpec [ consoleReporter ] do
+  describe "DOM tests" do
+    it "works with JSDOM" do
+      runUserM Nothing $ runTestMInBody do
+        body <- getContainer
+        toLower (Element.tagName body) `shouldEqual` "body"
+        liftEffect $ setupTestApp body
+        paragraph <- get $ ByRole Paragraph Nothing
+        pid <- liftEffect $ Element.id paragraph
+        pid `shouldEqual` "para"
+        text <- liftEffect $ textContent $ Element.toNode paragraph
+        text `shouldEqual` "Test content"
+        click =<< get (ByRole Button Nothing)
+        text' <- liftEffect $ textContent $ Element.toNode paragraph
+        text' `shouldEqual` "It worked!"
