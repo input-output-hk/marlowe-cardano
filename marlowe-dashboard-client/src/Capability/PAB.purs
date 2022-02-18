1037d8416521ca057d1b154be08a6ca2ca206dc2
Jamie Bertram <jamie.bertram@tweag.io>
2022-02-18 16:41:52 -0500

    Move endpoint locking to ManagePAB

--- marlowe-dashboard-client/src/Capability/PAB.purs
+++ marlowe-dashboard-client/src/Capability/PAB.purs
@@ -2,14 +2,15 @@ module Capability.PAB
   ( class ManagePAB
   , activateContract
   , deactivateContract
-  , getContractInstanceClientState
-  , getContractInstanceCurrentState
-  , getContractInstanceObservableState
-  , getContractInstanceHooks
-  , invokeEndpoint
-  , getWalletContractInstances
   , getAllContractInstances
   , getContractDefinitions
+  , getContractInstanceClientState
+  , getContractInstanceCurrentState
+  , getContractInstanceHooks
+  , getContractInstanceObservableState
+  , getWalletContractInstances
+  , invokeEndpoint
+  , onNewActiveEndpoints
   ) where
 
 import Prologue
@@ -17,11 +18,26 @@ import Prologue
 import API.Lenses (_cicCurrentState, _hooks, _observableState)
 import AppM (AppM)
 import Control.Monad.Except (lift)
+import Control.Monad.Reader (asks)
+import Data.Align (align)
 import Data.Argonaut (Json, encodeJson)
 import Data.Argonaut.Encode (class EncodeJson)
 import Data.Lens (view)
+import Data.Map (Map)
+import Data.Map as Map
+import Data.Map.Alignable (AlignableMap(..))
+import Data.Maybe (fromMaybe, maybe)
+import Data.Newtype (unwrap)
+import Data.Set as Set
+import Data.These (These(..))
+import Data.Traversable (sequence, traverse)
 import Data.WalletId (WalletId)
 import Data.WalletId as WalletId
+import Effect.Aff (Aff)
+import Effect.Aff.AVar (AVar)
+import Effect.Aff.AVar as AVar
+import Effect.Aff.Class (liftAff)
+import Env (_endpointSemaphores)
 import Halogen (HalogenM)
 import Marlowe.PAB (PlutusAppId)
 import MarloweContract (MarloweContract)
@@ -37,10 +53,7 @@ import Plutus.PAB.Webserver.Types
 import Types (AjaxResponse)
 import Wallet.Emulator.Wallet (Wallet(..))
 
--- TODO (possibly): make `AppM` a `MonadError` and remove all the `runExceptT`s
-class
-  Monad m <=
-  ManagePAB m where
+class Monad m <= ManagePAB m where
   activateContract
     :: MarloweContract -> WalletId -> m (AjaxResponse PlutusAppId)
   deactivateContract :: PlutusAppId -> m (AjaxResponse Unit)
@@ -66,6 +79,7 @@ class
     (AjaxResponse (Array (ContractInstanceClientState MarloweContract)))
   getContractDefinitions :: m
     (AjaxResponse (Array (ContractSignatureResponse MarloweContract)))
+  onNewActiveEndpoints :: PlutusAppId -> Array ActiveEndpoint -> m Unit
 
 instance ManagePAB AppM where
   activateContract contractActivationId wallet =
@@ -88,6 +102,20 @@ instance ManagePAB AppM where
     currentState <- getContractInstanceCurrentState plutusAppId
     pure $ map (view _hooks) currentState
   invokeEndpoint plutusAppId endpoint payload = do
+    endpointSemaphoresAVar <- asks $ view _endpointSemaphores
+    liftAff do
+      endpointSemaphores <- AVar.take endpointSemaphoresAVar
+      let
+        appSemaphores =
+          fromMaybe Map.empty $ Map.lookup plutusAppId endpointSemaphores
+      endpointSemaphore <- maybe AVar.empty pure
+        $ Map.lookup endpoint appSemaphores
+      let
+        appSemaphores' = Map.insert endpoint endpointSemaphore appSemaphores
+        endpointSemaphores' =
+          Map.insert plutusAppId appSemaphores' endpointSemaphores
+      AVar.put endpointSemaphores' endpointSemaphoresAVar
+      AVar.take endpointSemaphore
     PAB.postApiContractInstanceByContractinstanceidEndpointByEndpointname
       (encodeJson payload)
       plutusAppId
@@ -97,6 +125,33 @@ instance ManagePAB AppM where
       Nothing
   getAllContractInstances = PAB.getApiContractInstances Nothing
   getContractDefinitions = PAB.getApiContractDefinitions
+  onNewActiveEndpoints appId endpoints = do
+    let
+      endpointMap :: Map String Unit
+      endpointMap = Set.toMap $ Set.fromFoldable $ map
+        (_.getEndpointDescription <<< unwrap <<< _.aeDescription <<< unwrap)
+        endpoints
+    endpointSemaphoresAVar <- asks $ view _endpointSemaphores
+    liftAff do
+      endpointSemaphores <- AVar.take endpointSemaphoresAVar
+      appSemaphores' <- case Map.lookup appId endpointSemaphores of
+        Nothing -> traverse (const $ AVar.new unit) endpointMap
+        Just appSemaphores -> sequence $ unwrap $ align updateSemaphore
+          (AlignableMap appSemaphores)
+          (AlignableMap endpointMap)
+      AVar.put
+        (Map.insert appId appSemaphores' endpointSemaphores)
+        endpointSemaphoresAVar
+    where
+    -- endpoint was in semaphores, but not in new available endpoints. Lock it
+    -- (without blocking).
+    updateSemaphore :: These (AVar Unit) Unit -> Aff (AVar Unit)
+    updateSemaphore (This semaphore) = AVar.tryTake semaphore $> semaphore
+    -- endpoint was in new available endpoints, but not in semaphores. Create a
+    -- new, unlocked semaphore for it.
+    updateSemaphore (That _) = AVar.new unit
+    -- endpoint was in both available endpoints, and semaphores. Unlock it.
+    updateSemaphore (Both semaphore _) = AVar.put unit semaphore $> semaphore
 
 instance ManagePAB m => ManagePAB (HalogenM state action slots msg m) where
   activateContract contractActivationId wallet = lift $ activateContract
@@ -115,3 +170,4 @@ instance ManagePAB m => ManagePAB (HalogenM state action slots msg m) where
   getWalletContractInstances = lift <<< getWalletContractInstances
   getAllContractInstances = lift getAllContractInstances
   getContractDefinitions = lift getContractDefinitions
+  onNewActiveEndpoints appId = lift <<< onNewActiveEndpoints appId
--- marlowe-dashboard-client/src/Env.purs
+++ marlowe-dashboard-client/src/Env.purs
@@ -7,16 +7,27 @@ import Control.Logger.Effect (Logger)
 import Data.Lens (Lens')
 import Data.Lens.Iso.Newtype (_Newtype)
 import Data.Lens.Record (prop)
+import Data.Map (Map)
 import Data.Newtype (class Newtype)
 import Data.Time.Duration (Milliseconds)
 import Effect.AVar (AVar)
 import Halogen (SubscriptionId)
+import Marlowe.PAB (PlutusAppId)
 import Plutus.PAB.Webserver.Types
   ( CombinedWSStreamToClient
   , CombinedWSStreamToServer
   )
 import Type.Proxy (Proxy(..))
 import WebSocket.Support (WebSocketManager) as WS
+
+-- A two-layer mapping of semaphores. The keys of the outer Map correspond to
+-- the app IDs of the different plutus apps, the keys of the inner Map
+-- correspond to the endpoint names of the endpoints within that app, and the
+-- values of the inner map are AVars which are used to conrol when that
+-- endpoint is available (when the AVar is empty, it is unavailable, and
+-- prospective clients of that endpoint will need to wait until it becomes
+-- available again).
+type EndpointSemaphores = Map PlutusAppId (Map String (AVar Unit))
 
 -- Application enviroment configuration
 newtype Env = Env
@@ -31,8 +42,7 @@ newtype Env = Env
     --    In contrast, the Env is created in Main, where we already have access to Effect
     contractStepCarouselSubscription :: AVar SubscriptionId
   , logger :: Logger String
-  -- See note on Capability.PlutusApps.MarloweApp.Types
-  , marloweEndpoints :: MarloweApp.Endpoints
+  , endpointSemaphores :: AVar EndpointSemaphores
   -- For each request we fire, we store in a queue the tuple of the
   -- request id and an avar to wait for the response. We use an array
   -- instead of a Map because we only want to keep a limited number of
@@ -53,5 +63,5 @@ _pollingInterval = _Newtype <<< prop (Proxy :: _ "pollingInterval")
 _pendingResults :: Lens' Env MarloweApp.PendingResults
 _pendingResults = _Newtype <<< prop (Proxy :: _ "pendingResults")
 
-_marloweEndpoints :: Lens' Env MarloweApp.Endpoints
-_marloweEndpoints = _Newtype <<< prop (Proxy :: _ "marloweEndpoints")
+_endpointSemaphores :: Lens' Env (AVar EndpointSemaphores)
+_endpointSemaphores = _Newtype <<< prop (Proxy :: _ "endpointSemaphores")
