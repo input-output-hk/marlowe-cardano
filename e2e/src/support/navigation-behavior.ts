6016052fde23710da626ac3039056919acac854d
Larry Adames <adames.larry@gmail.com>
2022-03-08 16:58:38 -0500

    SCP-3508 fix cucumber specs using page element identifier from json mappings

--- e2e/src/support/navigation-behavior.ts
+++ e2e/src/support/navigation-behavior.ts
@@ -11,17 +11,9 @@ export const navigateToPage = async (
   } = process.env
 
   const hostPath = hostsConfig[`${hostName}`];
-
-  console.log("hostPath ", hostPath);
-
   const url = new URL(hostPath);
-
-  console.log("url ", url)
-
   const pagesConfigItem = pagesConfig[pageId];
   url.pathname = pagesConfigItem.route;
-
-  console.log("pages route ", url.pathname)
 
   await page.goto(url.href);
 }
