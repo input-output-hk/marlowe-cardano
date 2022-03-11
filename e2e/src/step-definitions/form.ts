83efcdd7b799672e10efd129eb3850c50adb9075
Larry Adames <adames.larry@gmail.com>
2022-03-11 16:28:30 -0500

    SCP-3457 implement html interaction to select dropdowns

--- e2e/src/step-definitions/form.ts
+++ e2e/src/step-definitions/form.ts
@@ -3,7 +3,10 @@ import { waitFor } from "../support/wait-for-behavior";
 import { ScenarioWorld } from './setup/world';
 import { getElementLocator } from '../support/web-element-helper';
 import { ElementKey } from '../env/global';
-import { inputValue } from '../support/html-behavior';
+import {
+  inputValue,
+  selectValue
+} from '../support/html-behavior';
 
 When(
   /^I fill in the "([^"]*)" input with "([^"]*)"$/,
@@ -24,7 +27,31 @@ When(
 
       if (result) {
         await inputValue(page, elementIdentifier, input)
+        return result;
+      }
+    })
+  }
+)
 
+When(
+  /^I select the "([^"]*)" option from the "([^"]*)"$/,
+  async function(this: ScenarioWorld, option: string, elementKey: ElementKey) {
+    const {
+      screen: { page },
+      globalConfig,
+    } = this;
+
+    console.log(`I select the ${option} option from the ${elementKey}`)
+
+    const elementIdentifier = getElementLocator(page, elementKey, globalConfig);
+
+    await waitFor(async() => {
+      const result = await page.waitForSelector(elementIdentifier, {
+        state: 'visible'
+      });
+
+      if (result) {
+        await selectValue(page, elementIdentifier, option)
         return result;
       }
     })
