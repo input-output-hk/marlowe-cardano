3c0f92353481827774a5010f429a1d8c2eaa9256
Jamie Bertram <jamie.bertram@tweag.io>
2022-03-17 14:34:45 -0600

    Set LineBuffering in Marlowe PAB

--- marlowe/pab/Main.hs
+++ marlowe/pab/Main.hs
@@ -6,9 +6,14 @@
 {-# LANGUAGE TypeFamilies       #-}
 module Main(main) where
 
+import GHC.IO.Handle (BufferMode (LineBuffering), hSetBuffering)
 import MarloweContract (MarloweContract)
 import qualified Plutus.PAB.Effects.Contract.Builtin as Builtin
 import Plutus.PAB.Run (runWith)
+import System.IO (stderr, stdout)
 
 main :: IO ()
-main = runWith (Builtin.handleBuiltin @MarloweContract)
+main = do
+  hSetBuffering stdout LineBuffering
+  hSetBuffering stderr LineBuffering
+  runWith (Builtin.handleBuiltin @MarloweContract)
