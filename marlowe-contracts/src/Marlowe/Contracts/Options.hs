276b0bf0e3ae2dd03055e801f9d3e876c6f171a5
Yves Hauser <yves.hauser@gmail.com>
2022-03-17 11:52:04 +0100

    SCP-3541: Added barrier options

--- marlowe-contracts/src/Marlowe/Contracts/Options.hs
+++ marlowe-contracts/src/Marlowe/Contracts/Options.hs
@@ -12,6 +12,7 @@ module Marlowe.Contracts.Options
   -- ** Partially collateralized
   , callSpread
   -- ** Not collateralized
+  , barrierOption
   , strangle
   , straddle
   )
@@ -32,6 +33,13 @@ data ExerciseType =
     | American -- ^ Execution anytime before expiry
   deriving Show
 
+-- |Barrier type
+data BarrierType =
+      DownAndIn  -- ^ Knock in
+    | DownAndOut -- ^ Knock out
+    | UpAndIn    -- ^ Knock in
+    | UpAndOut   -- ^ Knock out
+
 -- |An /option/ is a financial instrument that is based on the value of underlying
 -- securities. An options contract offers the buyer the opportunity but not
 -- the obligation to buy or sell the underlying asset.
@@ -44,7 +52,7 @@ option ::
   -> (Token, Value) -- ^ Underlying
   -> (Token, Value) -- ^ Strike
   -> Timeout        -- ^ Expiry
-  -> Timeout        -- ^ Settlement Date
+  -> Timeout        -- ^ Settlement date
   -> Contract       -- ^ Option Contract
 option American optionType buyer seller priceFeed asset strike expiry settlement =
   exercise
@@ -85,22 +93,22 @@ exercise ::
   -> Contract       -- ^ Contract
 exercise Call buyer seller Nothing asset strike timeout =
   When
-    [ choose (ChoiceId "Exercise Call" buyer)
+    [ choose (ChoiceId "Exercise Call" buyer) Close
     $ depositAndPay buyer seller strike asset timeout
     ]
 exercise Call buyer seller (Just choiceId) asset strike timeout =
   When
-    [ chooseOracle choiceId strike ValueLT
+    [ chooseOracle choiceId strike ValueLT Close
     $ depositAndPay buyer seller strike asset timeout
     ]
 exercise Put buyer seller Nothing asset strike timeout =
   When
-    [ choose (ChoiceId "Exercise Put" buyer)
+    [ choose (ChoiceId "Exercise Put" buyer) Close
     $ depositAndPay buyer seller asset strike timeout
     ]
 exercise Put buyer seller (Just choiceId) asset strike timeout =
   When
-    [ chooseOracle choiceId strike ValueGT
+    [ chooseOracle choiceId strike ValueGT Close
     $ depositAndPay buyer seller asset strike timeout
     ]
 
@@ -118,27 +126,29 @@ depositAndPay buyer seller asset strike timeout =
   $ pay buyer seller asset
     Close
 
--- |Choosing explicitly whether to exercise the option
+-- |Choosing explicitly
 choose ::
      ChoiceId -- ^ Choice id, references the party to chose
-  -> Contract -- ^ Continuation Contract if the option is exercised
+  -> Contract -- ^ Continuation Contract if 0 chosen
+  -> Contract -- ^ Continuation Contract if 1 chosen
   -> Case     -- ^ Case expression with continuation
-choose choiceId cont =
+choose choiceId continuation0 continuation1 =
   Case
     (Choice choiceId [Bound 0 1])
-    (If (ValueEQ (ChoiceValue choiceId) (Constant 0)) Close cont)
+    (If (ValueEQ (ChoiceValue choiceId) (Constant 0)) continuation0 continuation1)
 
--- |Choosing whether to exercise the option using a price feed
+-- |Choosing based on a price feed
 chooseOracle ::
      ChoiceId                        -- ^ Price feed
   -> (Token,Value)                   -- ^ Strike price
   -> (Value -> Value -> Observation) -- ^ Comparison function
-  -> Contract                        -- ^ Continuation Contract if the option is exercised
+  -> Contract                        -- ^ Continuation Contract if condition holds
+  -> Contract                        -- ^ Continuation Contract if condition does not hold
   -> Case                            -- ^ Case expression with continuation
-chooseOracle choiceId (_, strike) comparision cont =
+chooseOracle choiceId (_, strike) comparision continuation0 continuation1 =
    Case
       (Choice choiceId [Bound 0 100_000_000_000])
-      (If (comparision (ChoiceValue choiceId) strike) Close cont)
+      (If (ChoiceValue choiceId `comparision` strike) continuation0 continuation1)
 
 -- |A /Covered Call/ is an option strategy constructed by writing a call on a token
 -- and in addition providing the token as cover/collateral as part of the contract
@@ -150,9 +160,9 @@ coveredCall ::
   -> Token          -- ^ Underlying
   -> Value          -- ^ Strike price (in currency)
   -> Value          -- ^ Amount of underlying tokens per contract
-  -> Timeout        -- ^ Issue Date
+  -> Timeout        -- ^ Issue date
   -> Timeout        -- ^ Maturity
-  -> Timeout        -- ^ Settlement Date
+  -> Timeout        -- ^ Settlement date
   -> Contract       -- ^ Covered Call Contract
 coveredCall issuer counterparty priceFeed currency underlying strike ratio issue maturity settlement =
     deposit issuer issuer (underlying, ratio) issue Close
@@ -169,7 +179,7 @@ straddle ::
   -> Value          -- ^ Ratio
   -> Value          -- ^ Strike
   -> Timeout        -- ^ Maturity
-  -> Timeout        -- ^ Settlement Date
+  -> Timeout        -- ^ Settlement date
   -> Contract       -- ^ Straddle Contract
 straddle buyer seller priceFeed currency underlying ratio strike maturity settlement =
   let c = option European Call buyer seller priceFeed (underlying, ratio) (currency, strike) maturity settlement
@@ -188,7 +198,7 @@ strangle ::
   -> Value          -- ^ Lower Strike
   -> Value          -- ^ Upper Strike
   -> Timeout        -- ^ Maturity
-  -> Timeout        -- ^ Settlement Date
+  -> Timeout        -- ^ Settlement date
   -> Contract       -- ^ Straddle Contract
 strangle buyer seller priceFeed currency underlying ratio strike1 strike2 maturity settlement =
   let c = option European Call buyer seller priceFeed (underlying, ratio) (currency, strike1) maturity settlement
@@ -207,9 +217,49 @@ callSpread ::
   -> Value          -- ^ Strike price (in currency) for the short position
   -> Value          -- ^ Amount of underlying tokens per contract
   -> Timeout        -- ^ Maturity
-  -> Timeout        -- ^ Settlement Date
+  -> Timeout        -- ^ Settlement date
   -> Contract       -- ^ Call Spread Contract
 callSpread buyer seller priceFeed currency underlying strike1 strike2 ratio maturity settlement =
   let l = option European Call buyer seller priceFeed (underlying, ratio) (currency, strike1) maturity settlement
       s = option European Call seller buyer priceFeed (underlying, ratio) (currency, strike2) maturity settlement
    in l `both` s
+
+-- |A /Barrier Option/ is a path dependent option. The option gets activated or deactivated depending on current
+-- market conditions for the underlying instrument.
+barrierOption ::
+     ExerciseType   -- ^ Exercise Type
+  -> OptionType     -- ^ Type of Option
+  -> BarrierType    -- ^ Barrier Type
+  -> Party          -- ^ Issuer of the covered Call
+  -> Party          -- ^ Counter-party
+  -> Maybe ChoiceId -- ^ Price feed for the underlying
+  -> Token          -- ^ Currency
+  -> Token          -- ^ Underlying
+  -> Value          -- ^ Strike price (in currency)
+  -> Value          -- ^ Amount of underlying tokens per contract
+  -> Value          -- ^ Barrier
+  -> [Timeout]      -- ^ Barrier observation dates
+  -> Timeout        -- ^ Maturity
+  -> Timeout        -- ^ Settlement date
+  -> Contract       -- ^ Covered Call Contract
+barrierOption exerciseType optionType barrierType issuer counterparty priceFeed currency underlying strike ratio barrier observationDates maturity settlement =
+  foldl checkBarrierCondition initialContract observationDates
+  where
+    optionContract = option exerciseType optionType issuer counterparty priceFeed (underlying, ratio) (currency, strike) maturity settlement
+
+    initialContract = initialContract' barrierType
+      where
+        initialContract' DownAndIn  = Close
+        initialContract' DownAndOut = optionContract
+        initialContract' UpAndIn    = Close
+        initialContract' UpAndOut   = optionContract
+
+    checkBarrierCondition continuation timeout =
+      When [checkBarrierCondition' priceFeed barrierType] timeout Close
+      where
+        checkBarrierCondition' :: Maybe ChoiceId -> BarrierType -> Case
+        checkBarrierCondition' Nothing _                  = choose (ChoiceId "Barrier Breach" issuer) continuation optionContract
+        checkBarrierCondition' (Just choiceId) DownAndIn  = chooseOracle choiceId (currency, barrier) ValueGT continuation optionContract
+        checkBarrierCondition' (Just choiceId) DownAndOut = chooseOracle choiceId (currency, barrier) ValueGT continuation Close
+        checkBarrierCondition' (Just choiceId) UpAndIn    = chooseOracle choiceId (currency, barrier) ValueLT continuation optionContract
+        checkBarrierCondition' (Just choiceId) UpAndOut   = chooseOracle choiceId (currency, barrier) ValueLT continuation Close
