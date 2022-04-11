ea8bc872990de03377dae8cc15d00329c95baaca
Jamie Bertram <jamie.bertram@tweag.io>
2022-04-11 08:08:25 -0400

    SCP-3751-disable-darwin

--- release.nix
+++ release.nix
@@ -1,4 +1,5 @@
-{ supportedSystems ? [ "x86_64-linux" "x86_64-darwin" ]
+# TODO re-enable Darwin after Hydra Darwin build issues are resolved.
+{ supportedSystems ? [ "x86_64-linux" /* "x86_64-darwin" */ ]
 , rootsOnly ? false
   # We explicitly pass true here in the GitHub action but don't want to slow down hydra
 , checkMaterialization ? false
