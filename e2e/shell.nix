a3c1e3bdc9bf08c59ae95b238fec5109cf283afd
Shea Levy <shea@shealevy.com>
2022-04-26 10:20:58 -0400

    SCP-3705: Include chromium for playwright in shell

--- e2e/shell.nix
+++ e2e/shell.nix
@@ -1,7 +1,28 @@
-let pkgs = import <nixpkgs> { }; in
-pkgs.mkYarnPackage {
-  src = ./.;
-  shellHook = pkgs.yarn2nix-moretea.linkNodeModulesHook;
-  PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD = true;
-  PLAYWRIGHT_BROWSERS_PATH = 0;
-}
+let
+  inherit (import ../. { }) pkgs;
+
+  browsers = pkgs.runCommand "playwright-browsers" { } ''
+    mkdir -p $out/chromium-${browser-versions.chromium}/chrome-linux
+    ln -sv ${pkgs.chromium}/bin/chromium $out/chromium-${browser-versions.chromium}/chrome-linux/chrome
+
+    mkdir -p $out/ffmpeg-${browser-versions.ffmpeg}
+    ln -sv ${pkgs.ffmpeg}/bin/ffmpeg $out/ffmpeg-${browser-versions.ffmpeg}/ffmpeg-linux
+  '';
+
+  browser-versions = builtins.listToAttrs (map
+    (browser: {
+      name = browser.name;
+      value = browser.revision;
+    })
+    ((builtins.fromJSON (builtins.unsafeDiscardStringContext ((builtins.readFile "${pkg.node_modules}/playwright-core/browsers.json")))).browsers));
+
+  pkg = pkgs.mkYarnPackage
+    {
+      src = ./.;
+      shellHook = pkgs.yarn2nix-moretea.linkNodeModulesHook;
+    } // pkgs.lib.optionalAttrs (! pkgs.stdenv.isDarwin) {
+    PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD = true;
+    PLAYWRIGHT_BROWSERS_PATH = browsers;
+  };
+in
+pkg
