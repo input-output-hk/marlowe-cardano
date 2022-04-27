866bbc6216f164a40f6cb2a5bd97eeff379f8e0b
Tomasz Rybarczyk <tomasz.rybarczyk@iohk.io>
2022-04-27 20:40:53 +0200

    Fix shellcheck warnings

--- marlowe-cli/run-tests.sh
+++ marlowe-cli/run-tests.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 USAGE=\
-'USAGE:
+"USAGE:
   run-tests.sh [OPTIONS] [TEST_NAME]
 
 ARGS:
-  <TEST_NAME> Test to execute. Test name is a part of yaml file from `marlowe-cli/test` dir
-              without `test-` prefix and `.yaml` suffix. Tests are executed in order.
+  <TEST_NAME> Test to execute. Test name is a part of yaml file from \`marlowe-cli/test\` dir
+              without \`test-\` prefix and \`.yaml\` suffix. Tests are executed in order.
               If no tests are provided runner is going to execute the whole suite.
 OPTIONS:
   -m, --testnet-magic <CARDANO_TESTNET_MAGIC>
@@ -14,23 +14,23 @@ OPTIONS:
           Default is 1567.
   -s, --node-socket-path
           Falls back to the $CARDANO_NODE_SOCKET_PATH env variable.
-          We fallback to `./node.socket` when as a least resort.
+          We fallback to \`./node.socket\` when as a least resort.
   -t, --treasury
           Directory which should be used as a store for test suite treasury account.
-          We expect two files there: `payment.skey`, `payment.vkey`
+          We expect two files there: \`payment.skey\`, \`payment.vkey\`
           If given directory is empty this runner will create them.
-          We fallback to $TREASURY env variable and to `./` at the end.
+          We fallback to $TREASURY env variable and to \`./\` at the end.
   -f, --fund
           Whether to fund the testing treasury.
   -b, --build
-          Execute test suite by using `cabal run exe:marlowe-cli`
+          Execute test suite by using \`cabal run exe:marlowe-cli\`
   -w, --wallet-url
-          URL to the cardano-wallet. Default is `http://localhost:8090`
+          URL to the cardano-wallet. Default is \`http://localhost:8090\`
   -p, --pab-url
-          URL to the marlowe-pab server. Default is `http://localhost:9080`
+          URL to the marlowe-pab server. Default is \`http://localhost:9080\`
   -v, --verbose
   -h, --help
-'
+"
 
 set -e
 
@@ -38,7 +38,6 @@ BUILD=
 VERBOSE=
 NODE_SOCKET_PATH=$CARDANO_NODE_SOCKET_PATH
 TESTNET_MAGIC=$CARDANO_TESTNET_MAGIC
-TREASURY=$TREASURY
 WALLET_URL=
 PAB_URL=
 FUND=
@@ -63,7 +62,7 @@ do
       -v | --verbose)                   VERBOSE=1;              shift   ;;
       -h | --help)                      echo "$USAGE";          exit  0 ;;
       --) shift; break ;;
-      *) echo "Unrecognized option: $1" 1>&2; echo $USAGE; exit 1;;
+      *) echo "Unrecognized option: $1" 1>&2; echo "$USAGE"; exit 1;;
     esac
 done
 
@@ -99,7 +98,7 @@ then
 fi
 FAUCET_ADDRESS=$(cardano-cli address build --testnet-magic "$TESTNET_MAGIC" --payment-verification-key-file "$FAUCET_VKEY")
 
-if [[ ! -z "$FUND" ]]; then
+if [[ -n "$FUND" ]]; then
   # Fund the faucet with 5k tADA.
   marlowe-cli util faucet --testnet-magic "$TESTNET_MAGIC"                   \
                           --socket-path "$CARDANO_NODE_SOCKET_PATH" \
@@ -115,10 +114,10 @@ BURN_ADDRESS=addr_test1vqxdw4rlu6krp9fwgwcnld6y84wdahg585vrdy67n5urp9qyts0y7
 # The PAB passphrase must match the `--passphrase` argument of `marlowe-pab`.
 PAB_PASSPHRASE=fixme-allow-pass-per-wallet
 
-if [[ -z "$@" ]]; then
-  TEST_CASES="companion-notifications-for-two-contracts wait refund wallet-failure simple escrow escrow-with-collateral zero-coupon-bond zero-coupon-bond-too-late zero-coupon-bond-immediate-timeout coupon-bond-guaranteed contract-for-differences contract-for-differences-with-oracle swap-of-ada-for-ada follower-non-empty-payouts-initialization follower-notifies-about-payout-redemption"
+if [[ "${#@}" -eq 0 ]]; then
+  TEST_CASES=(companion-notifications-for-two-contracts wait refund wallet-failure simple escrow escrow-with-collateral zero-coupon-bond zero-coupon-bond-too-late zero-coupon-bond-immediate-timeout coupon-bond-guaranteed contract-for-differences contract-for-differences-with-oracle swap-of-ada-for-ada follower-non-empty-payouts-initialization follower-notifies-about-payout-redemption)
 else
-  TEST_CASES="$@"
+  TEST_CASES=("$@")
 fi
 
 if [[ -z "$BUILD" ]];then
@@ -140,10 +139,14 @@ runTest() {
   return $?
 }
 
+if [[ -n "$VERBOSE" ]]; then
+  echo "Running test cases in order: ${TEST_CASES[*]}"
+fi
 
-for TEST_CASE in $TEST_CASES
+
+for TEST_CASE in "${TEST_CASES[@]}"
 do
-  TEST_FILE="./test/test-"$TEST_CASE".yaml"
+  TEST_FILE="./test/test-$TEST_CASE.yaml"
   if [[ ! -e $TEST_FILE ]]; then
       echo "Given test file doesn't exist: $TEST_FILE. Skipping."
       continue
@@ -158,7 +161,7 @@ do
       echo "FAIL"
     fi
   else
-    runTest $TEST_FILE | tee "${TEST_FILE%%.yaml}".log
+    runTest "$TEST_FILE" | tee "${TEST_FILE%%.yaml}".log
   fi
 done
 
