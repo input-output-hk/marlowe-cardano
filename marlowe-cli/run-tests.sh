1dbcb10d02615a75b9116a910754118583521cc2
Dino Morelli <dino.morelli@iohk.io>
2022-05-03 13:56:15 -0400

    SCP-3884 Made some prelim changes to add more switches
    Also reordered the default list of all tests to go from simplest/fastest
    to more complex/slower.

--- marlowe-cli/run-tests.sh
+++ marlowe-cli/run-tests.sh
@@ -20,6 +20,10 @@ OPTIONS:
           We expect two files there: \`payment.skey\`, \`payment.vkey\`
           If given directory is empty this runner will create them.
           We fallback to $TREASURY env variable and to \`./\` at the end.
+  -A, --faucet-addr-file
+          File containing the faucet address
+  -S, --faucet-skey-file
+          File containing the faucet SKEY
   -f, --fund
           Whether to fund the testing treasury.
   -b, --build
@@ -42,8 +46,10 @@ WALLET_URL=
 PAB_URL=
 FUND=
 TREASURY_DIR=$TREASURY
+FAUCET_ADDR=
+FAUCET_SKEY=
 
-PARSED_ARGUMENTS=$(getopt -a -n "marlowe-cli/run-tests" -o "m:s:bvt:fh" --long "testnet-magic:,node-socket-path:,build,verbose,treasury:,fund,wallet-url:,pab-url:,help" -- "$@")
+PARSED_ARGUMENTS=$(getopt -a -n "marlowe-cli/run-tests" -o "m:s:bA:S:vt:fh" --long "testnet-magic:,node-socket-path:,build,faucet-addr-file:,faucet-skey-file:,verbose,treasury:,fund,help" -- "$@")
 VALID_ARGUMENTS=$?
 if [ "$VALID_ARGUMENTS" != "0" ]; then
   echo "$USAGE"
@@ -60,6 +66,8 @@ do
       -p | --pab-url)                   PAB_URL="$2";           shift 2 ;;
       -t | --treasury)                  TREASURY_DIR="$2";      shift 2 ;;
       -b | --build)                     BUILD=1;                shift   ;;
+      -A | --faucet-addr-file)          FAUCET_ADDR=$(cat "$2"); shift 2 ;;
+      -S | --faucet-skey-file)          FAUCET_SKEY="$2";       shift 2 ;;
       -v | --verbose)                   VERBOSE=1;              shift   ;;
       -h | --help)                      echo "$USAGE";          exit  0 ;;
       --) shift; break ;;
@@ -88,7 +96,12 @@ if [[ -z $PAB_URL ]]; then
 fi
 
 if [[ "${#@}" -eq 0 ]]; then
-  TEST_CASES=(companion-notifications-for-two-contracts wait refund wallet-failure simple escrow escrow-with-collateral zero-coupon-bond zero-coupon-bond-too-late zero-coupon-bond-immediate-timeout coupon-bond-guaranteed contract-for-differences contract-for-differences-with-oracle swap-of-ada-for-ada follower-non-empty-payouts-initialization follower-notifies-about-payout-redemption)
+# original test cases
+  # TEST_CASES=(companion-notifications-for-two-contracts wait refund wallet-failure simple escrow escrow-with-collateral zero-coupon-bond zero-coupon-bond-too-late zero-coupon-bond-immediate-timeout coupon-bond-guaranteed contract-for-differences contract-for-differences-with-oracle swap-of-ada-for-ada follower-non-empty-payouts-initialization follower-notifies-about-payout-redemption)
+# reordered in increasing complexity
+  TEST_CASES=(wait refund simple follower-non-empty-payouts-initialization follower-notifies-about-payout-redemption companion-notifications-for-two-contracts wallet-failure escrow escrow-with-collateral zero-coupon-bond zero-coupon-bond-too-late zero-coupon-bond-immediate-timeout coupon-bond-guaranteed contract-for-differences contract-for-differences-with-oracle swap-of-ada-for-ada)
+# ..and without the wait test
+  # TEST_CASES=(refund simple follower-non-empty-payouts-initialization follower-notifies-about-payout-redemption companion-notifications-for-two-contracts wallet-failure escrow escrow-with-collateral zero-coupon-bond zero-coupon-bond-too-late zero-coupon-bond-immediate-timeout coupon-bond-guaranteed contract-for-differences contract-for-differences-with-oracle swap-of-ada-for-ada)
 else
   TEST_CASES=("$@")
 fi
@@ -103,33 +116,35 @@ do
   fi
 done
 
-FAUCET_SKEY="$TREASURY_DIR"/payment.skey
-FAUCET_VKEY="$TREASURY_DIR"/payment.vkey
-
 # Create the payment signing and verification keys if they do not already exist.
 if [[ ! -e "$FAUCET_SKEY" ]]
 then
+  FAUCET_SKEY="$TREASURY_DIR"/payment.skey
+  FAUCET_VKEY="$TREASURY_DIR"/payment.vkey
+
   echo "CREATE"
   cardano-cli address key-gen --signing-key-file "$FAUCET_SKEY"      \
                               --verification-key-file "$FAUCET_VKEY"
+  # We only need to generate this if we created the wallet keys just now.
+  FAUCET_ADDR=$(cardano-cli address build --testnet-magic "$TESTNET_MAGIC" --payment-verification-key-file "$FAUCET_VKEY")
 fi
-FAUCET_ADDRESS=$(cardano-cli address build --testnet-magic "$TESTNET_MAGIC" --payment-verification-key-file "$FAUCET_VKEY")
 
 if [[ -n "$FUND" ]]; then
   # Fund the faucet with 5k tADA.
-  marlowe-cli util faucet --testnet-magic "$TESTNET_MAGIC"                   \
+  marlowe-cli util faucet --testnet-magic "$TESTNET_MAGIC"          \
                           --socket-path "$CARDANO_NODE_SOCKET_PATH" \
                           --out-file /dev/null                      \
                           --submit 600                              \
                           --lovelace 5000000000                     \
-                          "$FAUCET_ADDRESS"                         \
+                          "$FAUCET_ADDR"                            \
   > /dev/null
 fi
 
 BURN_ADDRESS=addr_test1vqxdw4rlu6krp9fwgwcnld6y84wdahg585vrdy67n5urp9qyts0y7
 
 # The PAB passphrase must match the `--passphrase` argument of `marlowe-pab`.
-PAB_PASSPHRASE=fixme-allow-pass-per-wallet
+# PAB_PASSPHRASE=fixme-allow-pass-per-wallet
+PAB_PASSPHRASE="pab1234567890"  # FIXME
 
 if [[ -z "$BUILD" ]];then
   EXEC="marlowe-cli"
@@ -137,13 +152,14 @@ else
   EXEC="cabal run exe:marlowe-cli"
 fi
 runTest() {
-  $EXEC -- test contracts                         \
+  # $EXEC -- test contracts                         \
+  time $EXEC test contracts                         \
         --testnet-magic "$TESTNET_MAGIC"          \
         --socket-path "$NODE_SOCKET_PATH"         \
         --wallet-url "$WALLET_URL"                \
         --pab-url "$PAB_URL"                      \
         --faucet-key "$FAUCET_SKEY"               \
-        --faucet-address "$FAUCET_ADDRESS"        \
+        --faucet-address "$FAUCET_ADDR"           \
         --burn-address "$BURN_ADDRESS"            \
         --passphrase "$PAB_PASSPHRASE"            \
          "$1"
