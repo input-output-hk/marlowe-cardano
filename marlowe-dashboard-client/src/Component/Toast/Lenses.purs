5958be156b7e08fd40cf4757cdfa832ab881da05
Hernan Rajchert <hrajchert@gmail.com>
2022-05-06 12:04:58 -0300

    SCP-3782 Initial refactor of Toast to add stack information

--- marlowe-dashboard-client/src/Component/Toast/Lenses.purs
+++ marlowe-dashboard-client/src/Component/Toast/Lenses.purs
@@ -1,29 +1,24 @@
-module Component.Toast.Lenses
-  ( _mToast
-  , _toastMessage
-  , _expanded
-  , _timeoutSubscription
-  ) where
+module Component.Toast.Lenses where
 
 import Prologue
 
-import Component.Toast.Types (State, ToastMessage, ToastState)
+import Component.Toast.Types (State, ToastEntry, ToastIndex(..), ToastMessage)
 import Data.Lens (Lens', _Just)
 import Data.Lens.Record (prop)
 import Data.Lens.Traversal (Traversal')
+import Data.List (List)
+import Data.Map (Map)
 import Halogen (SubscriptionId)
 import Type.Proxy (Proxy(..))
 
--- TODO: when we upgrade to 0.14 change this to AffineTraversal
-_mToast :: Lens' State (Maybe ToastState)
-_mToast = prop (Proxy :: _ "mToast")
+_toasts :: Lens' State (List ToastEntry)
+_toasts = prop (Proxy :: _ "toasts")
 
-_toastMessage :: Traversal' State ToastMessage
-_toastMessage = _mToast <<< _Just <<< prop (Proxy :: _ "message")
+-- _toastMessage :: Traversal' State ToastMessage
+-- _toastMessage = _mToast <<< _Just <<< prop (Proxy :: _ "message")
 
-_expanded :: Traversal' State Boolean
-_expanded = prop (Proxy :: _ "mToast") <<< _Just <<< prop
-  (Proxy :: _ "expanded")
+_expanded :: Lens' State (Maybe ToastIndex)
+_expanded = prop (Proxy :: _ "expanded")
 
-_timeoutSubscription :: Traversal' State SubscriptionId
-_timeoutSubscription = prop (Proxy :: _ "timeoutSubscription") <<< _Just
+_timeoutSubscriptions :: Lens' State (Map ToastIndex SubscriptionId)
+_timeoutSubscriptions = prop (Proxy :: _ "timeoutSubscriptions")
