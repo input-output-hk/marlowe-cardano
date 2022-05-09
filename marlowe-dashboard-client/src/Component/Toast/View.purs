0507cc43a30a72db719bc1e95c2efdb408a2467b
Hernan Rajchert <hrajchert@gmail.com>
2022-05-09 19:44:25 -0300

    SCP-3782 Add expanded view

--- marlowe-dashboard-client/src/Component/Toast/View.purs
+++ marlowe-dashboard-client/src/Component/Toast/View.purs
@@ -3,25 +3,16 @@ module Component.Toast.View (render) where
 import Prologue hiding (div)
 
 import Component.Icons (Icon(..), icon, icon_)
-import Component.Toast.Types
-  ( Action(..)
-  , State
-  , ToastEntry
-  , ToastMessage
-  , indexRef
-  )
-import Css as Css
-import Data.Lens (preview)
-import Data.List (List(..), (:))
+import Component.Toast.Types (Action(..), State, ToastEntry, indexRef)
+import Data.List (List(..))
 import Data.List as List
 import Data.Maybe (fromMaybe)
-import Data.Tuple.Nested (type (/\), (/\))
-import Halogen (AttrName(..), ElemName(..), RefLabel(..))
+import Data.Tuple.Nested ((/\))
 import Halogen.Css (classNames)
-import Halogen.HTML (HTML, a, attr, div, div_, span, text)
+import Halogen.HTML (HTML, a, div, div_, span, text)
 import Halogen.HTML.Elements.Keyed as HK
 import Halogen.HTML.Events.Extra (onClick_)
-import Halogen.HTML.Properties (id, ref)
+import Halogen.HTML.Properties (id)
 import Halogen.HTML.Properties.ARIA (describedBy, labelledBy, role)
 
 render
@@ -32,8 +23,23 @@ render state = case state.toasts of
   Nil -> div_ []
   toasts ->
     let
-      renderedToasts = renderToast <$> List.toUnfoldable toasts
+      renderedToasts = List.toUnfoldable toasts <#>
+        \entry ->
+          let
+            toastElem =
+              if entry.expanded then
+                renderExpandedToast entry
+              else
+                renderCollapsedToast entry
+          in
+            -- See note on HK.div
+            (indexRef "toast-message" entry.index) /\ toastElem
     in
+      -- We used the Keyed version of this div to tell Halogen to reutilize
+      -- the same nodes when they are moved around in the array. If we don't
+      -- do this there is some problem with animations (as the elements come and go)
+      -- and with the ref property
+      -- check issue https://github.com/purescript-halogen/purescript-halogen/issues/423
       HK.div
         [ classNames
             [ "fixed"
@@ -48,11 +54,11 @@ render state = case state.toasts of
         ]
         renderedToasts
 
-renderToast
+renderCollapsedToast
   :: forall p
    . ToastEntry
-  -> String /\ HTML p Action
-renderToast { index, message: toast } =
+  -> HTML p Action
+renderCollapsedToast { index, message: toast } =
   let
     readMore = case toast.longDescription of
       Nothing -> div_ []
@@ -61,11 +67,13 @@ renderToast { index, message: toast } =
           [ classNames [ "ml-4", "font-semibold", "underline", "flex-shrink-0" ]
           ]
           [ a
-              [ onClick_ $ ExpandToast index ]
+              [ onClick_ $ ToggleExpanded index ]
               [ text "Read more" ]
           ]
+    shortMessageId = indexRef "toast-short-message" index
+
   in
-    (indexRef "toast-message" index) /\ div
+    div
       [ classNames
           [ "px-4"
           , "py-2"
@@ -76,15 +84,15 @@ renderToast { index, message: toast } =
           , "min-w-90pc"
           , "max-w-90pc"
           , "sm:min-w-sm"
+          , "sm:max-w-sm"
           , "flex"
           , "justify-between"
           , toast.bgColor
           , toast.textColor
           ]
-      , ref $ RefLabel $ indexRef "toast-message" index
-      , attr (AttrName "data-custom-ref") $ indexRef "toast-message" index
+      , id $ indexRef "toast-message" index
       , role $ show toast.role
-      , labelledBy $ indexRef "toast-short-message" index
+      , labelledBy shortMessageId
       ]
       [ div [ classNames [ "flex-grow", "flex", "overflow-hidden" ] ]
           [ icon toast.icon [ "mr-2", toast.iconColor ]
@@ -95,7 +103,7 @@ renderToast { index, message: toast } =
                   , "whitespace-nowrap"
                   , "overflow-hidden"
                   ]
-              , id "toast-short-message"
+              , id shortMessageId
               ]
               [ text toast.shortDescription ]
           ]
@@ -107,109 +115,78 @@ renderToast { index, message: toast } =
           [ icon_ Close ]
       ]
 
--- FIXME: Delete
-renderExpanded
+renderExpandedToast
   :: forall p
    . ToastEntry
   -> HTML p Action
-renderExpanded { index: toastIndex, message: toast } =
-  div
-    [ classNames $ Css.cardOverlay true ]
-    [ div
-        [ classNames (Css.card true)
-        , role $ show toast.role
-        , labelledBy "toast-short-message"
-        , describedBy "toast-long-message"
+renderExpandedToast { index, message: toast } =
+  let
+    readLess =
+      div
+        [ classNames [ "ml-4", "font-semibold", "underline", "flex-shrink-0" ]
         ]
         [ a
-            [ classNames [ "absolute", "top-4", "right-4", toast.textColor ]
-            , onClick_ $ CloseToast toastIndex
-            ]
-            [ icon_ Close ]
-        , div
-            [ classNames
-                [ "flex"
-                , "font-semibold"
-                , "px-5"
-                , "py-4"
-                , toast.bgColor
-                , toast.textColor
-                ]
-            ]
-            [ icon toast.icon [ "mr-2", toast.iconColor ]
-            , span [ id "toast-short-message" ] [ text toast.shortDescription ]
-            ]
-        , div
-            [ classNames [ "px-5", "pb-6", "pt-3", "md:pb-8" ]
-            , id "toast-long-message"
-            ]
-            [ text $ fromMaybe "" toast.longDescription
-            ]
+            [ onClick_ $ ToggleExpanded index ]
+            [ text "Read less" ]
         ]
-    ]
+    shortMessageId = indexRef "toast-short-message" index
+    longMessageId = indexRef "toast-long-message" index
 
--- FIXME: Delete
-renderCollapsed
-  :: forall p
-   . ToastEntry
-  -> HTML p Action
-renderCollapsed { index: toastIndex, message: toast } =
-  let
-    readMore = case toast.longDescription of
-      Nothing -> div_ []
-      Just _ ->
-        div
-          [ classNames [ "ml-4", "font-semibold", "underline", "flex-shrink-0" ]
+    header = div
+      [ classNames
+          [ "px-4"
+          , "py-2"
+          , "flex"
+          , "justify-between"
+          , toast.bgColor
+          , toast.textColor
           ]
-          [ a
-              [ onClick_ $ ExpandToast toastIndex ]
-              [ text "Read more" ]
+      , role $ show toast.role
+      , labelledBy shortMessageId
+      , describedBy longMessageId
+      ]
+      [ div [ classNames [ "flex-grow", "flex", "overflow-hidden" ] ]
+          [ icon toast.icon [ "mr-2", toast.iconColor ]
+          , span
+              [ classNames
+                  [ "font-semibold"
+                  , "overflow-ellipsis"
+                  , "whitespace-nowrap"
+                  , "overflow-hidden"
+                  ]
+              , id shortMessageId
+              ]
+              [ text toast.shortDescription ]
           ]
+      , readLess
+      , a
+          [ classNames [ "ml-2", "leading-none", toast.textColor ]
+          , onClick_ $ AnimateCloseToast index
+          ]
+          [ icon_ Close ]
+      ]
+    body = div
+      [ classNames [ "px-5", "pb-6", "pt-3", "md:pb-8", "bg-white" ]
+      ]
+      [ text $ fromMaybe "" toast.longDescription
+      ]
   in
     div
       [ classNames
-          [ "fixed"
-          , "bottom-6"
-          , "md:bottom-10"
-          , "left-0"
-          , "right-0"
+          [ "rounded"
+          , "overflow-hidden"
+          , "mb-3"
+          , "md:mb-6"
+          , "shadow-lg"
+          , "min-w-90pc"
+          , "max-w-90pc"
+          , "sm:min-w-sm"
+          , "sm:max-w-sm"
           , "flex"
-          , "justify-center"
-          , "z-50"
+          , "flex-col"
           ]
+      , id $ indexRef "toast-message" index
       ]
-      [ div
-          [ classNames
-              [ "px-4"
-              , "py-2"
-              , "rounded"
-              , "shadow-lg"
-              , "min-w-90pc"
-              , "max-w-90pc"
-              , "sm:min-w-sm"
-              , "flex"
-              , "justify-between"
-              , "animate-from-below"
-              , toast.bgColor
-              , toast.textColor
-              ]
-          , ref $ RefLabel "collapsed-toast"
-          , role $ show toast.role
-          , labelledBy "toast-short-message"
-          ]
-          [ div [ classNames [ "flex", "overflow-hidden" ] ]
-              [ icon toast.icon [ "mr-2", toast.iconColor ]
-              , span
-                  [ classNames
-                      [ "font-semibold"
-                      , "overflow-ellipsis"
-                      , "whitespace-nowrap"
-                      , "overflow-hidden"
-                      ]
-                  , id "toast-short-message"
-                  ]
-                  [ text toast.shortDescription ]
-              ]
-          , readMore
-          ]
+      [ header
+      , body
       ]
