55c9d2313ef394dd5f51dc3d426f379d3577dca7
Jamie Bertram <jamie.bertram@tweag.io>
2022-05-12 15:44:13 -0400

    Remove git dependency in marlowe-cli build

--- marlowe-cli/app/Main.hs
+++ marlowe-cli/app/Main.hs
@@ -10,21 +10,12 @@
 --
 -----------------------------------------------------------------------------
 
-
-{-# LANGUAGE CPP               #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell   #-}
-
-
 module Main (
 -- * Entry Point
   main
 ) where
 
 
-import Cardano.Config.Git.RevFromGit (gitRevFromGit)
-import Data.Maybe (fromMaybe)
-import Data.Text (pack, strip, unpack)
 import Data.Version (showVersion)
 import Language.Marlowe.CLI.Command (runCLI)
 import Paths_marlowe_cli (version)
@@ -32,16 +23,4 @@ import Paths_marlowe_cli (version)
 
 -- | Run the Marlow CLI tool.
 main :: IO () -- ^ Action to run the tool.
-main =
-  runCLI
-    $ showVersion version <> fromMaybe mempty fromGit
-
-
-fromGit :: Maybe String
-#if defined(arm_HOST_ARCH)
-  -- cross compiling to arm fails; due to a linker bug
-fromGit = Nothing
-#else
-fromGit = Just ((\v -> if null v then mempty else " @ " <> v) . unpack . strip $ pack $(gitRevFromGit))
-#endif
-
+main = runCLI $ showVersion version
