9dbcf75c2b44b7bcca7ceab483dd1a424c527968
Jamie Bertram <jamie.bertram@tweag.io>
2022-05-13 09:59:36 -0400

    move mkIf inside attrset

--- nix/pkgs/haskell/haskell.nix
+++ nix/pkgs/haskell/haskell.nix
@@ -119,117 +119,123 @@ let
         }
       )
       ({ pkgs, config, ... }: {
-        packages = lib.mkIf (pkgs.stdenv.hostPlatform.isDarwin)
+        packages =
+          let
+            mkIfDarwin = lib.mkIf (pkgs.stdenv.hostPlatform.isDarwin);
+          in
           {
-            cardano-config.components.library.build-tools = [ pkgs.buildPackages.buildPackages.gitReallyMinimal ];
-            marlowe-cli.components.exes. marlowe-cli.build-tools = [ pkgs.buildPackages.buildPackages.gitReallyMinimal ];
-          } // {
+            # These libraries rely on a TemplateHaskell splice that requires
+            # git to be in the path at build time. This only seems to affect
+            # Darwin builds, and including them on Linux breaks lorri, so we
+            # only add these options when building on Darwin.
+            cardano-config.components.library.build-tools = mkIfDarwin [ pkgs.buildPackages.buildPackages.gitReallyMinimal ];
+            marlowe-cli.components.exes. marlowe-cli.build-tools = mkIfDarwin [ pkgs.buildPackages.buildPackages.gitReallyMinimal ];
 
-          # See https://github.com/input-output-hk/plutus/issues/1213 and
-          # https://github.com/input-output-hk/plutus/pull/2865.
-          marlowe.doHaddock = deferPluginErrors;
-          marlowe.flags.defer-plugin-errors = deferPluginErrors;
+            # See https://github.com/input-output-hk/plutus/issues/1213 and
+            # https://github.com/input-output-hk/plutus/pull/2865.
+            marlowe.doHaddock = deferPluginErrors;
+            marlowe.flags.defer-plugin-errors = deferPluginErrors;
 
-          plutus-contract.doHaddock = deferPluginErrors;
-          plutus-contract.flags.defer-plugin-errors = deferPluginErrors;
+            plutus-contract.doHaddock = deferPluginErrors;
+            plutus-contract.flags.defer-plugin-errors = deferPluginErrors;
 
-          plutus-use-cases.doHaddock = deferPluginErrors;
-          plutus-use-cases.flags.defer-plugin-errors = deferPluginErrors;
+            plutus-use-cases.doHaddock = deferPluginErrors;
+            plutus-use-cases.flags.defer-plugin-errors = deferPluginErrors;
 
-          plutus-ledger.doHaddock = deferPluginErrors;
-          plutus-ledger.flags.defer-plugin-errors = deferPluginErrors;
+            plutus-ledger.doHaddock = deferPluginErrors;
+            plutus-ledger.flags.defer-plugin-errors = deferPluginErrors;
 
-          # Packages we just don't want docs for
-          plutus-benchmark.doHaddock = false;
-          # FIXME: Haddock mysteriously gives a spurious missing-home-modules warning
-          plutus-tx-plugin.doHaddock = false;
+            # Packages we just don't want docs for
+            plutus-benchmark.doHaddock = false;
+            # FIXME: Haddock mysteriously gives a spurious missing-home-modules warning
+            plutus-tx-plugin.doHaddock = false;
 
-          # Fix missing executables on the paths of the test runners. This is arguably
-          # a bug, and the fix is a bit of a hack.
-          marlowe.components.tests.marlowe-test.preCheck = ''
-            PATH=${lib.makeBinPath [ z3 ]}:$PATH
-          '';
+            # Fix missing executables on the paths of the test runners. This is arguably
+            # a bug, and the fix is a bit of a hack.
+            marlowe.components.tests.marlowe-test.preCheck = ''
+              PATH=${lib.makeBinPath [ z3 ]}:$PATH
+            '';
 
-          marlowe-contracts.components.tests.marlowe-contracts-test.preCheck = ''
-            PATH=${lib.makeBinPath [ z3 ]}:$PATH
-          '';
+            marlowe-contracts.components.tests.marlowe-contracts-test.preCheck = ''
+              PATH=${lib.makeBinPath [ z3 ]}:$PATH
+            '';
 
-          # Relies on cabal-doctest, just turn it off in the Nix build
-          prettyprinter-configurable.components.tests.prettyprinter-configurable-doctest.buildable = lib.mkForce false;
+            # Relies on cabal-doctest, just turn it off in the Nix build
+            prettyprinter-configurable.components.tests.prettyprinter-configurable-doctest.buildable = lib.mkForce false;
 
-          plutus-core.components.benchmarks.update-cost-model = {
-            build-tools = r-packages;
-            # Seems to be broken on darwin for some reason
-            platforms = lib.platforms.linux;
+            plutus-core.components.benchmarks.update-cost-model = {
+              build-tools = r-packages;
+              # Seems to be broken on darwin for some reason
+              platforms = lib.platforms.linux;
+            };
+
+            plutus-core.components.benchmarks.cost-model-test = {
+              build-tools = r-packages;
+              # Seems to be broken on darwin for some reason
+              platforms = lib.platforms.linux;
+            };
+
+            marlowe-actus.components.exes.marlowe-shiny = {
+              build-tools = r-packages;
+              # Seems to be broken on darwin for some reason
+              platforms = lib.platforms.linux;
+            };
+
+            # The marlowe-actus tests depend on external data which is
+            # provided from Nix (as niv dependency)
+            marlowe-actus.components.tests.marlowe-actus-test.preCheck = ''
+              export ACTUS_TEST_DATA_DIR=${actus-tests}/tests/
+            '';
+
+
+            # Note: The following two statements say that these tests should
+            # _only_ run on linux. In actual fact we just don't want them
+            # running on the 'mac-mini' instances, because these tests time out
+            # there. In an ideal world this would be reflected here more
+            # accurately.
+            # TODO: Resolve this situation in a better way.
+            marlowe.components.tests.marlowe-test-long-running = {
+              platforms = lib.platforms.linux;
+            };
+
+            plutus-pab.components.tests.plutus-pab-test-full-long-running = {
+              platforms = lib.platforms.linux;
+            };
+
+            # Broken due to warnings, unclear why the setting that fixes this for the build doesn't work here.
+            iohk-monitoring.doHaddock = false;
+
+            # Werror everything. This is a pain, see https://github.com/input-output-hk/haskell.nix/issues/519
+            plutus-core.ghcOptions = [ "-Werror" ];
+            marlowe.ghcOptions = [ "-Werror" ];
+            marlowe-symbolic.ghcOptions = [ "-Werror" ];
+            marlowe-actus.ghcOptions = [ "-Werror" ];
+            marlowe-playground-server.ghcOptions = [ "-Werror" ];
+            marlowe-dashboard-server.ghcOptions = [ "-Werror" ];
+            marlowe-contract.ghcOptions = [ "-Werror" ];
+            playground-common.ghcOptions = [ "-Werror" ];
+            plutus-contract.ghcOptions = [ "-Werror" ];
+            plutus-ledger.ghcOptions = [ "-Werror" ];
+            plutus-ledger-api.ghcOptions = [ "-Werror" ];
+            plutus-playground-server.ghcOptions = [ "-Werror" ];
+            plutus-pab.ghcOptions = [ "-Werror" ];
+            plutus-tx.ghcOptions = [ "-Werror" ];
+            plutus-tx-plugin.ghcOptions = [ "-Werror" ];
+            plutus-doc.ghcOptions = [ "-Werror" ];
+            plutus-use-cases.ghcOptions = [ "-Werror" ];
+
+            # External package settings
+
+            inline-r.ghcOptions = [ "-XStandaloneKindSignatures" ];
+
+            # Honestly not sure why we need this, it has a mysterious unused dependency on "m"
+            # This will go away when we upgrade nixpkgs and things use ieee754 anyway.
+            ieee.components.library.libs = lib.mkForce [ ];
+
+            # See https://github.com/input-output-hk/iohk-nix/pull/488
+            cardano-crypto-praos.components.library.pkgconfig = lib.mkForce [ [ libsodium-vrf ] ];
+            cardano-crypto-class.components.library.pkgconfig = lib.mkForce [ [ libsodium-vrf ] ];
           };
-
-          plutus-core.components.benchmarks.cost-model-test = {
-            build-tools = r-packages;
-            # Seems to be broken on darwin for some reason
-            platforms = lib.platforms.linux;
-          };
-
-          marlowe-actus.components.exes.marlowe-shiny = {
-            build-tools = r-packages;
-            # Seems to be broken on darwin for some reason
-            platforms = lib.platforms.linux;
-          };
-
-          # The marlowe-actus tests depend on external data which is
-          # provided from Nix (as niv dependency)
-          marlowe-actus.components.tests.marlowe-actus-test.preCheck = ''
-            export ACTUS_TEST_DATA_DIR=${actus-tests}/tests/
-          '';
-
-
-          # Note: The following two statements say that these tests should
-          # _only_ run on linux. In actual fact we just don't want them
-          # running on the 'mac-mini' instances, because these tests time out
-          # there. In an ideal world this would be reflected here more
-          # accurately.
-          # TODO: Resolve this situation in a better way.
-          marlowe.components.tests.marlowe-test-long-running = {
-            platforms = lib.platforms.linux;
-          };
-
-          plutus-pab.components.tests.plutus-pab-test-full-long-running = {
-            platforms = lib.platforms.linux;
-          };
-
-          # Broken due to warnings, unclear why the setting that fixes this for the build doesn't work here.
-          iohk-monitoring.doHaddock = false;
-
-          # Werror everything. This is a pain, see https://github.com/input-output-hk/haskell.nix/issues/519
-          plutus-core.ghcOptions = [ "-Werror" ];
-          marlowe.ghcOptions = [ "-Werror" ];
-          marlowe-symbolic.ghcOptions = [ "-Werror" ];
-          marlowe-actus.ghcOptions = [ "-Werror" ];
-          marlowe-playground-server.ghcOptions = [ "-Werror" ];
-          marlowe-dashboard-server.ghcOptions = [ "-Werror" ];
-          marlowe-contract.ghcOptions = [ "-Werror" ];
-          playground-common.ghcOptions = [ "-Werror" ];
-          plutus-contract.ghcOptions = [ "-Werror" ];
-          plutus-ledger.ghcOptions = [ "-Werror" ];
-          plutus-ledger-api.ghcOptions = [ "-Werror" ];
-          plutus-playground-server.ghcOptions = [ "-Werror" ];
-          plutus-pab.ghcOptions = [ "-Werror" ];
-          plutus-tx.ghcOptions = [ "-Werror" ];
-          plutus-tx-plugin.ghcOptions = [ "-Werror" ];
-          plutus-doc.ghcOptions = [ "-Werror" ];
-          plutus-use-cases.ghcOptions = [ "-Werror" ];
-
-          # External package settings
-
-          inline-r.ghcOptions = [ "-XStandaloneKindSignatures" ];
-
-          # Honestly not sure why we need this, it has a mysterious unused dependency on "m"
-          # This will go away when we upgrade nixpkgs and things use ieee754 anyway.
-          ieee.components.library.libs = lib.mkForce [ ];
-
-          # See https://github.com/input-output-hk/iohk-nix/pull/488
-          cardano-crypto-praos.components.library.pkgconfig = lib.mkForce [ [ libsodium-vrf ] ];
-          cardano-crypto-class.components.library.pkgconfig = lib.mkForce [ [ libsodium-vrf ] ];
-        };
       })
     ] ++ lib.optional enableHaskellProfiling {
       enableLibraryProfiling = true;
