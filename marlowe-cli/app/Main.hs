306ab2d02bbd7c20a70c19b284a4c4e897172a59
Jamie Bertram <jamie.bertram@tweag.io>
2022-05-13 08:57:27 -0400

    Revert "Remove git dependency in marlowe-cli build"
    This reverts commit f5700a0cf1bc3b670f94d44f064ecdff38b6581f.

--- marlowe-cli/app/Main.hs
+++ marlowe-cli/app/Main.hs
@@ -10,12 +10,21 @@
 --
 -----------------------------------------------------------------------------
 
+
+{-# LANGUAGE CPP               #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
+
+
 module Main (
 -- * Entry Point
   main
 ) where
 
 
+import Cardano.Config.Git.RevFromGit (gitRevFromGit)
+import Data.Maybe (fromMaybe)
+import Data.Text (pack, strip, unpack)
 import Data.Version (showVersion)
 import Language.Marlowe.CLI.Command (runCLI)
 import Paths_marlowe_cli (version)
@@ -23,4 +32,16 @@ import Paths_marlowe_cli (version)
 
 -- | Run the Marlow CLI tool.
 main :: IO () -- ^ Action to run the tool.
-main = runCLI $ showVersion version
+main =
+  runCLI
+    $ showVersion version <> fromMaybe mempty fromGit
+
+
+fromGit :: Maybe String
+#if defined(arm_HOST_ARCH)
+  -- cross compiling to arm fails; due to a linker bug
+fromGit = Nothing
+#else
+fromGit = Just ((\v -> if null v then mempty else " @ " <> v) . unpack . strip $ pack $(gitRevFromGit))
+#endif
+
