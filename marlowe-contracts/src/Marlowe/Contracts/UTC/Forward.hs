935f3808efda41f69a566a0a2dbae1ee4349beb6
Yves Hauser <yves.hauser@gmail.com>
2022-05-19 14:49:49 +0200

    SCP-3942: Added forward contract

---
+++ marlowe-contracts/src/Marlowe/Contracts/UTC/Forward.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Marlowe.Contracts.UTC.Forward
+  ( forward
+  )
+where
+
+import Data.Time.Clock (UTCTime)
+import Language.Marlowe.Extended
+import qualified Marlowe.Contracts.Forward as C
+import Marlowe.Contracts.UTC.Common
+
+-- A forward contract is contract between to parties A and B to buy or sell an
+-- underlying asset at a specified price on a future date
+forward ::
+     Party    -- ^ Party A
+  -> Token    -- ^ Token A
+  -> Value    -- ^ Value A
+  -> UTCTime  -- ^ Deposit timeout A
+  -> Party    -- ^ Party B
+  -> Token    -- ^ Token B
+  -> Value    -- ^ Value B:ta
+  -> UTCTime  -- ^ Deposit timeout B
+  -> UTCTime  -- ^ Delivery date
+  -> Contract -- ^ Forward contract
+forward partyA tokenA valueA timeoutA partyB tokenB valueB timeoutB deliveryDate =
+  C.forward partyA tokenA valueA (toTimeout timeoutA) partyB tokenB valueB (toTimeout timeoutB) (toTimeout deliveryDate)
+
