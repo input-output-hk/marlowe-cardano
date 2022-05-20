0fbd80d08bc993e88545ac071f50dc8863872e0c
Jamie Bertram <jamie.bertram@tweag.io>
2022-05-20 15:24:10 -0400

    Refactor and use all serializable types

--- marlowe-runtime/src/Language/Marlowe/Runtime/Chain.hs
+++ marlowe-runtime/src/Language/Marlowe/Runtime/Chain.hs
@@ -1,34 +1,43 @@
 {-# LANGUAGE BlockArguments      #-}
+{-# LANGUAGE FlexibleContexts    #-}
 {-# LANGUAGE GADTs               #-}
 {-# LANGUAGE RankNTypes          #-}
 {-# LANGUAGE RecordWildCards     #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module Language.Marlowe.Runtime.Chain where
 
-import Cardano.Api (Block (..), BlockHeader (BlockHeader), BlockInMode (..), CardanoMode, ChainPoint,
-                    ChainSyncClient (..), ChainTip, EraInMode (..), GenesisParameters (..),
-                    LocalChainSyncClient (LocalChainSyncClient), LocalNodeClientProtocols (..), LocalNodeConnectInfo,
-                    QueryInEra (..), QueryInMode (..), QueryInShelleyBasedEra (..), ShelleyBasedEra (..), SlotNo (..),
-                    Tx, TxBody (..), TxBodyContent (..), TxIn (..), TxIx (..), TxMetadataInEra (..),
-                    TxMetadataJsonSchema (..), TxMintValue (..), TxOut (..), TxOutDatum (..), TxValidityLowerBound (..),
-                    TxValidityUpperBound (..), ValueNestedBundle (..), ValueNestedRep (..), connectToLocalNode,
-                    getTxBody, getTxId, metadataToJson, queryNodeLocalState, txOutValueToValue, valueToNestedRep)
+import Cardano.Api (Address, AddressInEra (..), Block (..), BlockHeader (..), BlockInMode (..), BlockNo (..),
+                    CardanoMode, ChainPoint (..), ChainSyncClient (..), ChainTip (..), CtxTx, EraInMode (..),
+                    GenesisParameters (..), LocalChainSyncClient (..), LocalNodeClientProtocols (..),
+                    LocalNodeConnectInfo, QueryInEra (..), QueryInMode (..), QueryInShelleyBasedEra (..),
+                    ShelleyBasedEra (..), SlotNo (..), Tx, TxBody (..), TxBodyContent (..), TxId, TxIn (..), TxIx (..),
+                    TxMetadataInEra (..), TxMetadataJsonSchema (..), TxMintValue (..), TxOut (..), TxOutDatum (..),
+                    TxValidityLowerBound (..), TxValidityUpperBound (..), ValueNestedBundle (..), ValueNestedRep (..),
+                    connectToLocalNode, getTxBody, getTxId, metadataToJson, queryNodeLocalState, txOutValueToValue,
+                    valueToNestedRep)
 import Cardano.Api.ChainSync.Client (ClientStIdle (..), ClientStIntersect (..), ClientStNext (..))
+import Cardano.Api.Shelley (AddressTypeInEra (ByronAddressInAnyEra, ShelleyAddressInEra), Hash (..))
 import Control.Monad.IO.Class (MonadIO (..))
-import Data.Foldable (for_)
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 import Data.Time (addUTCTime, nominalDiffTimeToSeconds, secondsToNominalDiffTime)
-import Language.Marlowe.Runtime.Chain.Types (MarloweChainEvent (..), MarloweChainSyncClient, MarloweTx (..),
-                                             MarloweTxOut (..), MarloweValidityInterval (MarloweValidityInterval))
+import Language.Marlowe.Runtime.Chain.Types (MarloweBlockHeader (..), MarloweBlockHeaderHash (..), MarloweBlockNo (..),
+                                             MarloweChainEvent (..),
+                                             MarloweChainPoint (MarloweChainPoint, MarloweChainPointAtGenesis),
+                                             MarloweChainSyncClient,
+                                             MarloweChainTip (MarloweChainTip, MarloweChainTipAtGenesis),
+                                             MarlowePolicyId (..), MarloweSlotNo (..), MarloweTx (..), MarloweTxId (..),
+                                             MarloweTxIn (..), MarloweTxOut (..), MarloweTxs (..),
+                                             MarloweValidityInterval (..))
+import Unlift (MonadUnlift, Unlift (runUnlift), askUnlift)
+import Unsafe.Coerce (unsafeCoerce)
 
 runMarloweChainSyncClient
   :: MonadIO m
   => LocalNodeConnectInfo CardanoMode
-  -> (GenesisParameters -> [ChainPoint] -> MarloweChainSyncClient IO ())
-  -> [ChainPoint]
-  -> m (Either String ())
-runMarloweChainSyncClient connectionInfo getClient startingPoints = do
+  -> (GenesisParameters -> m (MarloweChainSyncClient IO ()))
+  -> m (Either String (m ()))
+runMarloweChainSyncClient connectionInfo getClient = do
   mGenesisParams <- liftIO $ queryNodeLocalState connectionInfo Nothing
     $ QueryInEra AlonzoEraInCardanoMode
     $ QueryInShelleyBasedEra  ShelleyBasedEraAlonzo QueryGenesisParameters
@@ -36,19 +45,22 @@ runMarloweChainSyncClient connectionInfo getClient startingPoints = do
     Left err -> pure $ Left $ show err
     Right (Left err) -> pure $ Left $ show err
     Right (Right genesisParams) -> do
+      client <- getClient genesisParams
       let
-        client = getClient genesisParams startingPoints
         protocols = LocalNodeClientProtocols
           { localChainSyncClient = LocalChainSyncClient client
           , localTxSubmissionClient = Nothing
           , localStateQueryClient = Nothing
           }
-      liftIO $ pure <$> connectToLocalNode connectionInfo protocols
+      pure $ Right $ liftIO $ connectToLocalNode connectionInfo protocols
 
-stdOutMarloweChainSyncClient :: GenesisParameters -> [ChainPoint] -> MarloweChainSyncClient IO ()
-stdOutMarloweChainSyncClient genesisParams startingPoints = marloweChainSyncClient
-  genesisParams
+stdOutMarloweChainSyncClient
+  :: [ChainPoint]
+  -> GenesisParameters
+  -> IO (MarloweChainSyncClient IO ())
+stdOutMarloweChainSyncClient startingPoints genesisParams = marloweChainSyncClient
   startingPoints
+  genesisParams
   onStart
   (pure True)
   onEvent
@@ -58,79 +70,102 @@ stdOutMarloweChainSyncClient genesisParams startingPoints = marloweChainSyncClie
       putStrLn "Start"
       putStrLn $ "  Following chain from: " <> show startedAt
       putStrLn $ "  Current tip: " <> show tip
-    onEvent (MarloweRollForward (BlockHeader slot _ block) [] tip) = do
-      putStrLn $ "Roll forward SlotNo: " <> show slot <> " BlockNo: " <> show block <>  " Tip: " <> show tip
-    onEvent (MarloweRollForward (BlockHeader slot blockHash block) txs tip) = do
+    onEvent (MarloweRollForward (MarloweBlockHeader slot blockHash block) _ tip) = do
       putStrLn "Roll forward"
       putStrLn $ "  SlotNo: " <> show slot
       putStrLn $ "  BlockHash: " <> show blockHash
       putStrLn $ "  BlockNo: " <> show block
       putStrLn $ "  Tip: " <> show tip
-      for_ (zip [0..] txs) \(i :: Integer, MarloweTx{..}) -> do
-        putStrLn $ "  Transaction[" <> show i <> "]: "
-        putStrLn $ "    Id: " <> show marloweTx_id
-        putStrLn $ "    Policies: " <> show marloweTx_policies
-        putStrLn $ "    Interval: " <> show marloweTx_interval
-        putStrLn $ "    Metadata: " <> show marloweTx_metadata
-        putStrLn $ "    Inputs: " <> show marloweTx_inputs
-        for_ (zip [0..] marloweTx_outputs) \(j :: Integer, MarloweTxOut{..}) -> do
-          putStrLn $ "    Output[" <> show j <> "]: "
-          putStrLn $ "      TxIn: " <> show marloweTxOut_txIn
-          putStrLn $ "      Address: " <> show marloweTxOut_address
-          putStrLn $ "      Value: " <> show marloweTxOut_value
-          putStrLn $ "      Datum: " <> show marloweTxOut_datum
+      -- for_ (zip [0..] txs) \(i :: Integer, MarloweTx{..}) -> do
+      --   putStrLn $ "  Transaction[" <> show i <> "]: "
+      --   putStrLn $ "    Id: " <> show marloweTx_id
+      --   putStrLn $ "    Policies: " <> show marloweTx_policies
+      --   putStrLn $ "    Interval: " <> show marloweTx_interval
+      --   putStrLn $ "    Metadata: " <> show marloweTx_metadata
+      --   putStrLn $ "    Inputs: " <> show marloweTx_inputs
+      --   for_ (zip [0..] marloweTx_outputs) \(j :: Integer, MarloweTxOut{..}) -> do
+      --     putStrLn $ "    Output[" <> show j <> "]: "
+      --     putStrLn $ "      TxIn: " <> show marloweTxOut_txIn
+      --     putStrLn $ "      Address: " <> show marloweTxOut_address
+      --     putStrLn $ "      Value: " <> show marloweTxOut_value
+      --     putStrLn $ "      Datum: " <> show marloweTxOut_datum
     onEvent (MarloweRollBackward point tip) = do
       putStrLn "Roll backward"
       putStrLn $ "  ToPoint: " <> show point
       putStrLn $ "  Tip: " <> show tip
 
-
-
 marloweChainSyncClient
-  :: forall m a
-   . Monad m
-  => GenesisParameters -- ^ GenesisParameters for the network
-  -> [ChainPoint] -- ^ Known starting points
-  -> (Maybe ChainPoint -> ChainTip -> m ()) -- ^ onStart callback. Receives the starting point and the tip.
+  :: forall b m a
+   . MonadUnlift b m
+  => [ChainPoint] -- ^ Known starting points
+  -> GenesisParameters -- ^ GenesisParameters for the network
+  -> (Maybe MarloweChainPoint -> MarloweChainTip -> m ()) -- ^ onStart callback. Receives the starting point and the tip.
   -> m Bool -- ^ onIdle callback. Allows the caller to terminate the client by returning True.
-  -> (forall env. MarloweChainEvent env -> m ()) -- ^ onEvent callback. Receives the new block and the tip
+  -> (MarloweChainEvent -> m ()) -- ^ onEvent callback. Receives the new block and the tip
   -> m a -- ^ onDone callback. Allows the caller to yeild a result for the client.
-  -> MarloweChainSyncClient m a
-marloweChainSyncClient genesisParams startingPoints onStart onIdle onEvent =
-  mkChainSyncClient startingPoints onStart onIdle onRollForward onRollBackward
+  -> m (MarloweChainSyncClient b a)
+marloweChainSyncClient startingPoints genesisParams onStart onIdle onEvent =
+  mkChainSyncClient
+    startingPoints
+    (\mpoint tip -> onStart (extractChainPoint <$> mpoint) (extractChainTip tip))
+    onIdle
+    onRollForward
+    onRollBackward
   where
-    onRollForward (BlockInMode block AlonzoEraInCardanoMode ) = onRollForward' block
-    onRollForward (BlockInMode block MaryEraInCardanoMode   ) = onRollForward' block
-    onRollForward (BlockInMode block AllegraEraInCardanoMode) = onRollForward' block
-    onRollForward (BlockInMode block ShelleyEraInCardanoMode) = onRollForward' block
-    onRollForward (BlockInMode block ByronEraInCardanoMode  ) = onRollForward' block
+    onRollForward block tip = onEvent $ handleRollForward genesisParams block $ extractChainTip tip
+    onRollBackward point = onEvent . MarloweRollBackward (extractChainPoint point) . extractChainTip
 
-    onRollForward' :: forall era. Block era -> ChainTip -> m ()
-    onRollForward' (Block header txns) =
-       onEvent . MarloweRollForward header (extractMarloweTx <$> txns)
+extractChainPoint :: ChainPoint -> MarloweChainPoint
+extractChainPoint ChainPointAtGenesis = MarloweChainPointAtGenesis
+extractChainPoint (ChainPoint (SlotNo slot) (HeaderHash hash)) =
+  MarloweChainPoint (MarloweSlotNo slot) (MarloweBlockHeaderHash hash)
 
-    onRollBackward point = onEvent . MarloweRollBackward point
+extractChainTip :: ChainTip -> MarloweChainTip
+extractChainTip ChainTipAtGenesis = MarloweChainTipAtGenesis
+extractChainTip (ChainTip (SlotNo slot) (HeaderHash hash) (BlockNo block)) =
+  MarloweChainTip (MarloweSlotNo slot) (MarloweBlockHeaderHash hash) (MarloweBlockNo block)
 
-    extractMarloweTx :: forall era. Tx era -> MarloweTx era
-    extractMarloweTx tx =
+handleRollForward :: GenesisParameters -> BlockInMode CardanoMode -> MarloweChainTip  -> MarloweChainEvent
+handleRollForward genesisParams b = case b of
+  BlockInMode (Block header txs) AlonzoEraInCardanoMode  ->
+    MarloweRollForward (extractHeader header) (MarloweTxsShelley $ extractMarloweTx (ShelleyAddressInEra ShelleyBasedEraAlonzo) <$> txs)
+  BlockInMode (Block header txs) MaryEraInCardanoMode    ->
+    MarloweRollForward (extractHeader header) (MarloweTxsShelley $ extractMarloweTx (ShelleyAddressInEra ShelleyBasedEraMary ) <$> txs)
+  BlockInMode (Block header txs) AllegraEraInCardanoMode ->
+    MarloweRollForward (extractHeader header) (MarloweTxsShelley $ extractMarloweTx (ShelleyAddressInEra ShelleyBasedEraAllegra) <$> txs)
+  BlockInMode (Block header txs) ShelleyEraInCardanoMode ->
+    MarloweRollForward (extractHeader header) (MarloweTxsShelley $ extractMarloweTx (ShelleyAddressInEra ShelleyBasedEraShelley)  <$> txs)
+  BlockInMode (Block header txs) ByronEraInCardanoMode   ->
+    MarloweRollForward (extractHeader header) (MarloweTxsByron $ extractMarloweTx ByronAddressInAnyEra <$> txs)
+  where
+    extractHeader (BlockHeader (SlotNo slot) (HeaderHash hash) (BlockNo block)) =
+      MarloweBlockHeader
+        (MarloweSlotNo slot)
+        (MarloweBlockHeaderHash hash)
+        (MarloweBlockNo block)
+
+    extractMarloweTx :: forall addr era. AddressTypeInEra addr era -> Tx era -> MarloweTx addr
+    extractMarloweTx addrType tx =
       let
         txBody@(TxBody TxBodyContent {..}) = getTxBody tx
         txId = getTxId txBody
       in
         MarloweTx
-          txId
+          (MarloweTxId txId)
           (extractMints txMintValue)
           (extractInterval txValidityRange)
           (extractMetadata txMetadata)
-          (fst <$> txIns)
-          (uncurry (extractMarloweTxOut txId) <$> zip [0..] txOuts)
+          (extractTxIn . fst <$> txIns)
+          (uncurry (extractMarloweTxOut addrType txId) <$> zip [0..] txOuts)
+
+    extractTxIn (TxIn tid ix) = MarloweTxIn (MarloweTxId tid) ix
 
     extractMints TxMintNone = []
     extractMints (TxMintValue _ value _) =
       let
         extractPolicy (ValueNestedBundle policy quantities)
           | Map.null $ Map.filter (> 0) quantities = mempty
-          | otherwise = Set.singleton policy
+          | otherwise = Set.singleton $ MarlowePolicyId policy
         extractPolicy _ = mempty
         ValueNestedRep reps = valueToNestedRep value
       in
@@ -152,43 +187,55 @@ marloweChainSyncClient genesisParams startingPoints onStart onIdle onEvent =
     extractMetadata (TxMetadataInEra _ metadata) = Just $ metadataToJson TxMetadataJsonNoSchema  metadata
     extractMetadata _                            = Nothing
 
-    extractMarloweTxOut txId ix (TxOut address value datum) =
-      MarloweTxOut (TxIn txId (TxIx ix)) address (txOutValueToValue value) case datum of
-        TxOutDatum _ datum' -> Just datum'
-        _                   -> Nothing
+    extractMarloweTxOut
+      :: forall addr era
+       . AddressTypeInEra addr era
+      -> TxId
+      -> Word
+      -> TxOut CtxTx era
+      -> MarloweTxOut addr
+    extractMarloweTxOut _ txId ix (TxOut (AddressInEra _ address) value datum) =
+      MarloweTxOut
+        (MarloweTxIn (MarloweTxId txId) (TxIx ix))
+        (unsafeCoerce address :: Address addr) -- We know that addr' ~ addr here, but try convincing the stupid compiler...
+        (txOutValueToValue value)
+        case datum of
+          TxOutDatum _ datum' -> Just datum'
+          _                   -> Nothing
 
 mkChainSyncClient
-  :: Monad m
+  :: MonadUnlift b m
   => [ChainPoint] -- ^ Known starting points
   -> (Maybe ChainPoint -> ChainTip -> m ()) -- ^ onStart callback. Receives the starting point and the tip.
   -> m Bool -- ^ onIdle callback. Allows the caller to terminate the client by returning True.
   -> (BlockInMode CardanoMode  -> ChainTip -> m ()) -- ^ onRollForward callback. Receives the new block and the tip
   -> (ChainPoint -> ChainTip -> m ()) -- ^ onRollBackward callback. Receives the point to rollback to and the tip
   -> m a -- ^ onDone callback. Allows the caller to yeild a result for the client.
-  -> MarloweChainSyncClient m a
-mkChainSyncClient startingPoints onStart onIdle onRollForward onRollBackward onDone =
-  ChainSyncClient $ pure start
+  -> m (MarloweChainSyncClient b a)
+mkChainSyncClient startingPoints onStart onIdle onRollForward onRollBackward onDone = do
+  u <- askUnlift
+  pure $ ChainSyncClient $ pure $ start u
   where
-    start = SendMsgFindIntersect startingPoints $ ClientStIntersect
-        { recvMsgIntersectFound = \point tip -> ChainSyncClient do
+    start u = SendMsgFindIntersect startingPoints $ ClientStIntersect
+        { recvMsgIntersectFound = \point tip -> ChainSyncClient $ runUnlift u do
           onStart (Just point) tip
-          pure idle
-        , recvMsgIntersectNotFound = \tip -> ChainSyncClient do
+          pure $ idle u
+        , recvMsgIntersectNotFound = \tip -> ChainSyncClient $ runUnlift u do
           onStart Nothing tip
-          pure idle
+          pure $ idle u
         }
-    idle = SendMsgRequestNext next do
+    idle u = SendMsgRequestNext (next u) $ runUnlift u do
       terminate <- onIdle
-      pure if terminate then done else next
-    next = ClientStNext
-      { recvMsgRollForward = \block tip -> ChainSyncClient do
+      pure if terminate then done u else next u
+    next u = ClientStNext
+      { recvMsgRollForward = \block tip -> ChainSyncClient $ runUnlift u $  do
           onRollForward block tip
-          pure idle
-      , recvMsgRollBackward = \point tip -> ChainSyncClient do
+          pure $ idle u
+      , recvMsgRollBackward = \point tip -> ChainSyncClient $ runUnlift u $  do
           onRollBackward point tip
-          pure idle
+          pure $ idle u
       }
-    done = ClientStNext
-      { recvMsgRollForward = \_ _ -> ChainSyncClient $ SendMsgDone <$> onDone
-      , recvMsgRollBackward = \_ _ -> ChainSyncClient $ SendMsgDone <$> onDone
+    done u = ClientStNext
+      { recvMsgRollForward = \_ _ -> ChainSyncClient $ SendMsgDone <$> runUnlift u onDone
+      , recvMsgRollBackward = \_ _ -> ChainSyncClient $ SendMsgDone <$> runUnlift u onDone
       }
--- marlowe-runtime/src/Language/Marlowe/Runtime/Chain/Types.hs
+++ marlowe-runtime/src/Language/Marlowe/Runtime/Chain/Types.hs
@@ -1,36 +1,182 @@
+{-# LANGUAGE BlockArguments             #-}
+{-# LANGUAGE ExistentialQuantification  #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE StrictData                 #-}
+{-# LANGUAGE TypeApplications           #-}
+{-# LANGUAGE UndecidableInstances       #-}
+
 module Language.Marlowe.Runtime.Chain.Types where
 
-import Cardano.Api (AddressInEra, BlockHeader, BlockInMode, CardanoMode, ChainPoint, ChainSyncClient, ChainTip,
-                    PolicyId, ScriptData, TxId, TxIn, Value)
+import Cardano.Api (Address, AsType (..), BlockInMode, ByronAddr, CardanoMode, ChainPoint, ChainSyncClient, ChainTip,
+                    HasTypeProxy (proxyToAsType), PolicyId, ScriptData, SerialiseAsCBOR (..), SerialiseAsRawBytes (..),
+                    ShelleyAddr, TxId, TxIx (..), Value, serialiseToJSON)
+import Control.Monad (forM)
 import qualified Data.Aeson as Aeson
-import Data.Time (NominalDiffTime, UTCTime)
+import Data.Binary (Binary (..), Word64)
+import Data.ByteString.Short (ShortByteString)
+import Data.Data (Typeable)
+import Data.Proxy (Proxy (Proxy))
+import Data.Time (Day (..), NominalDiffTime, UTCTime (..), diffTimeToPicoseconds, nominalDiffTimeToSeconds,
+                  picosecondsToDiffTime, secondsToNominalDiffTime)
+import GHC.Generics (Generic)
 
-data MarloweChainEvent era
-  = MarloweRollForward BlockHeader [MarloweTx era] ChainTip
-  | MarloweRollBackward ChainPoint ChainTip
+type MarloweChainSyncClient = ChainSyncClient (BlockInMode CardanoMode) ChainPoint ChainTip
 
-data MarloweTx era = MarloweTx
-  { marloweTx_id       :: TxId
-  , marloweTx_policies :: [PolicyId]
+data MarloweChainEvent
+  = MarloweRollForward MarloweBlockHeader MarloweTxs MarloweChainTip
+  | MarloweRollBackward MarloweChainPoint MarloweChainTip
+  deriving (Generic, Typeable, Show, Eq)
+
+instance Binary MarloweChainEvent
+
+data MarloweBlockHeader = MarloweBlockHeader MarloweSlotNo MarloweBlockHeaderHash MarloweBlockNo
+  deriving (Generic, Typeable, Show, Eq)
+
+instance Binary MarloweBlockHeader
+
+newtype MarloweSlotNo = MarloweSlotNo Word64
+  deriving (Typeable, Show, Eq, Binary)
+
+newtype MarloweBlockNo = MarloweBlockNo Word64
+  deriving (Typeable, Show, Eq, Binary)
+
+newtype MarloweBlockHeaderHash = MarloweBlockHeaderHash ShortByteString
+  deriving (Typeable, Show, Eq, Binary)
+
+data MarloweChainTip
+  = MarloweChainTipAtGenesis
+  | MarloweChainTip MarloweSlotNo MarloweBlockHeaderHash MarloweBlockNo
+  deriving (Generic, Typeable, Show, Eq)
+
+instance Binary MarloweChainTip
+
+data MarloweChainPoint
+  = MarloweChainPointAtGenesis
+  | MarloweChainPoint MarloweSlotNo MarloweBlockHeaderHash
+  deriving (Generic, Typeable, Show, Eq)
+
+instance Binary MarloweChainPoint
+
+data MarloweTxIn = MarloweTxIn MarloweTxId TxIx
+  deriving (Typeable, Show, Eq, Ord)
+
+instance Binary MarloweTxIn where
+  put (MarloweTxIn tid (TxIx ix)) = do
+    put tid
+    put ix
+  get = do
+    tid <- get
+    ix <- get
+    pure $ MarloweTxIn tid $ TxIx ix
+
+newtype MarloweTxId = MarloweTxId TxId
+  deriving (Typeable, Show, Eq, Ord)
+
+instance Binary MarloweTxId where
+  put (MarloweTxId tid) = do
+    put $ serialiseToRawBytes tid
+  get = do
+    tidBytes <- get
+    tid <- case deserialiseFromRawBytes AsTxId tidBytes of
+      Nothing -> fail "Idvalid txId bytes"
+      Just a  -> pure a
+    pure $ MarloweTxId tid
+
+newtype MarlowePolicyId = MarlowePolicyId PolicyId
+  deriving (Typeable, Show, Eq, Ord)
+
+instance Binary MarlowePolicyId where
+  put (MarlowePolicyId tid) = do
+    put $ serialiseToRawBytes tid
+  get = do
+    tidBytes <- get
+    tid <- case deserialiseFromRawBytes AsPolicyId tidBytes of
+      Nothing -> fail "Idvalid policyId bytes"
+      Just a  -> pure a
+    pure $ MarlowePolicyId tid
+
+data MarloweTxs
+  = MarloweTxsByron [MarloweTx ByronAddr]
+  | MarloweTxsShelley [MarloweTx ShelleyAddr]
+  deriving (Generic, Typeable, Show, Eq)
+
+instance Binary MarloweTxs
+
+data MarloweTx addr = MarloweTx
+  { marloweTx_id       :: MarloweTxId
+  , marloweTx_policies :: [MarlowePolicyId]
   , marloweTx_interval :: Maybe MarloweValidityInterval
   , marloweTx_metadata :: Maybe Aeson.Value
-  , marloweTx_inputs   :: [TxIn]
-  , marloweTx_outputs  :: [MarloweTxOut era]
+  , marloweTx_inputs   :: [MarloweTxIn]
+  , marloweTx_outputs  :: [MarloweTxOut addr]
   }
-  deriving (Show, Eq)
+  deriving (Generic, Typeable, Show, Eq)
 
-data MarloweTxOut era = MarloweTxOut
-  { marloweTxOut_txIn    :: TxIn
-  , marloweTxOut_address :: AddressInEra era
+instance (HasTypeProxy addr, SerialiseAsRawBytes (Address addr)) => Binary (MarloweTx addr) where
+  put (MarloweTx tid policies interval metadata inputs outputs) = do
+    put tid
+    put policies
+    put interval
+    put $ Aeson.encode <$> metadata
+    put inputs
+    put outputs
+  get = do
+    tid <- get
+    policies <- get
+    interval <- get
+    metadataBytes <- get
+    metadata <- forM metadataBytes \bytes -> case Aeson.eitherDecode bytes of
+      Left err -> fail err
+      Right a  -> pure a
+    inputs <- get
+    outputs <- get
+    pure $ MarloweTx tid policies interval metadata inputs outputs
+
+data MarloweTxOut addr = MarloweTxOut
+  { marloweTxOut_txIn    :: MarloweTxIn
+  , marloweTxOut_address :: Address addr
   , marloweTxOut_value   :: Value
   , marloweTxOut_datum   :: Maybe ScriptData
   }
-  deriving (Show, Eq)
+  deriving (Generic, Typeable, Show, Eq)
 
-type MarloweChainSyncClient = ChainSyncClient (BlockInMode CardanoMode) ChainPoint ChainTip
+instance (HasTypeProxy addr, SerialiseAsRawBytes (Address addr)) => Binary (MarloweTxOut addr) where
+  put (MarloweTxOut txIn address value datum) = do
+    put txIn
+    put $ serialiseToRawBytes address
+    put $ serialiseToJSON value
+    put $ serialiseToCBOR <$> datum
+  get = do
+    txIn <- get
+    addressBytes <- get
+    valueBytes <- get
+    datumBytes <- get
+    address <- case deserialiseFromRawBytes (AsAddress $ proxyToAsType $ Proxy @addr) addressBytes of
+      Nothing -> fail "invalid address"
+      Just a  -> pure a
+    value <- case Aeson.eitherDecode valueBytes of
+      Left err -> fail err
+      Right a  -> pure a
+    datum <- forM datumBytes \bytes -> case deserialiseFromCBOR AsScriptData bytes of
+      Left err -> fail $ show err
+      Right a  -> pure a
+    pure $ MarloweTxOut txIn address value datum
 
 data MarloweValidityInterval = MarloweValidityInterval
   { startTime :: UTCTime
   , duration  :: NominalDiffTime
   }
-  deriving (Show, Eq)
+  deriving (Generic, Typeable, Show, Eq)
+
+instance Binary MarloweValidityInterval where
+  put (MarloweValidityInterval (UTCTime (ModifiedJulianDay d) dt) dt') = do
+    put d
+    put $ diffTimeToPicoseconds dt
+    put $ nominalDiffTimeToSeconds dt'
+  get = do
+    d <- get
+    dt <- picosecondsToDiffTime <$> get
+    dt' <- secondsToNominalDiffTime <$> get
+    pure $ MarloweValidityInterval (UTCTime (ModifiedJulianDay d) dt) dt'
