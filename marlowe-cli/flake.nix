d43fe0cc8dc9ca8df9355f67e22bb6488537369e
Brian W Bush <brian.bush@iohk.io>
2022-06-19 21:38:39 -0600

    SCP-4120 Added Marlowe environment to JupyterLab flake.

--- marlowe-cli/flake.nix
+++ marlowe-cli/flake.nix
@@ -1,23 +1,43 @@
 {
-  description = "JupyterLab Flake";
+  description = "JupyterLab Flake with Marlowe Environment";
 
   inputs = {
     jupyterWith.url = "github:tweag/jupyterWith";
     flake-utils.url = "github:numtide/flake-utils";
+    marlowe-source = {
+      url = "../";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, jupyterWith, flake-utils }:
+  outputs = { self, nixpkgs, jupyterWith, flake-utils, marlowe-source }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
       let
         pkgs = import nixpkgs {
           system = system;
           overlays = nixpkgs.lib.attrValues jupyterWith.overlays;
         };
+        marlowe = import marlowe-source.outPath { inherit system; };
+        ghcWithMarlowe = marlowe.marlowe.haskell.project.ghcWithPackages (p: [
+          p.marlowe
+        ]);
         ibash = pkgs.jupyterWith.kernels.bashKernel {
           name = "Marlowe";
         };
         jupyterEnvironment = pkgs.jupyterlabWith {
-          kernels = [ ibash ];
+          kernels = [
+            ibash
+          ];
+          extraPackages = p: [
+            marlowe.cardano-cli
+            marlowe.marlowe-cli
+            p.coreutils
+            p.curl
+            p.gnused
+            p.jq
+            p.remarshal
+            ghcWithMarlowe
+          ];
         };
       in
       rec {
