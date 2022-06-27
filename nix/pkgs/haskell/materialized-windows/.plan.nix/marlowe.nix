ba191624319bb3aee2148fd9cd013f8d506e4612
Hernan Rajchert <hrajchert@gmail.com>
2022-06-27 15:26:22 -0300

    Update materialized

--- nix/pkgs/haskell/materialized-windows/.plan.nix/marlowe.nix
+++ nix/pkgs/haskell/materialized-windows/.plan.nix/marlowe.nix
@@ -68,9 +68,9 @@
         buildable = true;
         modules = [
           "Language/Marlowe"
-          "Language/Marlowe/Extended"
-          "Language/Marlowe/Semantics"
-          "Language/Marlowe/Semantics/Types"
+          "Language/Marlowe/Extended/V1"
+          "Language/Marlowe/Core/V1/Semantics"
+          "Language/Marlowe/Core/V1/Semantics/Types"
           "Language/Marlowe/FindInputs"
           "Language/Marlowe/Client"
           "Language/Marlowe/Client/History"
