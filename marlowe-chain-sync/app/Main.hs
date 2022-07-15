c5ea4a614a418d0c76591746e802ce6c87e5ccf4
Jamie Bertram <jamie.bertram@tweag.io>
2022-07-15 17:21:02 -0400

    Implement pipelined chain sync client

--- marlowe-chain-sync/app/Main.hs
+++ marlowe-chain-sync/app/Main.hs
@@ -1,8 +1,27 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import Cardano.Api (Block (Block), BlockHeader (..), BlockInMode (BlockInMode), ConsensusModeParams (..),
+                    EpochSlots (..), LocalNodeConnectInfo (..), NetworkId (..), NetworkMagic (..))
+import Language.Marlowe.Runtime.ChainSync.NodeClient (ChainSyncEvent (..), runNodeClient)
+import Ouroboros.Network.Point (WithOrigin (..))
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  let
+    connectionInfo = LocalNodeConnectInfo
+      { localConsensusModeParams = CardanoModeParams $ EpochSlots 21600
+      , localNodeNetworkId = Testnet $ NetworkMagic 1566
+      , localNodeSocketPath = "/var/lib/containers/storage/volumes/marlowe-dashboard-client_cardano-ipc/_data/node.socket"
+      }
+    getHeaderAtPoint _ = pure Origin
+    getIntersectionPoints _ _ = pure []
+  runNodeClient connectionInfo getHeaderAtPoint getIntersectionPoints \case
+    RollForward (BlockInMode (Block header _) _) _ -> putStrLn (showHeader header)
+    RollBackward _ _                               -> putStrLn "rollback"
+
+showHeader :: BlockHeader -> String
+showHeader (BlockHeader slotNo hash blockNo) =
+  "BlockHeader "
+    <> "(" <> show slotNo <> ")"
+    <> "(" <> show hash <> ")"
+    <> "(" <> show blockNo <> ")"
