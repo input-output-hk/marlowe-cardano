f5e1b578082a6686f4b2c75382fd1555d8a81937
Jamie Bertram <jamie.bertram@tweag.io>
2022-07-18 09:30:07 -0400

    refactor NodeClient

--- marlowe-chain-sync/src/Language/Marlowe/Runtime/ChainSync/NodeClient.hs
+++ marlowe-chain-sync/src/Language/Marlowe/Runtime/ChainSync/NodeClient.hs
@@ -9,13 +9,19 @@ module Language.Marlowe.Runtime.ChainSync.NodeClient
 
 import Cardano.Api (Block (..), BlockHeader (..), BlockInMode (..), BlockNo, CardanoMode, ChainPoint (..),
                     ChainSyncClientPipelined (..), ChainTip (..), LocalChainSyncClient (..),
-                    LocalNodeClientProtocols (..), LocalNodeConnectInfo, connectToLocalNode)
+                    LocalNodeClientProtocols (..), LocalNodeConnectInfo, chainPointToSlotNo, connectToLocalNode)
 import Cardano.Api.ChainSync.ClientPipelined (ClientPipelinedStIdle (..), ClientPipelinedStIntersect (..),
                                               ClientStNext (..), MkPipelineDecision, N (Z), Nat (..),
-                                              PipelineDecision (..), pipelineDecisionLowHighMark, runPipelineDecision)
+                                              PipelineDecision (..), mapChainSyncClientPipelined,
+                                              pipelineDecisionLowHighMark, runPipelineDecision)
+import Control.Arrow ((&&&))
+import Data.List (sortOn)
+import Data.Ord (Down (Down))
 import Ouroboros.Network.Point (WithOrigin (..))
 
 type CardanoBlock = BlockInMode CardanoMode
+type NumberedCardanoBlock = (BlockNo, CardanoBlock)
+type NumberedChainTip = (WithOrigin BlockNo, ChainTip)
 
 data ChainSyncEvent
   = RollForward CardanoBlock ChainTip
@@ -29,22 +35,44 @@ runNodeClient
   -> IO ()
 runNodeClient nodeConnectInfo getHeaderAtPoint getIntersectionPoints onEvent =
   connectToLocalNode nodeConnectInfo LocalNodeClientProtocols
-    { localChainSyncClient = LocalChainSyncClientPipelined $ pipelinedClient onEvent getHeaderAtPoint getIntersectionPoints
+    { localChainSyncClient = LocalChainSyncClientPipelined
+        $ mapChainSyncClientPipelined id id (blockToBlockNo &&& id) (chainTipToBlockNo &&& id)
+        $ pipelinedClient onEvent getBlockNoAtPoint getIntersectionPoints
     , localTxSubmissionClient = Nothing
     , localStateQueryClient   = Nothing
     }
+  where
+    blockHeaderToBlockNo :: BlockHeader -> BlockNo
+    blockHeaderToBlockNo (BlockHeader _ _ blockNo) = blockNo
+
+    blockToBlockNo :: CardanoBlock -> BlockNo
+    blockToBlockNo (BlockInMode (Block header _) _) = blockHeaderToBlockNo header
+
+    getBlockNoAtPoint :: ChainPoint -> IO (WithOrigin BlockNo)
+    getBlockNoAtPoint = fmap (fmap blockHeaderToBlockNo) . getHeaderAtPoint
+
+    chainTipToBlockNo :: ChainTip -> WithOrigin BlockNo
+    chainTipToBlockNo = \case
+      ChainTipAtGenesis    -> Origin
+      ChainTip _ _ blockNo -> At blockNo
+
 
 pipelinedClient
-  :: (ChainSyncEvent -> IO ())
-  -> (ChainPoint -> IO (WithOrigin BlockHeader))
-  -> (ChainPoint -> Maybe ChainPoint -> IO [ChainPoint])
-  -> ChainSyncClientPipelined CardanoBlock ChainPoint ChainTip IO ()
-pipelinedClient onEvent getHeaderAtPoint getIntersectionPoints =
+  :: forall m
+   . Monad m
+  => (ChainSyncEvent -> m ())
+  -> (ChainPoint -> m (WithOrigin BlockNo))
+  -> (ChainPoint -> Maybe ChainPoint -> m [ChainPoint])
+  -> ChainSyncClientPipelined NumberedCardanoBlock ChainPoint NumberedChainTip m ()
+pipelinedClient onEvent getBlockNoAtPoint getIntersectionPoints =
   ChainSyncClientPipelined $ intersect ChainPointAtGenesis Nothing
   where
-    intersect :: ChainPoint -> Maybe ChainPoint -> IO (ClientPipelinedStIdle 'Z CardanoBlock ChainPoint ChainTip IO ())
+    intersect
+      :: ChainPoint
+      -> Maybe ChainPoint
+      -> m (ClientPipelinedStIdle 'Z NumberedCardanoBlock ChainPoint NumberedChainTip m ())
     intersect lowerBound upperBound = do
-      points <- getIntersectionPoints lowerBound upperBound
+      points <- sortPoints <$> getIntersectionPoints lowerBound upperBound
       pure $ SendMsgFindIntersect points ClientPipelinedStIntersect
         { recvMsgIntersectFound = \point tip -> case refineBounds point points of
             Nothing                         -> clientStIdle point tip
@@ -52,73 +80,80 @@ pipelinedClient onEvent getHeaderAtPoint getIntersectionPoints =
         , recvMsgIntersectNotFound = clientStIdle ChainPointAtGenesis
         }
 
+    sortPoints :: [ChainPoint] -> [ChainPoint]
+    sortPoints = sortOn $ Down . chainPointToSlotNo
+
+    -- INVARIANT: points are sorted in descenting order by slot
     refineBounds :: ChainPoint -> [ChainPoint] -> Maybe (ChainPoint, ChainPoint)
     refineBounds found points = (found,) <$> case takeWhile (/= found) points of
       []      -> Nothing
       points' -> Just $ last points'
 
-    clientStIdle :: ChainPoint -> ChainTip -> IO (ClientPipelinedStIdle 'Z CardanoBlock ChainPoint ChainTip IO ())
+    clientStIdle
+      :: ChainPoint
+      -> NumberedChainTip
+      -> m (ClientPipelinedStIdle 'Z NumberedCardanoBlock ChainPoint NumberedChainTip m ())
     clientStIdle point nodeTip = do
-      blockHeader <- getHeaderAtPoint point
-      let clientTip = (\(BlockHeader _ _ blockNo) -> blockNo) <$> blockHeader
-      pure $ mkClientStIdle onEvent getHeaderAtPoint pipelinePolicy Zero clientTip nodeTip
+      clientTip <- getBlockNoAtPoint point
+      pure $ mkClientStIdle onEvent getBlockNoAtPoint pipelinePolicy Zero clientTip nodeTip
 
     pipelinePolicy :: MkPipelineDecision
     pipelinePolicy = pipelineDecisionLowHighMark 1 50
 
 mkClientStIdle
-  :: (ChainSyncEvent -> IO ())
-  -> (ChainPoint -> IO (WithOrigin BlockHeader))
+  :: forall m n
+   . Monad m
+  => (ChainSyncEvent -> m ())
+  -> (ChainPoint -> m (WithOrigin BlockNo))
   -> MkPipelineDecision
   -> Nat n
   -> WithOrigin BlockNo
-  -> ChainTip
-  -> ClientPipelinedStIdle n CardanoBlock ChainPoint ChainTip IO ()
-mkClientStIdle onEvent getHeaderAtPoint pipelinePolicy n clientTip nodeTip =
-  case (n, runPipelineDecision pipelinePolicy n clientTip nodeTip') of
-    (_, (Request, nextPipelinePolicy)) ->
-      SendMsgRequestNext clientStNext $ pure clientStNext
-        where
-          clientStNext = mkClientStNext onEvent getHeaderAtPoint
-            $ mkClientStIdle onEvent getHeaderAtPoint nextPipelinePolicy n
-    (_, (Pipeline, nextPipelinePolicy)) ->
-      SendMsgRequestNextPipelined
-        $ mkClientStIdle onEvent getHeaderAtPoint nextPipelinePolicy (Succ n) clientTip nodeTip
-    (Succ n', (CollectOrPipeline, nextPipelinePolicy)) ->
+  -> NumberedChainTip
+  -> ClientPipelinedStIdle n NumberedCardanoBlock ChainPoint NumberedChainTip m ()
+mkClientStIdle onEvent getBlockNoAtPoint pipelineDecision n clientTip nodeTip =
+  case (n, runPipelineDecision pipelineDecision n clientTip (fst nodeTip)) of
+    (_, (Request, pipelineDecision')) ->
+      SendMsgRequestNext (collect pipelineDecision' n) $ pure (collect pipelineDecision' n)
+
+    (_, (Pipeline, pipelineDecision')) ->
+      nextPipelineRequest pipelineDecision'
+
+    (Succ n', (CollectOrPipeline, pipelineDecision')) ->
       CollectResponse
-        ( Just
-            $ pure
-            $ SendMsgRequestNextPipelined
-            $ mkClientStIdle onEvent getHeaderAtPoint nextPipelinePolicy (Succ n) clientTip nodeTip
-        )
-        ( mkClientStNext onEvent  getHeaderAtPoint
-            $ mkClientStIdle onEvent getHeaderAtPoint nextPipelinePolicy n'
-        )
-    (Succ n', (Collect, nextPipelinePolicy)) ->
-      CollectResponse
-        Nothing
-        ( mkClientStNext onEvent getHeaderAtPoint
-            $ mkClientStIdle onEvent getHeaderAtPoint nextPipelinePolicy n'
-        )
-    where
-      nodeTip' = case nodeTip of
-        ChainTipAtGenesis    -> Origin
-        ChainTip _ _ blockNo -> At blockNo
+        (Just $ pure $ nextPipelineRequest pipelineDecision')
+        (collect pipelineDecision' n')
+
+    (Succ n', (Collect, pipelineDecision')) ->
+      CollectResponse Nothing (collect pipelineDecision' n')
+
+  where
+    nextPipelineRequest
+      :: MkPipelineDecision
+      -> ClientPipelinedStIdle n NumberedCardanoBlock ChainPoint NumberedChainTip m ()
+    nextPipelineRequest pipelineDecision' = SendMsgRequestNextPipelined
+      $ mkClientStIdle onEvent getBlockNoAtPoint pipelineDecision' (Succ n) clientTip nodeTip
+
+    collect
+      :: forall n'
+        . MkPipelineDecision
+        -> Nat n'
+        -> ClientStNext n' NumberedCardanoBlock ChainPoint NumberedChainTip m ()
+    collect pipelineDecision' = mkClientStNext onEvent getBlockNoAtPoint
+      . mkClientStIdle onEvent getBlockNoAtPoint pipelineDecision'
 
 mkClientStNext
-  :: (ChainSyncEvent -> IO ())
-  -> (ChainPoint -> IO (WithOrigin BlockHeader))
-  -> (WithOrigin BlockNo -> ChainTip -> ClientPipelinedStIdle n CardanoBlock ChainPoint ChainTip IO ())
-  -> ClientStNext n CardanoBlock ChainPoint ChainTip IO ()
-mkClientStNext onEvent getHeaderAtPoint next = ClientStNext
-  { recvMsgRollForward = \block tip -> do
-      onEvent $ RollForward block tip
-      let BlockInMode (Block (BlockHeader _ _ blockNo) _) _ = block
+  :: Monad m
+  => (ChainSyncEvent -> m ())
+  -> (ChainPoint -> m (WithOrigin BlockNo))
+  -> (WithOrigin BlockNo -> NumberedChainTip -> ClientPipelinedStIdle n NumberedCardanoBlock ChainPoint NumberedChainTip m ())
+  -> ClientStNext n NumberedCardanoBlock ChainPoint NumberedChainTip m ()
+mkClientStNext onEvent getBlockNoAtPoint next = ClientStNext
+  { recvMsgRollForward = \(blockNo, block) tip -> do
+      onEvent $ RollForward block $ snd tip
       let clientTip = At blockNo
       pure $ next clientTip tip
   , recvMsgRollBackward = \point tip -> do
-      onEvent $ RollBackward point tip
-      blockHeader <- getHeaderAtPoint point
-      let clientTip = (\(BlockHeader _ _ blockNo) -> blockNo) <$> blockHeader
+      onEvent $ RollBackward point $ snd tip
+      clientTip <- getBlockNoAtPoint point
       pure $ next clientTip tip
   }
