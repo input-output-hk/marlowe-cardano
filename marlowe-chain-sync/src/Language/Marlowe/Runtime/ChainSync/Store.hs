0cd0b18c6e9053b0d596e080a6c6fa96c8cfdc59
Jamie Bertram <jamie.bertram@tweag.io>
2022-07-20 10:58:28 -0400

    Refactor components

--- marlowe-chain-sync/src/Language/Marlowe/Runtime/ChainSync/Store.hs
+++ marlowe-chain-sync/src/Language/Marlowe/Runtime/ChainSync/Store.hs
@@ -1,102 +1,67 @@
 module Language.Marlowe.Runtime.ChainSync.Store
   ( Changes(..)
-  , StoreDependencies(..)
-  , StoreApi(..)
-  , mkStore
-  , mkStoreEventHandler
+  , CommitBlocks(..)
+  , CommitRollback(..)
+  , ChainStoreDependencies(..)
+  , ChainStore(..)
+  , mkChainStore
   ) where
 
-import Cardano.Api (Block (..), BlockHeader (..), BlockInMode (..), ChainPoint (..), getBlockHeader)
-import Control.Concurrent (threadDelay)
-import Control.Concurrent.STM (STM, atomically, modifyTVar, newTVar, readTVar, writeTVar)
+import Cardano.Api (ChainPoint (..))
+import Control.Concurrent.STM (STM, atomically)
+import Control.Concurrent.STM.Delay (Delay, newDelay, waitDelay)
 import Control.Monad (guard)
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.Maybe (MaybeT (..))
 import Data.Foldable (traverse_)
-import Data.Function (on)
 import Data.Time (NominalDiffTime, UTCTime, addUTCTime, diffUTCTime, getCurrentTime, nominalDiffTimeToSeconds)
-import Language.Marlowe.Runtime.ChainSync.NodeClient (CardanoBlock, ChainEventHandler, ChainSyncEvent (..))
+import Language.Marlowe.Runtime.ChainSync.NodeClient (CardanoBlock, Changes (..), isEmptyChanges)
+import Prelude hiding (filter)
 import Witherable (Witherable (..))
 
-type CommitRollback m = ChainPoint -> m ()
-type CommitBlocks m = [CardanoBlock] -> m ()
+newtype CommitRollback m = CommitRollback { runCommitRollback :: ChainPoint -> m () }
+newtype CommitBlocks m = CommitBlocks { runCommitBlocks :: [CardanoBlock] -> m () }
 
-data Changes = Changes
-  { changesRollback :: !(Maybe ChainPoint)
-  , changesBlocks   :: ![CardanoBlock]
-  }
-
-emptyChanges :: Changes
-emptyChanges = Changes Nothing []
-
-isEmpty :: Changes -> Bool
-isEmpty (Changes Nothing []) = True
-isEmpty _                    = False
-
-instance Eq Changes where
-  a == b = changesRollback a == changesRollback b
-    && on (==) (map (headerToTuple . getBlock) . changesBlocks) a b
-    where
-      getBlock (BlockInMode block _) = getBlockHeader block
-      headerToTuple (BlockHeader slot hash block) = (slot, hash, block)
-
-data StoreDependencies = StoreDependencies
+data ChainStoreDependencies = ChainStoreDependencies
   { commitRollback :: !(CommitRollback IO)
   , commitBlocks   :: !(CommitBlocks IO)
-  , awaitChanges   :: !(STM Changes)
   , rateLimit      :: !NominalDiffTime
+  , getChanges     :: !(STM Changes)
+  , clearChanges   :: !(STM ())
   }
 
-newtype StoreApi = StoreApi
-  { runStore :: IO ()
+newtype ChainStore = ChainStore
+  { runChainStore :: IO ()
   }
 
-mkStore :: StoreDependencies -> IO StoreApi
-mkStore StoreDependencies{..} = do
-  pure $ StoreApi
-    { runStore = runStore Nothing
-    }
-  where
-    runStore :: Maybe UTCTime -> IO ()
-    runStore lastWrite = do
-      delay <- wither computeDelay lastWrite
-      traverse_ (threadDelay . round . (* 1_000_000) . nominalDiffTimeToSeconds) delay
-      Changes{..} <- atomically awaitChanges
-      traverse_ commitRollback changesRollback
-      commitBlocks changesBlocks
-      runStore . Just =<< getCurrentTime
+mkChainStore :: ChainStoreDependencies -> STM ChainStore
+mkChainStore ChainStoreDependencies{..} = do
+  let
+    awaitChanges :: Maybe Delay -> STM Changes
+    awaitChanges delay = do
+      traverse_ waitDelay delay
+      changes <- getChanges
+      guard $ not $ isEmptyChanges changes
+      clearChanges
+      pure changes
 
-    computeDelay :: UTCTime -> IO (Maybe NominalDiffTime)
-    computeDelay lastWrite = do
-      currentTime <- getCurrentTime
-      pure do
-        let nextRun = addUTCTime rateLimit lastWrite
-        guard $ nextRun > currentTime
-        pure $ diffUTCTime nextRun currentTime
+    runChainStore :: IO ()
+    runChainStore = go Nothing
+      where
+        go lastWrite = do
+          delay <- wither computeDelay lastWrite
+          Changes{..} <- atomically $ awaitChanges delay
+          traverse_ (runCommitRollback commitRollback) changesRollback
+          runCommitBlocks commitBlocks changesBlocks
+          go . Just =<< getCurrentTime
 
-mkStoreEventHandler :: STM (STM Changes, ChainEventHandler STM)
-mkStoreEventHandler = do
-  tchanges <- newTVar emptyChanges
-  pure
-    ( do
-        changes <- readTVar tchanges
-        guard $ not $ isEmpty changes
-        writeTVar tchanges emptyChanges
-        pure changes
-    , \case
-        RollBackward ChainPointAtGenesis _ ->
-          writeTVar tchanges $ Changes (Just ChainPointAtGenesis) []
-        RollBackward point@(ChainPoint slot _) _ ->
-          modifyTVar tchanges \Changes{..} -> Changes
-            (Just $ maybe point (minPoint point) changesRollback)
-            (filter ((<= slot) . blockSlot) changesBlocks)
-        RollForward block _ ->
-          modifyTVar tchanges \changes ->
-            changes { changesBlocks = block : changesBlocks changes }
-    )
-  where
-    minPoint ChainPointAtGenesis _ = ChainPointAtGenesis
-    minPoint _ ChainPointAtGenesis = ChainPointAtGenesis
-    minPoint p1@(ChainPoint s1 _) p2@(ChainPoint s2 _)
-      | s1 < s2 = p1
-      | otherwise = p2
+    computeDelay :: UTCTime -> IO (Maybe Delay)
+    computeDelay lastWrite = runMaybeT do
+      currentTime <- lift getCurrentTime
+      let nextWrite = addUTCTime rateLimit lastWrite
+      guard $ nextWrite > currentTime
+      let delay = nextWrite `diffUTCTime` currentTime
+      let delayMicroseconds = floor $ 1_000_000 * nominalDiffTimeToSeconds delay
+      lift $ newDelay delayMicroseconds
 
-    blockSlot (BlockInMode (Block (BlockHeader slot _ _) _) _) = slot
+  pure $ ChainStore { runChainStore }
