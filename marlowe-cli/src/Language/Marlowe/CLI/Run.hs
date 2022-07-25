d3433f2d4acaa680785c42498aae82aecdbd7138
Tomasz Rybarczyk <tomasz.rybarczyk@iohk.io>
2022-07-25 11:07:35 +0200

    Make serialization generic over shelley based eras

--- marlowe-cli/src/Language/Marlowe/CLI/Run.hs
+++ marlowe-cli/src/Language/Marlowe/CLI/Run.hs
@@ -46,7 +46,9 @@ import Cardano.Api (AddressAny, AddressInEra (..), AddressTypeInEra (ShelleyAddr
                     writeFileTextEnvelope)
 import qualified Cardano.Api as Api (Value)
 import Cardano.Api.Shelley (ProtocolParameters, ReferenceScript (ReferenceScriptNone),
-                            StakeCredential (StakeCredentialByKey, StakeCredentialByScript), fromPlutusData)
+                            StakeCredential (StakeCredentialByKey, StakeCredentialByScript), fromPlutusData,
+                            toShelleyAddr)
+import Cardano.Wallet.Api.Types (AnyAddress)
 import Cardano.Wallet.Shelley.Compatibility (toCardanoStakeCredential)
 import Control.Monad (forM_, guard, unless, when)
 import Control.Monad.Except (MonadError, MonadIO, catchError, liftIO, throwError)
@@ -317,15 +319,17 @@ runTransaction connection marloweInBundle marloweOutFile inputs outputs changeAd
                                                           ]
                                                     pure ([spend'], Just collateral, merkles)
     let
-      alonzoToAddressAny' :: AddressInEra AlonzoEra -> AddressAny
-      alonzoToAddressAny' (AddressInEra _ address) = toAddressAny address
-
       babbageToAddressAny' :: AddressInEra BabbageEra -> AddressAny
       babbageToAddressAny' (AddressInEra _ address) = toAddressAny address
 
       network = localNodeNetworkId connection
+
       scriptAddress :: AddressInEra BabbageEra
       scriptAddress = viAddress $ mtValidator marloweOut
+
+      scriptAddressInAlonzo :: AddressInEra AlonzoEra
+      scriptAddressInAlonzo =  anyAddressInShelleyBasedEra (babbageToAddressAny' scriptAddress)
+
       outputDatum = diDatum $ buildDatum (mtContract marloweOut) (mtState marloweOut)
     outputValue <-
       mconcat
@@ -340,7 +344,8 @@ runTransaction connection marloweInBundle marloweOutFile inputs outputs changeAd
         do
           guard (outputValue /= mempty)
           pure
-            $ buildPayToScript scriptAddress outputValue outputDatum
+            $ buildPayToScript scriptAddressInAlonzo outputValue outputDatum
+
       roleAddress = viAddress $ mtRoleValidator marloweOut :: AddressInEra BabbageEra
     payments <-
       catMaybes
