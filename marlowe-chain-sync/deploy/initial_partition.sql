d8a9d0856952eba210d6a098470b0acae4fa2a80
Jamie Bertram <jamie.bertram@tweag.io>
2022-07-28 11:53:50 -0400

    Use pg_temp for temp function

--- marlowe-chain-sync/deploy/initial_partition.sql
+++ marlowe-chain-sync/deploy/initial_partition.sql
@@ -5,7 +5,7 @@
 
 BEGIN;
 
-CREATE FUNCTION chain.createInitialPartitions(name text)
+CREATE FUNCTION pg_temp.createInitialPartitions(name text)
 RETURNS VOID
 LANGUAGE plpgsql
 AS $$
@@ -17,11 +17,9 @@ BEGIN
 END;
 $$;
 
-SELECT chain.createInitialPartitions('block');
-SELECT chain.createInitialPartitions('tx');
-SELECT chain.createInitialPartitions('txOut');
-SELECT chain.createInitialPartitions('txIn');
-
-DROP FUNCTION chain.createInitialPartitions;
+SELECT pg_temp.createInitialPartitions('block');
+SELECT pg_temp.createInitialPartitions('tx');
+SELECT pg_temp.createInitialPartitions('txOut');
+SELECT pg_temp.createInitialPartitions('txIn');
 
 COMMIT;
