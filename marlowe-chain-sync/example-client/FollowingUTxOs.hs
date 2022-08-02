9a0004d828ce0d2f8bfb685cb6438c88eacadc33
Jamie Bertram <jamie.bertram@tweag.io>
2022-08-02 10:55:38 -0400

    Extract example clients into separate modules

---
+++ marlowe-chain-sync/example-client/FollowingUTxOs.hs
@@ -0,0 +1,47 @@
+module FollowingUTxOs where
+
+import Data.List (uncons)
+import Data.Maybe (fromMaybe)
+import Language.Marlowe.Runtime.ChainSync.Api
+import Network.Protocol.ChainSeek.Client
+
+-- This client follows a Genesis TxOut through transactions until it finds a
+-- UTxO at index 0 of some tx.
+client :: RuntimeChainSeekClient IO ()
+client = ChainSeekClient stInit
+  where
+    stInit = pure $ SendMsgRequestHandshake schemaVersion1_0 stHandshake
+
+    stHandshake = ClientStHandshake
+      { recvMsgHandshakeRejected = \supportedVersions -> do
+          putStr "Schema version not supported by server. Supported versions: "
+          print supportedVersions
+      , recvMsgHandshakeConfirmed = stIdle []
+      }
+
+    stIdle hist = do
+      let
+        (txOut, hist') = fromMaybe
+          (TxOutRef "0a3eecbba8c47b5d941a4a06b3661a664c7290c071212365bb0fbc2e8ccd4112" 0, [])
+          $ uncons hist
+      putStrLn $ "Waiting for UTxO to be consumed: " <> show txOut
+      pure $ SendMsgQueryNext (ConsumeUTxO txOut) (stNext txOut hist') $ pure $ stNext txOut hist'
+
+    stNext txOut hist = ClientStNext
+      { recvMsgQueryRejected = \err _ -> case err of
+          UTxONotFound -> do
+            putStrLn $ "TxOut not found: " <> show txOut
+            putStrLn "Exiting"
+            pure $ SendMsgDone ()
+          UTxOSpent txId -> do
+            putStrLn $ "TxOut already spent: " <> show txOut
+            putStrLn $ "Spent by tx: " <> show txId
+            putStrLn "Exiting"
+            pure $ SendMsgDone ()
+      , recvMsgRollForward = \Transaction{..} _ _ -> do
+          putStrLn $ "UTxO spent by transaction: " <> show txId
+          stIdle $ TxOutRef txId 0 : hist
+      , recvMsgRollBackward = \_ _ -> do
+          putStr "Rolling back"
+          stIdle hist
+      }
--- marlowe-chain-sync/example-client/Main.hs
+++ marlowe-chain-sync/example-client/Main.hs
@@ -2,74 +2,51 @@ module Main where
 
 import Control.Exception (bracket, bracketOnError, throwIO)
 import Data.Functor (void)
-import Data.Void (absurd)
-import Language.Marlowe.Runtime.ChainSync.Api (Move (..), TxOutRef (..), UTxOError (..), WithGenesis (..),
-                                               runtimeChainSeekCodec, schemaVersion1_0)
+import qualified FollowingUTxOs
+import Language.Marlowe.Runtime.ChainSync.Api (WithGenesis (..), runtimeChainSeekCodec)
 import Network.Channel (socketAsChannel)
-import Network.Protocol.ChainSeek.Client (ChainSeekClient (ChainSeekClient), ClientStHandshake (..), ClientStIdle (..),
-                                          ClientStInit (..), ClientStNext (..), chainSeekClientPeer)
+import Network.Protocol.ChainSeek.Client (chainSeekClientPeer)
 import Network.Protocol.Driver (mkDriver)
 import Network.Socket (AddrInfo (..), HostName, PortNumber, Socket, SocketType (..), close, connect, defaultHints,
                        getAddrInfo, openSocket)
 import Network.TypedProtocol (Driver (..), runPeerWithDriver)
+import Numeric.Natural (Natural)
+import Options.Applicative (auto)
 import qualified Options.Applicative as O
+import qualified SkippingBlocks
 
 main :: IO ()
 main = do
   Options{..} <- getOptions
   let hints = defaultHints { addrSocketType = Stream }
   addr <- head <$> getAddrInfo (Just hints) (Just host) (Just $ show port)
-  bracket (open addr) close run
+  bracket (open addr) close (run example)
   where
     open addr = bracketOnError (openSocket addr) close \sock -> do
       connect sock $ addrAddress addr
       pure sock
 
--- | This example client skips every 1000 blocks until it catches up to the
--- tip, at which point it requests one block at a time.
-run :: Socket -> IO ()
-run conn = void $ runPeerWithDriver driver peer (startDState driver)
+run :: Natural -> Socket -> IO ()
+run example conn = void $ runPeerWithDriver driver peer (startDState driver)
   where
     driver = mkDriver throwIO runtimeChainSeekCodec channel
     channel = socketAsChannel conn
     peer = chainSeekClientPeer Genesis client
-    client = ChainSeekClient $ pure $ SendMsgRequestHandshake schemaVersion1_0 stHandshake
-    stHandshake = ClientStHandshake
-      { recvMsgHandshakeRejected = \supportedVersions -> do
-          putStr "Schema version not supported by server. Supported versions: "
-          print supportedVersions
-      , recvMsgHandshakeConfirmed = stIdleGenesis
-      }
-    stIdleGenesis = pure $ SendMsgQueryNext (AdvanceSlots 25644) stNextGenesis (pure stNextGenesis)
-    stNextGenesis = ClientStNext
-      { recvMsgQueryRejected = absurd
-      , recvMsgRollForward = \_ _ _ -> stIdleConsume
-      , recvMsgRollBackward = \_ _ -> stIdleGenesis
-      }
-    stIdleConsume = pure $ SendMsgQueryNext (ConsumeUTxO (TxOutRef "59b32f725ef2909ed3af209d92f97a0a29198ed9b85b9a2ac3ca4bdd05817a53" 0)) stNext (pure stNext)
-    stNext = ClientStNext
-      { recvMsgQueryRejected = \case
-          UTxONotFound -> error "UTxO not found"
-          UTxOSpent _  -> error "UTxO already spent"
-      , recvMsgRollForward = \tx _ _ -> do
-          putStr "UTxO spent by transaction: "
-          print tx
-          pure $ SendMsgDone ()
-      , recvMsgRollBackward = \point _ -> do
-          putStr "Roll backward: "
-          print point
-          stIdleConsume
-      }
+    client = case example of
+      0 -> SkippingBlocks.client
+      1 -> FollowingUTxOs.client
+      _ -> error "Invalid example"
 
 data Options = Options
-  { port :: !PortNumber
-  , host :: !HostName
+  { port    :: !PortNumber
+  , host    :: !HostName
+  , example :: !Natural
   }
 
 getOptions :: IO Options
 getOptions = O.execParser $ O.info parser infoMod
   where
-    parser = O.helper <*> (Options <$> portParser <*> hostParser)
+    parser = O.helper <*> (Options <$> portParser <*> hostParser <*> exampleParser)
     portParser = O.option O.auto $ mconcat
       [ O.long "port-number"
       , O.short 'p'
@@ -83,6 +60,10 @@ getOptions = O.execParser $ O.info parser infoMod
       , O.value "127.0.0.1"
       , O.metavar "HOST_NAME"
       , O.help "The hostname of the chain sync server to connect to"
+      ]
+    exampleParser = O.argument auto $ mconcat
+      [ O.metavar "EXAMPLE_NUM"
+      , O.help "The example to run: 0: skipping blocks, 1: following UTxOs"
       ]
     infoMod = mconcat
       [ O.fullDesc
