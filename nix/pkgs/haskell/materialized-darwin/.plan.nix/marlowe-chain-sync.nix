5ff9b2478adb751581c7d70ee4154a3c5284f8d6
Tomasz Rybarczyk <tomasz.rybarczyk@iohk.io>
2022-08-04 12:45:56 +0200

    $ updateMaterialized

---
+++ nix/pkgs/haskell/materialized-darwin/.plan.nix/marlowe-chain-sync.nix
@@ -0,0 +1,136 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.4";
+      identifier = { name = "marlowe-chain-sync"; version = "0.0.0.0"; };
+      license = "Apache-2.0";
+      copyright = "";
+      maintainer = "jamie.bertram@iohk.io";
+      author = "Jamie Bertram";
+      homepage = "";
+      url = "";
+      synopsis = "Cardano chain sync system for thee Marlowe Runtime";
+      description = "Marlowe runtime component for Cardano node synchronization. Communicates with\ndownstream compoents using the Chain Seek protocol, which provides\nefficient push and pull-based traversal of the cardano blockchain.";
+      buildType = "Simple";
+      isLocal = true;
+      detailLevel = "FullDetails";
+      licenseFiles = [ "LICENSE" "NOTICE" ];
+      dataDir = ".";
+      dataFiles = [];
+      extraSrcFiles = [];
+      extraTmpFiles = [];
+      extraDocFiles = [];
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."async" or (errorHandler.buildDepError "async"))
+          (hsPkgs."base16" or (errorHandler.buildDepError "base16"))
+          (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."cborg" or (errorHandler.buildDepError "cborg"))
+          (hsPkgs."cardano-api" or (errorHandler.buildDepError "cardano-api"))
+          (hsPkgs."cardano-binary" or (errorHandler.buildDepError "cardano-binary"))
+          (hsPkgs."cardano-crypto-wrapper" or (errorHandler.buildDepError "cardano-crypto-wrapper"))
+          (hsPkgs."cardano-ledger-byron" or (errorHandler.buildDepError "cardano-ledger-byron"))
+          (hsPkgs."cardano-ledger-babbage" or (errorHandler.buildDepError "cardano-ledger-babbage"))
+          (hsPkgs."cardano-ledger-alonzo" or (errorHandler.buildDepError "cardano-ledger-alonzo"))
+          (hsPkgs."cardano-ledger-core" or (errorHandler.buildDepError "cardano-ledger-core"))
+          (hsPkgs."cardano-ledger-shelley" or (errorHandler.buildDepError "cardano-ledger-shelley"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."foldl" or (errorHandler.buildDepError "foldl"))
+          (hsPkgs."hasql" or (errorHandler.buildDepError "hasql"))
+          (hsPkgs."hasql-th" or (errorHandler.buildDepError "hasql-th"))
+          (hsPkgs."hasql-transaction" or (errorHandler.buildDepError "hasql-transaction"))
+          (hsPkgs."marlowe-protocols" or (errorHandler.buildDepError "marlowe-protocols"))
+          (hsPkgs."ouroboros-consensus-shelley" or (errorHandler.buildDepError "ouroboros-consensus-shelley"))
+          (hsPkgs."ouroboros-network" or (errorHandler.buildDepError "ouroboros-network"))
+          (hsPkgs."parallel" or (errorHandler.buildDepError "parallel"))
+          (hsPkgs."plutus-ledger-api" or (errorHandler.buildDepError "plutus-ledger-api"))
+          (hsPkgs."profunctors" or (errorHandler.buildDepError "profunctors"))
+          (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
+          (hsPkgs."stm-delay" or (errorHandler.buildDepError "stm-delay"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."these" or (errorHandler.buildDepError "these"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          (hsPkgs."typed-protocols" or (errorHandler.buildDepError "typed-protocols"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          (hsPkgs."witherable" or (errorHandler.buildDepError "witherable"))
+          ];
+        buildable = true;
+        modules = [
+          "Language/Marlowe/Runtime/ChainSync"
+          "Language/Marlowe/Runtime/ChainSync/Database"
+          "Language/Marlowe/Runtime/ChainSync/Database/PostgreSQL"
+          "Language/Marlowe/Runtime/ChainSync/Genesis"
+          "Language/Marlowe/Runtime/ChainSync/NodeClient"
+          "Language/Marlowe/Runtime/ChainSync/Server"
+          "Language/Marlowe/Runtime/ChainSync/Store"
+          "Language/Marlowe/Runtime/ChainSync/Api"
+          ];
+        hsSourceDirs = [ "src" ];
+        };
+      exes = {
+        "chainseekd" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."base16" or (errorHandler.buildDepError "base16"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."cardano-api" or (errorHandler.buildDepError "cardano-api"))
+            (hsPkgs."cardano-crypto-wrapper" or (errorHandler.buildDepError "cardano-crypto-wrapper"))
+            (hsPkgs."cardano-ledger-byron" or (errorHandler.buildDepError "cardano-ledger-byron"))
+            (hsPkgs."hasql" or (errorHandler.buildDepError "hasql"))
+            (hsPkgs."hasql-pool" or (errorHandler.buildDepError "hasql-pool"))
+            (hsPkgs."marlowe-chain-sync" or (errorHandler.buildDepError "marlowe-chain-sync"))
+            (hsPkgs."marlowe-protocols" or (errorHandler.buildDepError "marlowe-protocols"))
+            (hsPkgs."network" or (errorHandler.buildDepError "network"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            (hsPkgs."ouroboros-network" or (errorHandler.buildDepError "ouroboros-network"))
+            (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            ];
+          buildable = true;
+          modules = [ "Options" "Paths_marlowe_chain_sync" ];
+          hsSourceDirs = [ "app" ];
+          mainPath = [ "Main.hs" ];
+          };
+        "example-client" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."cardano-api" or (errorHandler.buildDepError "cardano-api"))
+            (hsPkgs."marlowe-chain-sync" or (errorHandler.buildDepError "marlowe-chain-sync"))
+            (hsPkgs."marlowe-protocols" or (errorHandler.buildDepError "marlowe-protocols"))
+            (hsPkgs."network" or (errorHandler.buildDepError "network"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."these" or (errorHandler.buildDepError "these"))
+            (hsPkgs."typed-protocols" or (errorHandler.buildDepError "typed-protocols"))
+            ];
+          buildable = true;
+          modules = [
+            "SkippingBlocks"
+            "FollowingUTxOs"
+            "Paths_marlowe_chain_sync"
+            ];
+          hsSourceDirs = [ "example-client" ];
+          mainPath = [ "Main.hs" ];
+          };
+        };
+      };
+    } // rec { src = (pkgs.lib).mkDefault ../marlowe-chain-sync; }
---
+++ nix/pkgs/haskell/materialized-windows/.plan.nix/marlowe-protocols.nix
@@ -0,0 +1,57 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.4";
+      identifier = { name = "marlowe-protocols"; version = "0.0.0.0"; };
+      license = "Apache-2.0";
+      copyright = "";
+      maintainer = "jamie.bertram@iohk.io";
+      author = "Jamie Bertram";
+      homepage = "";
+      url = "";
+      synopsis = "Protocol definitions for Marlowe";
+      description = "";
+      buildType = "Simple";
+      isLocal = true;
+      detailLevel = "FullDetails";
+      licenseFiles = [ "LICENSE" "NOTICE" ];
+      dataDir = ".";
+      dataFiles = [];
+      extraSrcFiles = [];
+      extraTmpFiles = [];
+      extraDocFiles = [];
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."network" or (errorHandler.buildDepError "network"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."typed-protocols" or (errorHandler.buildDepError "typed-protocols"))
+          ];
+        buildable = true;
+        modules = [
+          "Network/Channel"
+          "Network/Protocol/Driver"
+          "Network/Protocol/ChainSeek/Client"
+          "Network/Protocol/ChainSeek/Codec"
+          "Network/Protocol/ChainSeek/Server"
+          "Network/Protocol/ChainSeek/Types"
+          ];
+        hsSourceDirs = [ "src" ];
+        };
+      };
+    } // rec {
+    src = (pkgs.lib).mkDefault ../marlowe-chain-sync/marlowe-protocols;
+    }
