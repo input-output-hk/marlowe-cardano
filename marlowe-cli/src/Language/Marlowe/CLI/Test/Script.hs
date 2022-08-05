408f83729c38b29ed99297a2ddd0064e8c218a95
Larry Adames <adames.larry@gmail.com>
2022-08-05 10:41:16 -0400

    SCP-4225 wip on annotation of error

--- marlowe-cli/src/Language/Marlowe/CLI/Test/Script.hs
+++ marlowe-cli/src/Language/Marlowe/CLI/Test/Script.hs
@@ -169,6 +169,11 @@ interpret Prepare {..} = do
 
 interpret (Fail message) = throwError $ CliError message
 
+anotateCliError :: String -> CliError -> CliError
+anotateCliError anotation (CliError msg) = CliError $ annotation <> msg
+
+asPrepareError = annotateError "Prepare: "
+
 withError :: MonadError e m => (e -> e) -> m a -> m a
 withError modifyError action = catchError action \e -> do
                                 throwError $ modifyError e
