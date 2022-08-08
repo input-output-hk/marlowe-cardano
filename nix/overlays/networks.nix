cee74c542d86557e2bef577544cdc0f90e0b2079
Jamie Bertram <jamie.bertram@tweag.io>
2022-08-08 09:38:28 -0400

    Remove pab stuff

--- nix/overlays/networks.nix
+++ nix/overlays/networks.nix
@@ -14,20 +14,13 @@ in
 {
   networks = {
     testnet-pioneers = mkNetwork environments.marlowe-pioneers // {
-      slotZeroTime = 1649949631000;
-      slotLengthMillis = 1000;
-      pabResumeFrom = { tag = "PointAtGenesis"; };
       magic = 1567;
     };
     testnet-dev = mkNetwork environments.marlowe-dev // {
-      slotZeroTime = 1649976791000;
-      slotLengthMillis = 1000;
-      pabResumeFrom = {
-        tag = "Point";
-        pointBlockId = "8d10922f8368d5426e1851b118e3f626725167593c04ccd28b0012d490ec8768";
-        pointSlot = { getSlot = 3999983; };
-      };
       magic = 1566;
+    };
+    testnet = mkNetwork environments.testnet // {
+      magic = 1097911063;
     };
     # TODO add testnet-public, mainnet in the future
   };
