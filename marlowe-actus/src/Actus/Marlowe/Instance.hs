5c2f90db91eca2a00071eef01957d4b1cc63ab1a
Yves Hauser <yves.hauser@gmail.com>
2022-08-09 22:41:20 +0200

    Try to evaluate `Value`s before reducing the structure

--- marlowe-actus/src/Actus/Marlowe/Instance.hs
+++ marlowe-actus/src/Actus/Marlowe/Instance.hs
@@ -18,6 +18,7 @@ module Actus.Marlowe.Instance
 where
 
 import Actus.Domain (CashFlow, ContractState, ContractTerms, RiskFactors)
+import Control.Applicative (liftA2)
 import GHC.Real (Ratio (..))
 import Language.Marlowe.Extended.V1
 
@@ -46,6 +47,9 @@ instance Num Value where
     where _max x y = Cond (ValueGT x y) x y
   fromInteger n = Constant $ n * marloweFixedPoint
   negate a = NegValue a
+  signum a | evalVal a > Just 0 = 1
+  signum a | evalVal a < Just 0 = -1
+  signum a | evalVal a == Just 0 = 0
   signum _ = error "Num partially implemented"
 
 instance Fractional Value where
@@ -53,14 +57,16 @@ instance Fractional Value where
   fromRational (x:%y) = DivValue (fromInteger (marloweFixedPoint * x)) (fromInteger y)
 
 instance Eq Value where
-  n == m | evalVal n == evalVal m = True
-  n == m | evalVal n /= evalVal m = False
-  (==) _ _ = error "Eq partially implemented"
+  m == n = equals (evalVal m) (evalVal n)
+    where
+      equals :: Maybe Integer -> Maybe Integer -> Bool
+      equals (Just i) (Just j) = i == j
+      equals _ _               = error "Eq partially implemented"
 
 instance Ord Value where
-  n `compare` m | evalVal n < evalVal m = LT
-  n `compare` m | evalVal n > evalVal m = GT
-  n `compare` m | evalVal n == evalVal m = EQ
+  m `compare` n | evalVal m < evalVal n = LT
+  m `compare` n | evalVal m > evalVal n = GT
+  m `compare` n | evalVal m == evalVal n = EQ
   compare _ _ = error "Ord partially implemented"
 
 instance Real Value where
@@ -77,38 +83,40 @@ reduceContract Close = Close
 reduceContract (Pay a b c d e) = Pay a b c (reduceValue d) (reduceContract e)
 reduceContract (When cs t c) = When (map f cs) t (reduceContract c)
   where
-    f (Case a x)           = Case a (reduceContract x)
-reduceContract (If obs a b) = let c = evalObs obs in if c then reduceContract a else reduceContract b
+    f (Case a x) = Case a (reduceContract x)
+reduceContract i@(If obs a b) = case evalObs obs of
+  Just c  -> if c then reduceContract a else reduceContract b
+  Nothing -> i
 reduceContract (Let v o c) = Let v (reduceValue o) (reduceContract c)
 reduceContract (Assert o c) = Assert (reduceObs o) (reduceContract c)
 
-evalObs :: Observation -> Bool
-evalObs (AndObs lhs rhs)   = evalObs lhs && evalObs rhs
-evalObs (OrObs lhs rhs)    = evalObs lhs || evalObs rhs
-evalObs (NotObs subObs)    = not (evalObs subObs)
-evalObs (ChoseSomething _) = error "evalObs not implemented for ChoseSomething"
-evalObs (ValueGE lhs rhs)  = evalVal lhs >= evalVal rhs
-evalObs (ValueGT lhs rhs)  = evalVal lhs > evalVal rhs
-evalObs (ValueLT lhs rhs)  = evalVal lhs < evalVal rhs
-evalObs (ValueLE lhs rhs)  = evalVal lhs <= evalVal rhs
-evalObs (ValueEQ lhs rhs)  = evalVal lhs == evalVal rhs
-evalObs TrueObs            = True
-evalObs FalseObs           = False
+evalObs :: Observation -> Maybe Bool
+evalObs (AndObs lhs rhs)   = liftA2 (&&) (evalObs lhs) (evalObs rhs)
+evalObs (OrObs lhs rhs)    = liftA2 (||) (evalObs lhs) (evalObs rhs)
+evalObs (NotObs subObs)    = not <$> evalObs subObs
+evalObs (ChoseSomething _) = Nothing
+evalObs (ValueGT lhs rhs)  = liftA2 (>) (evalVal lhs) (evalVal rhs)
+evalObs (ValueLT lhs rhs)  = liftA2 (<) (evalVal lhs) (evalVal rhs)
+evalObs (ValueGE lhs rhs)  = liftA2 (>=) (evalVal lhs) (evalVal rhs)
+evalObs (ValueLE lhs rhs)  = liftA2 (<=) (evalVal lhs) (evalVal rhs)
+evalObs (ValueEQ lhs rhs)  = liftA2 (==) (evalVal lhs) (evalVal rhs)
+evalObs TrueObs            = Just True
+evalObs FalseObs           = Just False
 
-evalVal :: Value -> Integer
-evalVal (AvailableMoney _ _) = error "evalVal not implemented for AvailableMoney"
-evalVal (Constant n)         = n
-evalVal (ConstantParam _)    = error "evalVal not implemented for ConstantParam"
-evalVal (NegValue val)       = negate (evalVal val)
-evalVal (AddValue lhs rhs)   = evalVal lhs + evalVal rhs
-evalVal (SubValue lhs rhs)   = evalVal lhs - evalVal rhs
-evalVal (MulValue lhs rhs)   = evalVal lhs * evalVal rhs
-evalVal (DivValue lhs rhs)   = evalVal lhs `quot` evalVal rhs
-evalVal (ChoiceValue _)      = error "evalVal not implemented for ChoiceValue"
-evalVal TimeIntervalStart    = error "evalVal not implemented for TimeIntervalStart"
-evalVal TimeIntervalEnd      = error "evalVal not implemented for TimeIntervalEnd"
-evalVal (UseValue _)         = error "evalVal not implemented for UseValue"
-evalVal (Cond o a b)         = if evalObs o then evalVal a else evalVal b
+evalVal :: Value -> Maybe Integer
+evalVal (AvailableMoney _ _) = Nothing
+evalVal (Constant n)         = Just n
+evalVal (ConstantParam _)    = Nothing
+evalVal (NegValue val)       = negate <$> evalVal val
+evalVal (AddValue lhs rhs)   = liftA2 (+) (evalVal lhs) (evalVal rhs)
+evalVal (SubValue lhs rhs)   = liftA2 (-) (evalVal lhs) (evalVal rhs)
+evalVal (MulValue lhs rhs)   = liftA2 (*) (evalVal lhs) (evalVal rhs)
+evalVal (DivValue lhs rhs)   = liftA2 quot (evalVal lhs) (evalVal rhs)
+evalVal (ChoiceValue _)      = Nothing
+evalVal TimeIntervalStart    = Nothing
+evalVal TimeIntervalEnd      = Nothing
+evalVal (UseValue _)         = Nothing
+evalVal (Cond o a b)         = evalObs o >>= \obs -> if obs then evalVal a else evalVal b
 
 reduceObs :: Observation -> Observation
 reduceObs (AndObs a b)  = AndObs (reduceObs a) (reduceObs b)
@@ -122,4 +130,4 @@ reduceObs (ValueEQ a b) = ValueEQ (reduceValue a) (reduceValue b)
 reduceObs x             = x
 
 reduceValue :: Value -> Value
-reduceValue v = Constant $ evalVal v
+reduceValue v = maybe v Constant (evalVal v)
