270f0f32d808fff37b24c590a2504716dbf564da
Dino Morelli <dino.morelli@iohk.io>
2022-08-26 16:18:40 -0400

    Added familiar green prompt to `nix develop`
    `nix develop` doesn't change the prompt like `nix-shell` did which
    turned out to be a handy visual indicator that you were in the
    development shell. This change replicates that prompt style and color.

--- dev-shell.nix
+++ dev-shell.nix
@@ -101,6 +101,9 @@ let
 
   defaultShellHook = ''
     ${pre-commit-check.shellHook}
+
+    # The green prompt familiar to those used to nix-shell
+    export PS1="\n\[\033[1;32m\][nix develop:\w]\$\[\033[0m\] "
   ''
   # Work around https://github.com/NixOS/nix/issues/3345, which makes
   # tests etc. run single-threaded in a nix shell.
