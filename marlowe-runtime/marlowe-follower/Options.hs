8ab1960236fcb6c4994bddca15985f632516d454
Jamie Bertram <jamie.bertram@tweag.io>
2022-08-29 09:49:20 -0400

    Extract follower options to separate module

---
+++ marlowe-runtime/marlowe-follower/Options.hs
@@ -0,0 +1,51 @@
+module Options where
+
+import Language.Marlowe.Runtime.Core.Api (ContractId (..), parseContractId)
+import Network.Socket (HostName, PortNumber)
+import Options.Applicative (argument, auto, execParser, fullDesc, header, help, helper, info, long, maybeReader,
+                            metavar, option, progDesc, short, strOption, value)
+
+data Options = Options
+  { port       :: PortNumber
+  , queryPort  :: PortNumber
+  , host       :: HostName
+  , contractId :: ContractId
+  }
+
+getOptions :: IO Options
+getOptions = execParser $ info (helper <*> parser) infoMod
+  where
+    parser = Options <$> portParser <*> queryPortParser <*> hostParser <*> contractIdParser
+
+    portParser = option auto $ mconcat
+      [ long "port-number"
+      , value 3715
+      , metavar "PORT_NUMBER"
+      , help "The port number of the chain seek server. Default value: 3715"
+      ]
+
+    queryPortParser = option auto $ mconcat
+      [ long "query-port-number"
+      , value 3716
+      , metavar "PORT_NUMBER"
+      , help "The query port number of the chain seek server. Default value: 3716"
+      ]
+
+    hostParser = strOption $ mconcat
+      [ long "host"
+      , short 'h'
+      , value "127.0.0.1"
+      , metavar "HOST_NAME"
+      , help "The host name of the chain seek server. Default value: 127.0.0.1"
+      ]
+
+    contractIdParser = argument (maybeReader parseContractId) $ mconcat
+      [ metavar "CONTRACT_ID"
+      , help "The UTxO that created the contract"
+      ]
+
+    infoMod = mconcat
+      [ fullDesc
+      , progDesc "Contract follower for Marlowe Runtime"
+      , header "marlowe-follower : a contract follower for the Marlowe Runtime."
+      ]
