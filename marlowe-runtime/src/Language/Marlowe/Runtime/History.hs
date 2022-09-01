d39b6815340579afcbec13c89739dfe496ddd876
Jamie Bertram <jamie.bertram@tweag.io>
2022-09-01 17:13:36 -0400

    Refactor and fix multiple steps per block logic

--- marlowe-runtime/src/Language/Marlowe/Runtime/History.hs
+++ marlowe-runtime/src/Language/Marlowe/Runtime/History.hs
@@ -6,11 +6,10 @@
 module Language.Marlowe.Runtime.History where
 
 import Control.Concurrent.Async (Concurrently (..))
-import Control.Concurrent.STM (STM, atomically)
-import Control.Monad (when)
+import Control.Concurrent.STM (STM)
 import Data.Foldable (asum)
 import Language.Marlowe.Runtime.ChainSync.Api (RuntimeChainSeekClient, ScriptHash, SlotConfig)
-import Language.Marlowe.Runtime.Core.Api (SomeMarloweVersion, parseContractId)
+import Language.Marlowe.Runtime.Core.Api (SomeMarloweVersion)
 import Language.Marlowe.Runtime.History.FollowerSupervisor
 import Language.Marlowe.Runtime.History.JobServer
 import Language.Marlowe.Runtime.History.QueryServer
@@ -43,33 +42,11 @@ mkHistory HistoryDependencies{..} = do
   HistoryQueryServer{..} <- mkHistoryQueryServer HistoryQueryServerDependencies{..}
   HistoryStore{..} <- mkHistoryStore HistoryStoreDependencies{..}
   HistorySyncServer{..} <- mkHistorySyncServer HistorySyncServerDependencies{..}
-  let
-    repl = do
-      line <- getLine
-      loop <- case words line of
-
-        ["add", cidRaw] -> True <$ case parseContractId cidRaw of
-          Nothing  -> putStrLn "invalid cid"
-          Just cid -> print =<< atomically (followContract cid)
-
-        ["rm", cidRaw] -> True <$ case parseContractId cidRaw of
-          Nothing  -> putStrLn "invalid cid"
-          Just cid -> print =<< atomically (stopFollowingContract cid)
-
-        ["status"] -> do
-          print =<< atomically followerStatuses
-          pure True
-
-        ["quit"] -> pure False
-
-        _ -> True <$ putStrLn "invalid command"
-      when loop repl
   pure History
     { runHistory = runConcurrently $ asum $ Concurrently <$>
         [ runFollowerSupervisor
         , runHistoryJobServer
         , runHistoryQueryServer
-        , putStrLn "enter a command" *> repl
         , runHistoryStore
         , runHistorySyncServer
         ]
