2e95a84a99e93c724e5c65f2e4b8a0ebe287a58a
Jamie Bertram <jamie.bertram@tweag.io>
2022-09-26 15:13:41 -0400

    Add monad and env type for marlowe CLI

---
+++ marlowe-runtime/cli/Language/Marlowe/Runtime/CLI/Env.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE RankNTypes #-}
+
+module Language.Marlowe.Runtime.CLI.Env
+  where
+
+import Control.Exception (Exception, bracket, bracketOnError, throwIO)
+import Data.ByteString.Lazy (ByteString)
+import Language.Marlowe.Protocol.Sync.Client (MarloweSyncClient)
+import Language.Marlowe.Runtime.History.Api (HistoryCommand, HistoryQuery)
+import Language.Marlowe.Runtime.Transaction.Api (MarloweTxCommand)
+import Network.Channel (socketAsChannel)
+import Network.Protocol.Driver (mkDriver)
+import Network.Protocol.Job.Client (JobClient)
+import Network.Protocol.Query.Client (QueryClient)
+import Network.Socket (AddrInfo, addrAddress, close, connect, openSocket)
+import Network.TypedProtocol (Driver(startDState), Peer, PeerRole(AsClient), runPeerWithDriver)
+import Network.TypedProtocol.Codec (Codec)
+
+-- | A function signature for running a client for some protocol in some monad m.
+type RunClient m client = forall a. client m a -> m a
+
+-- | The environment for the Marlowe Runtime CLI.
+data Env m = Env
+  { envRunHistoryJobClient :: !(RunClient m (JobClient HistoryCommand))
+  , envRunHistoryQueryClient :: !(RunClient m (QueryClient HistoryQuery))
+  , envRunHistorySyncClient :: !(RunClient m MarloweSyncClient)
+  , envRunTxJobClient :: !(RunClient m (JobClient MarloweTxCommand))
+  }
+
+-- | Run a client as a typed protocols peer over a socket.
+runClientPeerOverSocket
+  :: Exception ex
+  => AddrInfo -- ^ Socket address to connect to
+  -> Codec protocol ex IO ByteString -- ^ A codec for the protocol
+  -> (forall a. client IO a -> Peer protocol 'AsClient st IO a) -- ^ Interpret the client as a protocol peer
+  -> RunClient IO client
+runClientPeerOverSocket addr codec clientToPeer client = bracket open close \socket -> do
+  let channel = socketAsChannel socket
+  let driver = mkDriver throwIO codec channel
+  let peer = clientToPeer client
+  fst <$> runPeerWithDriver driver peer (startDState driver)
+  where
+    open = bracketOnError (openSocket addr) close \sock -> do
+      connect sock $ addrAddress addr
+      pure sock
---
+++ marlowe-runtime/cli/Language/Marlowe/Runtime/CLI/Monad.hs
@@ -0,0 +1,74 @@
+{-# LANGUAGE UndecidableInstances #-}
+
+module Language.Marlowe.Runtime.CLI.Monad
+  where
+
+import Control.Monad (MonadPlus)
+import Control.Monad.Base (MonadBase)
+import Control.Monad.Fix (MonadFix)
+import Control.Monad.IO.Class (MonadIO(..))
+import Control.Monad.Trans.Control (MonadBaseControl)
+import Control.Monad.Trans.Reader (ReaderT, ask, asks, local)
+import Data.Void (Void)
+import Language.Marlowe.Protocol.Sync.Client (MarloweSyncClient)
+import Language.Marlowe.Runtime.CLI.Env (Env(..))
+import Language.Marlowe.Runtime.History.Api (HistoryCommand, HistoryQuery)
+import Language.Marlowe.Runtime.Transaction.Api (MarloweTxCommand)
+import Network.Protocol.Job.Client (JobClient, liftCommand)
+import Network.Protocol.Query.Client (QueryClient, liftQuery)
+import Options.Applicative (Alternative)
+
+-- | A monad type for Marlowe Runtime CLI programs.
+newtype CLI a = CLI { runCLI :: ReaderT (Env IO) IO a }
+  deriving newtype
+    ( Functor
+    , Applicative
+    , Monad
+    , MonadFix
+    , MonadFail
+    , MonadIO
+    , Alternative
+    , MonadPlus
+    , MonadBase IO
+    , MonadBaseControl IO
+    )
+
+-- | Get the environment.
+askEnv :: CLI (Env IO)
+askEnv = CLI ask
+
+-- | Get a value from the environment.
+asksEnv :: (Env IO -> a) -> CLI a
+asksEnv = CLI . asks
+
+-- | Run a CLI program in a locally modified environment.
+localEnv :: (Env IO -> Env IO) -> CLI a -> CLI a
+localEnv f = CLI . local f . runCLI
+
+-- | Run a History Job client.
+runHistoryJobClient :: JobClient HistoryCommand IO a -> CLI a
+runHistoryJobClient client = liftIO . ($ client) =<< asksEnv envRunHistoryJobClient
+
+-- | Run a History Query client.
+runHistoryQueryClient :: QueryClient HistoryQuery IO a -> CLI a
+runHistoryQueryClient client = liftIO . ($ client) =<< asksEnv envRunHistoryQueryClient
+
+-- | Run a Marlowe Sync client.
+runHistorySyncClient :: MarloweSyncClient IO a -> CLI a
+runHistorySyncClient client = liftIO . ($ client) =<< asksEnv envRunHistorySyncClient
+
+-- | Run a Tx Job client.
+runTxJobClient :: JobClient MarloweTxCommand IO a -> CLI a
+runTxJobClient client = liftIO . ($ client) =<< asksEnv envRunTxJobClient
+
+-- | Run a simple History command.
+runHistoryCommand :: HistoryCommand Void err result -> CLI (Either err result)
+runHistoryCommand = runHistoryJobClient . liftCommand
+
+-- | Run a simple History query.
+queryHistory :: HistoryQuery Void err results -> CLI (Either err results)
+queryHistory = runHistoryQueryClient . liftQuery
+
+-- | Run a simple Tx command.
+runTxCommand :: MarloweTxCommand Void err result -> CLI (Either err result)
+runTxCommand = runTxJobClient . liftCommand
