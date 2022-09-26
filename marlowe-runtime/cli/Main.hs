830189dea790a3ea4731644a28a4cc2bdfcbdd17
Jamie Bertram <jamie.bertram@tweag.io>
2022-09-26 15:18:42 -0400

    Implement main module

--- marlowe-runtime/cli/Main.hs
+++ marlowe-runtime/cli/Main.hs
@@ -1,2 +1,13 @@
 module Main
   where
+
+import GHC.IO.Handle (hSetBuffering)
+import Language.Marlowe.Runtime.CLI.Command
+import System.IO (BufferMode(LineBuffering), stderr, stdout)
+
+main :: IO ()
+main = do
+  hSetBuffering stdout LineBuffering
+  hSetBuffering stderr LineBuffering
+  options@Options{..} <- getOptions
+  runCLIWithOptions options $ runCommand cmd
