502fb4114692275cf1de185bd0f68893bd30de78
Jamie Bertram <jamie.bertram@tweag.io>
2022-09-29 10:20:38 -0400

    Write option parser for Withdraw command

--- marlowe-runtime/cli/Language/Marlowe/Runtime/CLI/Command/Withdraw.hs
+++ marlowe-runtime/cli/Language/Marlowe/Runtime/CLI/Command/Withdraw.hs
@@ -1,10 +1,11 @@
 module Language.Marlowe.Runtime.CLI.Command.Withdraw
   where
 
-import Language.Marlowe.Runtime.CLI.Command.Tx (TxCommand)
+import Language.Marlowe.Runtime.CLI.Command.Tx (TxCommand, txCommandParser)
 import Language.Marlowe.Runtime.CLI.Monad (CLI)
-import Language.Marlowe.Runtime.ChainSync.Api (TokenName)
-import Language.Marlowe.Runtime.Core.Api (ContractId)
+import Language.Marlowe.Runtime.CLI.Option (txOutRefParser)
+import Language.Marlowe.Runtime.ChainSync.Api (TokenName(TokenName))
+import Language.Marlowe.Runtime.Core.Api (ContractId(ContractId))
 import Options.Applicative
 
 data WithdrawCommand = WithdrawCommand
@@ -13,7 +14,21 @@ data WithdrawCommand = WithdrawCommand
   }
 
 withdrawCommandParser :: ParserInfo (TxCommand WithdrawCommand)
-withdrawCommandParser = info empty $ progDesc "Withdraw funds paid to a role in a contract"
+withdrawCommandParser = info (txCommandParser parser) $ progDesc "Withdraw funds paid to a role in a contract"
+  where
+    parser = WithdrawCommand <$> contractIdParser <*> roleParser
+    contractIdParser = option (ContractId <$> txOutRefParser) $ mconcat
+      [ long "contract"
+      , short 'c'
+      , metavar "CONTRACT_ID"
+      , help "The ID of the Marlowe contract from which to withdraw funds."
+      ]
+    roleParser = fmap TokenName $ strOption $ mconcat
+      [ long "role"
+      , metavar "ROLE_NAME"
+      , help "The name of the role from which to withdraw funds."
+      ]
+
 
 runWithdrawCommand :: TxCommand WithdrawCommand -> CLI ()
 runWithdrawCommand = error "not implemented"
