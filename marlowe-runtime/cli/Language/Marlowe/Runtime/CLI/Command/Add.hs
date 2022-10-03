cac8ff41655339b14eeb2946f30cc724fcc96b0b
Jamie Bertram <jamie.bertram@tweag.io>
2022-10-03 14:12:58 -0400

    Make add and rm capable of handling multiple contract IDs

--- marlowe-runtime/cli/Language/Marlowe/Runtime/CLI/Command/Add.hs
+++ marlowe-runtime/cli/Language/Marlowe/Runtime/CLI/Command/Add.hs
@@ -4,6 +4,7 @@ module Language.Marlowe.Runtime.CLI.Command.Add
 import Control.Monad (when)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Except (ExceptT(..))
+import Data.Foldable (for_)
 import qualified Data.Text.IO as T
 import Language.Marlowe.Runtime.CLI.Monad (CLI, runCLIExceptT, runHistoryCommand)
 import Language.Marlowe.Runtime.CLI.Option (contractIdArgument)
@@ -11,16 +12,16 @@ import Language.Marlowe.Runtime.Core.Api (ContractId, renderContractId)
 import Language.Marlowe.Runtime.History.Api (HistoryCommand(..))
 import Options.Applicative
 
-newtype AddCommand = AddCommand { contractId :: ContractId }
+newtype AddCommand = AddCommand { contractIds :: [ContractId] }
   deriving (Show, Eq)
 
 addCommandParser :: ParserInfo AddCommand
 addCommandParser = info parser $ progDesc "Start managing a new contract"
   where
-    parser = AddCommand <$> contractIdArgument "The ID of the contract to add"
+    parser = AddCommand <$> many (contractIdArgument "A contract ID to add (i.e. start managing)")
 
 runAddCommand :: AddCommand -> CLI ()
-runAddCommand AddCommand{..} = runCLIExceptT do
+runAddCommand AddCommand{..} = runCLIExceptT $ for_ contractIds \contractId -> do
   added <- ExceptT $ runHistoryCommand $ FollowContract contractId
   when added do
     liftIO $ T.putStrLn $ renderContractId contractId
