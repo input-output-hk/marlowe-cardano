c6da8ae0668f0a44ece262b7ba094e14549a3350
Yves Hauser <yves.hauser@gmail.com>
2022-10-31 15:13:15 +0100

    SCP-2998: data types from JSON file

--- marlowe-actus/actus-core/app/GenActus.hs
+++ marlowe-actus/actus-core/app/GenActus.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
@@ -20,37 +21,77 @@ import System.Environment
 
 main :: IO ()
 main = do
-  p <- getEnv "ACTUS_DICTIONARY_DIR"
-  Right d <- load (p <> "/actus-dictionary.json")
-  runGhc (Just libdir) $ putPpr (actusModule d)
+  path <- getEnv "ACTUS_DICTIONARY_DIR"
+  actusDictionary <- load (path <> "/actus-dictionary.json")
+  runGhc (Just libdir) $ putPpr (actusModule $ either error id actusDictionary)
 
 data ContractType = ContractType
-  { acronym :: String
+  { identifier :: String
   , name :: String
+  , acronym :: String
+  , family :: String
+  , class' :: String
+  , description :: String
+  , coverage :: Maybe String
+  , status :: Maybe String
   }
-  deriving stock (Show, Generic)
-  deriving anyclass (FromJSON, ToJSON)
+  deriving Show
 
-data Terms = Terms
-  { group :: String
+instance FromJSON ContractType where
+  parseJSON (Object v) =
+    ContractType
+      <$> v .: "identifier"
+      <*> v .: "name"
+      <*> v .: "acronym"
+      <*> v .: "family"
+      <*> v .: "class"
+      <*> v .: "description"
+      <*> v .:? "coverage"
+      <*> v .:? "status"
+  parseJSON _ = mzero
+
+data Term = Term
+  { identifier :: String
+  , group :: Maybe String
+  , name :: String
+  , acronym :: String
+  , type' :: String
+  -- TODO: allowed values depends on type
+  -- , allowedValues :: [AllowedValue]
+  , default' :: String
+  , description :: Maybe String
+  }
+  deriving Show
+
+instance FromJSON Term where
+  parseJSON (Object v) =
+    Term
+      <$> v .: "identifier"
+      <*> v .: "group"
+      <*> v .: "name"
+      <*> v .: "acronym"
+      <*> v .: "type"
+   --   <*> v .: "allowedValues"
+      <*> v .: "default"
+      <*> v .:? "description"
+  parseJSON _ = mzero
+
+data AllowedValue = AllowedValue
+  { option :: String
   , identifier :: String
+  , name :: String
+  , acronym :: String
+  , description :: String
   }
   deriving stock (Show, Generic)
-  deriving anyclass (FromJSON, ToJSON)
-
-data Version = ActusVersion
-  { version' :: String,
-    date :: String
-  }
-  deriving stock (Show, Generic)
-  deriving anyclass (FromJSON, ToJSON)
+  deriving anyclass (FromJSON)
 
 data ActusDictionary = ActusDictionary
   { taxonomy :: Map String ContractType,
-    terms :: Map String Terms
+    terms :: Map String Term
   }
   deriving stock (Show, Generic)
-  deriving anyclass (FromJSON, ToJSON)
+  deriving anyclass (FromJSON)
 
 load :: FilePath -> IO (Either String ActusDictionary)
 load f = eitherDecode <$> B.readFile f
@@ -65,7 +106,7 @@ actusModule ActusDictionary {..} =
         "CT"
         []
         (map f (elems taxonomy))
-        []
+        [deriving' [var "Show", var "Read", var "Eq", var "Generic", var "FromJSON"]]
     ]
   where
     f ContractType {..} = prefixCon (fromString acronym) []
