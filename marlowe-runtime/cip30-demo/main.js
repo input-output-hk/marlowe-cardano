35d9ffdf61ebfca1c43d35a34be664f6760f14fd
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2022-11-08 15:46:45 +0100

    nicefication

--- marlowe-runtime/cip30-demo/main.js
+++ marlowe-runtime/cip30-demo/main.js
@@ -2,24 +2,35 @@ const loadingWalletMessage = document.createElement("p");
 loadingWalletMessage.innerHTML = "Loading wallets...";
 document.body.appendChild(loadingWalletMessage);
 
+/** Incomplete CIP-30 wallet API type
+@typedef {Object} WalletApi
+@property {(...args: any[]) => any} enable
+*/
+
+/** @type {(x: any) => x is { [key: string]: any }} */
+const isObject = (x) => typeof x === "object" && x !== null;
+
+/** @type {(x: any) => x is WalletApi} */
+const isWalletApi = (x) => isObject(x) && typeof x.enable === "function";
+
 setTimeout(() => {
   loadingWalletMessage.remove();
   const cardano = /** @type {any} */ (window).cardano;
 
   try {
-    if (cardano === null || typeof cardano !== "object") {
+    if (!isObject(cardano)) {
       throw "No wallets installed!";
     } else {
       const wallets = Object.entries(cardano)
-        .filter(([, value]) => typeof value.enable === "function")
+        .filter(([, value]) => isWalletApi(value))
         .map(([key]) => key);
 
       if (wallets.length === 0) {
         throw "No wallets installed!";
       } else {
-        const walleListLabel = document.createElement("h2");
-        walleListLabel.innerHTML = "Installed wallets:";
-        document.body.appendChild(walleListLabel);
+        const walletListLabel = document.createElement("h2");
+        walletListLabel.innerHTML = "Installed wallets:";
+        document.body.appendChild(walletListLabel);
         const walletList = document.createElement("ul");
         document.body.appendChild(walletList);
 
