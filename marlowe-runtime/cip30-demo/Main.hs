9bfd3f8bce765805c99f0c2322488d659aba1a9a
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2022-11-11 14:31:03 +0100

    serve wallet options menu

--- marlowe-runtime/cip30-demo/Main.hs
+++ marlowe-runtime/cip30-demo/Main.hs
@@ -5,14 +5,27 @@
 module Main
   where
 
+import qualified Text.Blaze as Blaze
 import Text.Julius (juliusFile)
+import qualified Text.Julius as Julius
 import Yesod (Yesod)
 import qualified Yesod
 
 data CIP30 = CIP30
 
+newtype WalletName = WalletName String
+  deriving newtype
+    ( Eq
+    , Read
+    , Show
+    , Yesod.PathPiece
+    , Julius.ToJavascript
+    , Blaze.ToMarkup
+    )
+
 Yesod.mkYesod "CIP30" [Yesod.parseRoutes|
-/ HomeR GET
+/            HomeR   GET
+/#WalletName WalletR GET
 |]
 
 instance Yesod CIP30
@@ -20,6 +33,28 @@ instance Yesod CIP30
 getHomeR :: Handler Yesod.Html
 getHomeR = Yesod.defaultLayout do
   Yesod.toWidget $(juliusFile "./cip30-demo/main.js")
+
+getWalletR :: WalletName -> Handler Yesod.Html
+getWalletR walletName = Yesod.defaultLayout do
+  Yesod.toWidget [Yesod.hamlet|
+    <h1>#{walletName} wallet options
+    <ul>
+      <li>
+        <h2><a>create
+        <i>Create a new contract
+      <li>
+        <h2><a>deposit
+        <i>Deposit funds into running contract
+      <li>
+        <h2><a>choose
+        <i>Make a choice in a running contract
+      <li>
+        <h2><a>notify
+        <i>Poke a running contract to continue
+      <li>
+        <h2><a>withdraw
+        <i>Withdraw payouts from a running contract
+  |]
 
 main :: IO ()
 main = do
