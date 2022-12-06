fedb02ed3c121e6fe42b77b37f041dffabd7a97c
Parthiv Seetharaman <pachum99@myrdd.info>
2022-12-06 13:49:27 -0800

    chore: add iog cache to flake config

--- flake.nix
+++ flake.nix
@@ -146,4 +146,16 @@
             systems);
         };
     };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.iog.io"
+    ];
+    extra-trusted-public-keys = [
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+    allow-import-from-derivation = true;
+    accept-flake-config = true;
+  };
+
 }
