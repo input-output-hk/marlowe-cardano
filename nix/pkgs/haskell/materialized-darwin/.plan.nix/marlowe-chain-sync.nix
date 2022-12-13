7972c0a29eba37140bfd7ed753e3fdcc3b6223df
Jamie Bertram <jamie.bertram@tweag.io>
2022-12-13 13:35:15 -0700

    Move chain sync libs to better named folders

--- nix/pkgs/haskell/materialized-darwin/.plan.nix/marlowe-chain-sync.nix
+++ nix/pkgs/haskell/materialized-darwin/.plan.nix/marlowe-chain-sync.nix
@@ -59,7 +59,7 @@
           "Language/Marlowe/Runtime/Cardano/Feature"
           "Language/Marlowe/Runtime/ChainSync/Api"
           ];
-        hsSourceDirs = [ "api" ];
+        hsSourceDirs = [ "src" ];
         };
       sublibs = {
         "libchainseek" = {
@@ -100,12 +100,53 @@
             "Language/Marlowe/Runtime/ChainSync"
             "Language/Marlowe/Runtime/ChainSync/Database"
             "Language/Marlowe/Runtime/ChainSync/Database/PostgreSQL"
-            "Language/Marlowe/Runtime/ChainSync/Genesis"
             "Language/Marlowe/Runtime/ChainSync/JobServer"
-            "Language/Marlowe/Runtime/ChainSync/NodeClient"
             "Language/Marlowe/Runtime/ChainSync/Server"
             "Language/Marlowe/Runtime/ChainSync/QueryServer"
-            "Language/Marlowe/Runtime/ChainSync/Store"
+            ];
+          hsSourceDirs = [ "libchainseek" ];
+          };
+        "chain-indexer" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."async-components" or (errorHandler.buildDepError "async-components"))
+            (hsPkgs."base16" or (errorHandler.buildDepError "base16"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."cborg" or (errorHandler.buildDepError "cborg"))
+            (hsPkgs."cardano-api" or (errorHandler.buildDepError "cardano-api"))
+            (hsPkgs."cardano-binary" or (errorHandler.buildDepError "cardano-binary"))
+            (hsPkgs."cardano-crypto-wrapper" or (errorHandler.buildDepError "cardano-crypto-wrapper"))
+            (hsPkgs."cardano-ledger-byron" or (errorHandler.buildDepError "cardano-ledger-byron"))
+            (hsPkgs."cardano-ledger-babbage" or (errorHandler.buildDepError "cardano-ledger-babbage"))
+            (hsPkgs."cardano-ledger-alonzo" or (errorHandler.buildDepError "cardano-ledger-alonzo"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."eventuo11y" or (errorHandler.buildDepError "eventuo11y"))
+            (hsPkgs."foldl" or (errorHandler.buildDepError "foldl"))
+            (hsPkgs."hasql" or (errorHandler.buildDepError "hasql"))
+            (hsPkgs."hasql-th" or (errorHandler.buildDepError "hasql-th"))
+            (hsPkgs."hasql-transaction" or (errorHandler.buildDepError "hasql-transaction"))
+            (hsPkgs."marlowe-chain-sync" or (errorHandler.buildDepError "marlowe-chain-sync"))
+            (hsPkgs."marlowe-protocols" or (errorHandler.buildDepError "marlowe-protocols"))
+            (hsPkgs."ouroboros-network" or (errorHandler.buildDepError "ouroboros-network"))
+            (hsPkgs."profunctors" or (errorHandler.buildDepError "profunctors"))
+            (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
+            (hsPkgs."stm-delay" or (errorHandler.buildDepError "stm-delay"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."these" or (errorHandler.buildDepError "these"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            (hsPkgs."typed-protocols" or (errorHandler.buildDepError "typed-protocols"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            (hsPkgs."witherable" or (errorHandler.buildDepError "witherable"))
+            ];
+          buildable = true;
+          modules = [
+            "Language/Marlowe/Runtime/ChainIndexer"
+            "Language/Marlowe/Runtime/ChainIndexer/Database"
+            "Language/Marlowe/Runtime/ChainIndexer/Database/PostgreSQL"
+            "Language/Marlowe/Runtime/ChainIndexer/Genesis"
+            "Language/Marlowe/Runtime/ChainIndexer/NodeClient"
+            "Language/Marlowe/Runtime/ChainIndexer/Store"
             ];
           hsSourceDirs = [ "src" ];
           };
@@ -127,6 +168,34 @@
           };
         };
       exes = {
+        "marlowe-chain-indexer" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."async-components" or (errorHandler.buildDepError "async-components"))
+            (hsPkgs."cardano-api" or (errorHandler.buildDepError "cardano-api"))
+            (hsPkgs."cardano-crypto-wrapper" or (errorHandler.buildDepError "cardano-crypto-wrapper"))
+            (hsPkgs."cardano-ledger-byron" or (errorHandler.buildDepError "cardano-ledger-byron"))
+            (hsPkgs."eventuo11y" or (errorHandler.buildDepError "eventuo11y"))
+            (hsPkgs."eventuo11y-extras" or (errorHandler.buildDepError "eventuo11y-extras"))
+            (hsPkgs."hasql" or (errorHandler.buildDepError "hasql"))
+            (hsPkgs."hasql-pool" or (errorHandler.buildDepError "hasql-pool"))
+            (hsPkgs."marlowe-chain-sync" or (errorHandler.buildDepError "marlowe-chain-sync"))
+            (hsPkgs."marlowe-chain-sync".components.sublibs.libchainseek or (errorHandler.buildDepError "marlowe-chain-sync:libchainseek"))
+            (hsPkgs."marlowe-protocols" or (errorHandler.buildDepError "marlowe-protocols"))
+            (hsPkgs."network" or (errorHandler.buildDepError "network"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            (hsPkgs."ouroboros-network" or (errorHandler.buildDepError "ouroboros-network"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            (hsPkgs."uuid" or (errorHandler.buildDepError "uuid"))
+            ];
+          buildable = true;
+          modules = [ "Logging" "Options" "Paths_marlowe_chain_sync" ];
+          hsSourceDirs = [ "marlowe-chain-indexer" ];
+          mainPath = [ "Main.hs" ];
+          };
         "chainseekd" = {
           depends = [
             (hsPkgs."base" or (errorHandler.buildDepError "base"))
