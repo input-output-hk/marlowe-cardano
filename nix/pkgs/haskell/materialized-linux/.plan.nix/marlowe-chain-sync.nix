4233769681cf509e4728cbbd5ac538d3854646e4
Jamie Bertram <jamie.bertram@tweag.io>
2022-12-13 10:17:53 -0700

    Update materialized.

--- nix/pkgs/haskell/materialized-linux/.plan.nix/marlowe-chain-sync.nix
+++ nix/pkgs/haskell/materialized-linux/.plan.nix/marlowe-chain-sync.nix
@@ -10,7 +10,7 @@
   {
     flags = {};
     package = {
-      specVersion = "2.4";
+      specVersion = "3.0";
       identifier = { name = "marlowe-chain-sync"; version = "0.0.0.0"; };
       license = "Apache-2.0";
       copyright = "";
@@ -35,58 +35,96 @@
         depends = [
           (hsPkgs."base" or (errorHandler.buildDepError "base"))
           (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
-          (hsPkgs."async-components" or (errorHandler.buildDepError "async-components"))
           (hsPkgs."base16" or (errorHandler.buildDepError "base16"))
           (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
           (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
-          (hsPkgs."cborg" or (errorHandler.buildDepError "cborg"))
           (hsPkgs."cardano-api" or (errorHandler.buildDepError "cardano-api"))
-          (hsPkgs."cardano-binary" or (errorHandler.buildDepError "cardano-binary"))
-          (hsPkgs."cardano-crypto-wrapper" or (errorHandler.buildDepError "cardano-crypto-wrapper"))
-          (hsPkgs."cardano-ledger-byron" or (errorHandler.buildDepError "cardano-ledger-byron"))
-          (hsPkgs."cardano-ledger-babbage" or (errorHandler.buildDepError "cardano-ledger-babbage"))
-          (hsPkgs."cardano-ledger-alonzo" or (errorHandler.buildDepError "cardano-ledger-alonzo"))
           (hsPkgs."cardano-ledger-core" or (errorHandler.buildDepError "cardano-ledger-core"))
           (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
-          (hsPkgs."eventuo11y" or (errorHandler.buildDepError "eventuo11y"))
-          (hsPkgs."foldl" or (errorHandler.buildDepError "foldl"))
-          (hsPkgs."hasql" or (errorHandler.buildDepError "hasql"))
-          (hsPkgs."hasql-th" or (errorHandler.buildDepError "hasql-th"))
-          (hsPkgs."hasql-transaction" or (errorHandler.buildDepError "hasql-transaction"))
           (hsPkgs."marlowe-protocols" or (errorHandler.buildDepError "marlowe-protocols"))
           (hsPkgs."ouroboros-consensus" or (errorHandler.buildDepError "ouroboros-consensus"))
           (hsPkgs."ouroboros-network" or (errorHandler.buildDepError "ouroboros-network"))
           (hsPkgs."plutus-tx" or (errorHandler.buildDepError "plutus-tx"))
           (hsPkgs."plutus-ledger-api" or (errorHandler.buildDepError "plutus-ledger-api"))
-          (hsPkgs."profunctors" or (errorHandler.buildDepError "profunctors"))
           (hsPkgs."serialise" or (errorHandler.buildDepError "serialise"))
-          (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
-          (hsPkgs."stm-delay" or (errorHandler.buildDepError "stm-delay"))
           (hsPkgs."text" or (errorHandler.buildDepError "text"))
           (hsPkgs."these" or (errorHandler.buildDepError "these"))
           (hsPkgs."time" or (errorHandler.buildDepError "time"))
-          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
           (hsPkgs."typed-protocols" or (errorHandler.buildDepError "typed-protocols"))
           (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
-          (hsPkgs."witherable" or (errorHandler.buildDepError "witherable"))
           ];
         buildable = true;
         modules = [
           "Language/Marlowe/Runtime/Cardano/Api"
           "Language/Marlowe/Runtime/Cardano/Feature"
-          "Language/Marlowe/Runtime/ChainSync"
-          "Language/Marlowe/Runtime/ChainSync/Database"
-          "Language/Marlowe/Runtime/ChainSync/Database/PostgreSQL"
-          "Language/Marlowe/Runtime/ChainSync/Genesis"
-          "Language/Marlowe/Runtime/ChainSync/JobServer"
-          "Language/Marlowe/Runtime/ChainSync/NodeClient"
-          "Language/Marlowe/Runtime/ChainSync/Server"
-          "Language/Marlowe/Runtime/ChainSync/QueryServer"
-          "Language/Marlowe/Runtime/ChainSync/Store"
           "Language/Marlowe/Runtime/ChainSync/Api"
-          "Language/Marlowe/Runtime/Plutus/V2/Api"
           ];
-        hsSourceDirs = [ "src" ];
+        hsSourceDirs = [ "api" ];
+        };
+      sublibs = {
+        "libchainseek" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."async-components" or (errorHandler.buildDepError "async-components"))
+            (hsPkgs."base16" or (errorHandler.buildDepError "base16"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."cborg" or (errorHandler.buildDepError "cborg"))
+            (hsPkgs."cardano-api" or (errorHandler.buildDepError "cardano-api"))
+            (hsPkgs."cardano-binary" or (errorHandler.buildDepError "cardano-binary"))
+            (hsPkgs."cardano-crypto-wrapper" or (errorHandler.buildDepError "cardano-crypto-wrapper"))
+            (hsPkgs."cardano-ledger-byron" or (errorHandler.buildDepError "cardano-ledger-byron"))
+            (hsPkgs."cardano-ledger-babbage" or (errorHandler.buildDepError "cardano-ledger-babbage"))
+            (hsPkgs."cardano-ledger-alonzo" or (errorHandler.buildDepError "cardano-ledger-alonzo"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."eventuo11y" or (errorHandler.buildDepError "eventuo11y"))
+            (hsPkgs."foldl" or (errorHandler.buildDepError "foldl"))
+            (hsPkgs."hasql" or (errorHandler.buildDepError "hasql"))
+            (hsPkgs."hasql-th" or (errorHandler.buildDepError "hasql-th"))
+            (hsPkgs."hasql-transaction" or (errorHandler.buildDepError "hasql-transaction"))
+            (hsPkgs."marlowe-chain-sync" or (errorHandler.buildDepError "marlowe-chain-sync"))
+            (hsPkgs."marlowe-protocols" or (errorHandler.buildDepError "marlowe-protocols"))
+            (hsPkgs."ouroboros-network" or (errorHandler.buildDepError "ouroboros-network"))
+            (hsPkgs."profunctors" or (errorHandler.buildDepError "profunctors"))
+            (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
+            (hsPkgs."stm-delay" or (errorHandler.buildDepError "stm-delay"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."these" or (errorHandler.buildDepError "these"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            (hsPkgs."typed-protocols" or (errorHandler.buildDepError "typed-protocols"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            (hsPkgs."witherable" or (errorHandler.buildDepError "witherable"))
+            ];
+          buildable = true;
+          modules = [
+            "Language/Marlowe/Runtime/ChainSync"
+            "Language/Marlowe/Runtime/ChainSync/Database"
+            "Language/Marlowe/Runtime/ChainSync/Database/PostgreSQL"
+            "Language/Marlowe/Runtime/ChainSync/Genesis"
+            "Language/Marlowe/Runtime/ChainSync/JobServer"
+            "Language/Marlowe/Runtime/ChainSync/NodeClient"
+            "Language/Marlowe/Runtime/ChainSync/Server"
+            "Language/Marlowe/Runtime/ChainSync/QueryServer"
+            "Language/Marlowe/Runtime/ChainSync/Store"
+            ];
+          hsSourceDirs = [ "src" ];
+          };
+        "plutus-compat" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."cardano-api" or (errorHandler.buildDepError "cardano-api"))
+            (hsPkgs."cardano-ledger-byron" or (errorHandler.buildDepError "cardano-ledger-byron"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."marlowe-chain-sync" or (errorHandler.buildDepError "marlowe-chain-sync"))
+            (hsPkgs."plutus-tx" or (errorHandler.buildDepError "plutus-tx"))
+            (hsPkgs."plutus-ledger-api" or (errorHandler.buildDepError "plutus-ledger-api"))
+            (hsPkgs."serialise" or (errorHandler.buildDepError "serialise"))
+            ];
+          buildable = true;
+          modules = [ "Language/Marlowe/Runtime/Plutus/V2/Api" ];
+          hsSourceDirs = [ "plutus-compat" ];
+          };
         };
       exes = {
         "chainseekd" = {
@@ -102,6 +140,7 @@
             (hsPkgs."hasql" or (errorHandler.buildDepError "hasql"))
             (hsPkgs."hasql-pool" or (errorHandler.buildDepError "hasql-pool"))
             (hsPkgs."marlowe-chain-sync" or (errorHandler.buildDepError "marlowe-chain-sync"))
+            (hsPkgs."marlowe-chain-sync".components.sublibs.libchainseek or (errorHandler.buildDepError "marlowe-chain-sync:libchainseek"))
             (hsPkgs."marlowe-protocols" or (errorHandler.buildDepError "marlowe-protocols"))
             (hsPkgs."network" or (errorHandler.buildDepError "network"))
             (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
