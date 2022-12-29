d6cb6b7ebfef2bd46ebd3ac0607c3d84b2f83489
Brian W Bush <brian.bush@iohk.io>
2022-12-29 11:40:28 -0700

    SCP-4636 Refactored oracle logic for ignoring transactions.

--- marlowe-apps/oracle/Language/Marlowe/Oracle/Process.hs
+++ marlowe-apps/oracle/Language/Marlowe/Oracle/Process.hs
@@ -1,9 +1,8 @@
 
 
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE TupleSections #-}
 
 
 module Language.Marlowe.Oracle.Process
@@ -13,11 +12,10 @@ module Language.Marlowe.Oracle.Process
 import Control.Concurrent (forkIO, threadDelay)
 import Control.Concurrent.STM (atomically)
 import Control.Concurrent.STM.TChan (TChan, newTChanIO, readTChan, writeTChan)
-import Control.Monad (forever, void)
+import Control.Monad (forever, unless, void)
 import Control.Monad.Except (ExceptT(ExceptT), liftIO, runExceptT)
 import Data.List (intercalate)
 import Data.Maybe (mapMaybe)
-import Data.String (fromString)
 import Language.Marlowe.Core.V1.Semantics.Types
   (ChoiceId(ChoiceId), Contract, Input(NormalInput), InputContent(IChoice), Party)
 import Language.Marlowe.Oracle.Detect (containsOracleAction, contractReadyForOracle)
@@ -34,7 +32,7 @@ import Language.Marlowe.Runtime.App.Transact (apply)
 import Language.Marlowe.Runtime.App.Types (Config)
 import Language.Marlowe.Runtime.ChainSync.Api (Address, TxId)
 import Language.Marlowe.Runtime.Core.Api (ContractId, MarloweVersionTag(V1))
-import Network.Oracle (OracleEnv, readOracle, toOracleSymbol)
+import Network.Oracle (Oracle, OracleEnv, readOracle, toOracleSymbol)
 import Plutus.V2.Ledger.Api (toBuiltin)
 import System.IO (hPutStrLn, stderr)
 
@@ -42,8 +40,8 @@ import qualified Cardano.Api as C (PaymentExtendedKey, SigningKey)
 import qualified Data.Aeson as A (encode)
 import qualified Data.ByteString.Char8 as BS8 (pack)
 import qualified Data.ByteString.Lazy.Char8 as LBS8 (unpack)
-import qualified Data.Map.Strict as M (Map, delete, insert, lookup)
-import qualified Data.Set as S (Set, delete, insert, member)
+import qualified Data.Map.Strict as M (Map, adjust, delete, insert, lookup)
+import qualified Data.Set as S (Set, insert, member)
 
 
 runDiscovery
@@ -85,6 +83,17 @@ runDetection config pollingFrequency party inChannel =
     pure outChannel
 
 
+data LastSeen =
+  LastSeen
+  {
+    thisContractId :: ContractId
+  , lastContract :: Contract
+  , lastTxId :: TxId
+  , ignoredTxIds :: S.Set TxId
+  }
+    deriving (Show)
+
+
 runOracle
   :: OracleEnv
   -> Config
@@ -97,72 +106,102 @@ runOracle
   -> IO ()
 runOracle oracleEnv config pollingFrequency address key party inChannel outChannel =
   let
-    go :: M.Map ContractId (TxId, Contract) -> S.Set TxId -> IO ()
-    go contracts ignored =
+    -- Nothing needs updating.
+    rollback :: ContractStream 'V1 -> M.Map ContractId LastSeen -> M.Map ContractId LastSeen
+    rollback = const id
+    -- Remove the contract from tracking.
+    delete :: ContractId -> M.Map ContractId LastSeen -> M.Map ContractId LastSeen
+    delete = M.delete
+    -- Update the contract and its latest transaction.
+    update :: ContractStream 'V1 -> M.Map ContractId LastSeen -> M.Map ContractId LastSeen
+    update cs lastSeen =
+      let
+        contractId = csContractId cs
+      in
+        flip (M.insert contractId) lastSeen
+          $ case (contractId `M.lookup` lastSeen, contractFromStream cs, transactionIdFromStream cs) of
+              (Nothing  , Just contract, Just txId) -> LastSeen contractId contract txId mempty
+              (Just seen, Just contract, Just txId) -> seen {lastContract = contract, lastTxId = txId}
+              _                                     -> error
+                                                         $ "Invalid contract stream: seen = "
+                                                         <> show (contractId `M.lookup` lastSeen)
+                                                         <> ", contract = " <> show (contractFromStream cs)
+                                                         <> " txId = " <> show (transactionIdFromStream cs)
+                                                         <> "."
+    -- Ignore the transaction in the future.
+    ignore :: ContractId -> TxId -> M.Map ContractId LastSeen -> M.Map ContractId LastSeen
+    ignore contractId txId lastSeen =
+      M.adjust (\seen -> seen {ignoredTxIds = txId `S.insert` ignoredTxIds seen}) contractId lastSeen
+    -- Revisit a contract later.
+    revisit :: ContractId -> IO ()
+    revisit contractId =
+      -- FIXME: This is a workaround for contract discovery not tailing past the tip of the blockchain.
+      void . forkIO
+        $ threadDelay pollingFrequency
+        >> atomically (writeTChan outChannel contractId)
+    -- Find the oracle symbols requested, the ones that can be serviced, and whether to ignore them, respectively.
+    lookupSymbols :: LastSeen -> ([String], [Oracle], Bool)
+    lookupSymbols LastSeen{..} =
+      let
+        rawSymbols = contractReadyForOracle party lastContract
+        validSymbols = mapMaybe toOracleSymbol rawSymbols
+        ignored = lastTxId `S.member` ignoredTxIds
+      in
+        (rawSymbols, validSymbols, ignored)
+    -- Build and submit a transaction to report the oracle's value.
+    report :: ContractId -> Oracle -> ExceptT String IO ()
+    report contractId symbol =
+      do
+        value <- ExceptT $ readOracle oracleEnv symbol
+        void
+          . apply config address key contractId
+          . pure . NormalInput
+          $ IChoice (ChoiceId (toBuiltin . BS8.pack $ show symbol) party) value
+        liftIO . hPutStrLn stderr
+          $ "  " <> show symbol <> " = " <> show value <> " [scaled integer units]"
+    -- Print the context of the transaction.
+    printContext :: ContractId -> LastSeen -> [String] -> [Oracle] -> IO ()
+    printContext contractId LastSeen{lastTxId} rawSymbols validSymbols =
+      do
+        hPutStrLn stderr ""
+        hPutStrLn stderr $ "Contract ID: " <> (init . tail . LBS8.unpack . A.encode) contractId
+        hPutStrLn stderr $ "  Previous transaction ID: " <> (init . tail . show) lastTxId
+        hPutStrLn stderr $ "  Ready for oracle: " <> intercalate ", " (show <$> rawSymbols)
+        hPutStrLn stderr $ "  Available from oracle: " <> intercalate ", " (show <$> validSymbols)
+    -- Print the result of the transaction.
+    printResult :: String -> IO ()
+    printResult = hPutStrLn stderr . ("  " <>)
+    -- Process the stream of changes to contracts.
+    go :: M.Map ContractId LastSeen -> IO ()
+    go lastSeen =
       do
         cs <- liftIO . atomically $ readTChan inChannel
-        uncurry go =<<
+        go =<<
           case cs of
-            ContractStreamFinish{..} -> pure
-                                          (
-                                            csContractId `M.delete` contracts
-                                          , maybe ignored ((`S.delete` ignored) . fst) $ csContractId `M.lookup` contracts
-                                          )
-            ContractStreamWait{..}   -> do
-                                          -- FIXME: Break this into separate functions.
-                                          let
-                                            maybeContract = csContractId `M.lookup` contracts
-                                            rawSymbols = maybe mempty (contractReadyForOracle party . snd) maybeContract
-                                            transactionId = maybe (fromString "" :: TxId) fst maybeContract
-                                            ignore = transactionId `S.member` ignored
-                                            validSymbols = mapMaybe toOracleSymbol rawSymbols
-                                            report symbol =
-                                              do
-                                                value <- ExceptT $ readOracle oracleEnv symbol
-                                                void
-                                                  . apply config address key csContractId
-                                                  . pure . NormalInput
-                                                  $ IChoice (ChoiceId (toBuiltin . BS8.pack $ show symbol) party) value
-                                                liftIO . hPutStrLn stderr
-                                                  $ "  " <> show symbol <> " = " <> show value <> " [scaled integer units]"
-                                          -- FIXME: This is a workaround for contract discovery
-                                          --        not tailing past the tip of the blockchain.
-                                          ignored' <-
-                                            if ignore
-                                              then pure ignored
-                                              else do
-                                                hPutStrLn stderr ""
-                                                hPutStrLn stderr $ "Contract ID: " <> (init . tail . LBS8.unpack . A.encode) csContractId
-                                                hPutStrLn stderr $ "  Transaction ID: " <> (init . tail . show) transactionId
-                                                hPutStrLn stderr $ "  Ready for oracle: " <> intercalate ", " (show <$> rawSymbols)
-                                                hPutStrLn stderr $ "  Available from oracle: " <> intercalate ", " (show <$> validSymbols)
-                                                runExceptT (mapM_ report  $ take 1 validSymbols)
-                                                  >>= \result ->
-                                                    case (result, null validSymbols) of
-                                                      (Right ()    , True ) -> do
-                                                                                 hPutStrLn stderr "  Ignored."
-                                                                                 pure $ transactionId `S.insert` ignored
-                                                      (Right ()    , False) -> do
-                                                                                 hPutStrLn stderr "  Confirmed."
-                                                                                 pure $ transactionId `S.delete` ignored
-                                                      (Left message, _    ) -> do
-                                                                                 hPutStrLn stderr $ "  Failed: " <> message
-                                                                                 pure $ transactionId `S.insert` ignored
-                                          -- FIXME: This is a workaround for contract discovery
-                                          --        not tailing past the tip of the blockchain.
-                                          void . forkIO
-                                            $ threadDelay pollingFrequency
-                                            >> atomically (writeTChan outChannel csContractId)
-                                          hPutStrLn stderr $ show ignored
-                                          pure (contracts, ignored')
-            _                        -> let
-                                          transactionId = maybe (fromString "" :: TxId) id $ transactionIdFromStream cs
-                                        in
-                                          pure
-                                            (
-                                              maybe contracts (flip (M.insert $ csContractId cs) contracts . (transactionId, ))
-                                                $ contractFromStream cs
-                                            , transactionId `S.delete` ignored
-                                            )
+            ContractStreamStart{}      -> pure $ update cs lastSeen
+            ContractStreamContinued{}  -> pure $ update cs lastSeen
+            ContractStreamRolledBack{} -> pure $ rollback cs lastSeen
+            ContractStreamWait{..}     -> do
+                                            let
+                                              seen@LastSeen{lastTxId} =
+                                                case csContractId `M.lookup` lastSeen of
+                                                  Just seen' -> seen'
+                                                  _          -> error
+                                                                  $ "Invalid contract stream: seen = "
+                                                                  <> show (csContractId `M.lookup` lastSeen)
+                                                                  <> "."
+                                              (rawSymbols, validSymbols, ignored) = lookupSymbols seen
+                                            unless ignored
+                                              $ do
+                                                printContext csContractId seen rawSymbols validSymbols
+                                                result <- runExceptT $ mapM_ (report csContractId) (take 1 validSymbols)
+                                                printResult
+                                                  $ case (result, null validSymbols) of
+                                                      (Right ()    , True ) -> "Ignored."
+                                                      (Right ()    , False) -> "Confirmed."
+                                                      (Left message, _    ) -> "Failed: " <> message
+                                            revisit csContractId
+                                            pure $ ignore csContractId lastTxId lastSeen
+            ContractStreamFinish{..}   -> pure $ delete csContractId lastSeen
   in
-    go mempty mempty
+    go mempty
