d9347cf66713846fd24ca97d8c47cd961e330272
Parthiv Seetharaman <pachum99@myrdd.info>
2023-01-10 13:54:21 -0800

    fix: split out chain-indexer

--- nix/nomadEnv.nix
+++ nix/nomadEnv.nix
@@ -1,10 +1,16 @@
 { inputs }:
 
 let
-  inherit (inputs) self data-merge dapps-world cardano-world;
+  inherit (inputs) self data-merge dapps-world cardano-world nixpkgs;
+
   inherit (cardano-world) cardano;
   inherit (dapps-world) cloud;
   inherit (cloud.constants) baseDomain;
+
+  getNetworkMagic = env: (builtins.fromJSON
+    (builtins.readFile
+      cardano.environments.${env}.networkConfig.ShelleyGenesisFile
+    )).networkMagic;
 
   # marlowe namespace is created and configured in dapps-world
 
@@ -20,7 +26,7 @@ let
             namespace = "marlowe";
             nodeClass = "marlowe";
             domain = "${jobname}.${baseDomain}";
-            scaling = 3;
+            scaling = 1;
           }
         )
       )
@@ -36,12 +42,34 @@ let
               PUBLIC_ROOTS_SRV_DNS = "_${environment}-node._tcp.service.consul";
             };
           };
+          chain-indexer = {
+            env = {
+              DB_NAME = "${environment}_chainseek";
+              BYRON_GENESIS_CONFIG = "/persist/${jobname}/config/${environment}/byron-genesis.json";
+              BYRON_GENESIS_HASH = cardano.environments.${environment}.nodeConfig.ByronGenesisHash;
+              SHELLEY_GENESIS_CONFIG = "/persist/${jobname}/config/${environment}/shelley-genesis.json";
+              CARDANO_TESTNET_MAGIC = getNetworkMagic environment;
+              MASTER_REPLICA_SRV_DNS = "_infra-database._master.service.us-east-1.consul";
+            };
+            template = data-merge.append [
+              {
+                change_mode = "restart";
+                data = ''
+                  {{- with secret "kv/data/chainseek/${environment}" }}
+                  DB_USER={{ .Data.data.pgUser }}
+                  DB_PASS={{ .Data.data.pgPass }}
+                  {{ end -}}
+                '';
+                destination = "/secrets/db-secrets.env";
+                env = true;
+              }
+            ];
+          };
           chainseekd = {
             env = {
               DB_NAME = "${environment}_chainseek";
-              GENESIS_CONFIG = "/persist/${jobname}/config/custom/byron-genesis.json";
-              GENESIS_HASH = cardano.environments.${environment}.nodeConfig.ByronGenesisHash;
               MASTER_REPLICA_SRV_DNS = "_infra-database._master.service.us-east-1.consul";
+              CARDANO_TESTNET_MAGIC = getNetworkMagic environment;
             };
             template = data-merge.append [
               {
