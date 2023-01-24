157175913073e201ca2551bdfc50752d3ef0be9a
Parthiv Seetharaman <pachum99@myrdd.info>
2023-01-23 17:15:58 -0800

    add public image definitions

--- deploy/oci-images.nix
+++ deploy/oci-images.nix
@@ -1,7 +1,9 @@
 { inputs }:
 let
-  inherit (inputs) std self;
+  inherit (inputs) std self nixpkgs;
+  inherit (nixpkgs.lib) removePrefix;
   inherit (self) operables;
+  inherit (self.sourceInfo) lastModifiedDate;
 
   mkImage = name: std.lib.ops.mkStandardOCI {
     name = "registry.ci.iog.io/dapps-world-${name}";
@@ -9,6 +11,16 @@ let
     debug = true;
   };
 
+  mkPublicImage = name:
+    let
+      tagName = removePrefix "marlowe-" name;
+      tagDate = builtins.substring 0 8 lastModifiedDate; # pull out just date
+    in
+    std.lib.ops.mkStandardOCI {
+      name = "iohkbuild/marlowe";
+      tag = "${tagName}-${tagDate}";
+      operable = operables.${name};
+    };
 in
 {
   chain-indexer = mkImage "chain-indexer";
@@ -16,4 +28,10 @@ in
   marlowe-history = mkImage "marlowe-history";
   marlowe-discovery = mkImage "marlowe-discovery";
   marlowe-tx = mkImage "marlowe-tx";
+
+  chain-indexer-public = mkPublicImage "chain-indexer";
+  chainseekd-public = mkPublicImage "chainseekd";
+  marlowe-history-public = mkPublicImage "marlowe-history";
+  marlowe-discovery-public = mkPublicImage "marlowe-discovery";
+  marlowe-tx-public = mkPublicImage "marlowe-tx";
 }
