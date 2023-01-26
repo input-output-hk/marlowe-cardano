6764f88a4a25e66791794fa3c272a52963044ed1
Jamie Bertram <jamie.bertram@tweag.io>
2023-01-26 09:43:36 -0700

    Implement getApplySteps

--- marlowe-runtime/sync/Language/Marlowe/Runtime/Sync/Database/PostgreSQL/GetNextSteps.hs
+++ marlowe-runtime/sync/Language/Marlowe/Runtime/Sync/Database/PostgreSQL/GetNextSteps.hs
@@ -3,14 +3,49 @@
 module Language.Marlowe.Runtime.Sync.Database.PostgreSQL.GetNextSteps
   where
 
+import Control.Applicative ((<|>))
+import qualified Control.Foldl as Fold
+import Data.Binary (get)
+import Data.Binary.Get (runGet)
+import Data.ByteString (ByteString)
+import Data.ByteString.Lazy (fromStrict)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe (fromJust)
+import Data.Time (localTimeToUTC, utc)
 import qualified Data.Vector as V
-import Hasql.TH (maybeStatement)
+import Hasql.TH (foldStatement, maybeStatement)
 import qualified Hasql.Transaction as T
+import Language.Marlowe.Core.V1.Semantics (MarloweData(..), MarloweParams(MarloweParams))
 import Language.Marlowe.Runtime.ChainSync.Api
-  (BlockHeader(..), BlockHeaderHash(..), ChainPoint, TxId(..), TxOutRef(..), WithGenesis(..))
-import Language.Marlowe.Runtime.Core.Api (ContractId(..), MarloweVersion(..), MarloweVersionTag(V1), Transaction)
+  ( Address(Address)
+  , AssetId(AssetId)
+  , Assets(..)
+  , BlockHeader(..)
+  , BlockHeaderHash(..)
+  , ChainPoint
+  , PolicyId(PolicyId)
+  , TokenName(TokenName)
+  , Tokens(Tokens)
+  , TxId(..)
+  , TxOutRef(..)
+  , WithGenesis(..)
+  , fromDatum
+  )
+import Language.Marlowe.Runtime.Core.Api
+  ( ContractId(..)
+  , MarloweVersion(..)
+  , MarloweVersionTag(V1)
+  , Payout(..)
+  , Transaction(..)
+  , TransactionOutput(..)
+  , TransactionScriptOutput(..)
+  )
 import Language.Marlowe.Runtime.History.Api (ContractStep(..), RedeemStep)
 import Language.Marlowe.Runtime.Sync.Database (NextSteps(..))
+import qualified Plutus.V2.Ledger.Api as PV2
+import Prelude hiding (init)
+import Witherable (catMaybes, mapMaybe)
 
 getNextSteps :: ContractId -> ChainPoint -> T.Transaction NextSteps
 getNextSteps contractId point = do
@@ -20,8 +55,8 @@ getNextSteps contractId point = do
     BeforeTip -> getNextTxIds contractId point >>= \case
       Nothing -> pure Wait
       Just NextTxIds{..} -> do
-        applySteps <- getApplySteps nextBlock nextApplyTxIds
-        redeemSteps <- getRedeemSteps nextBlock nextWithdrawalTxIds
+        applySteps <- getApplySteps nextBlock contractId nextApplyTxIds
+        redeemSteps <- getRedeemSteps nextWithdrawalTxIds
         pure $ Next MarloweV1 nextBlock $ (ApplyTransaction <$> applySteps) <> (RedeemPayout <$> redeemSteps)
 
 data Orientation
@@ -125,8 +160,245 @@ getNextTxIds (ContractId TxOutRef{..}) point = T.statement params $ fmap (NextTx
     decodeWithdrawalTxIds (_, _, _, _, ids) = decodeTxIds ids
     decodeTxIds ids = V.toList $ V.map TxId ids
 
-getRedeemSteps :: BlockHeader -> [TxId] -> T.Transaction [RedeemStep 'V1]
+getApplySteps :: BlockHeader -> ContractId -> [TxId] -> T.Transaction [Transaction 'V1]
+getApplySteps blockHeader contractId txIds = T.statement params $
+  [foldStatement|
+    WITH txIds (txId) AS
+      ( SELECT * FROM UNNEST ($1 :: bytea[])
+      )
+    SELECT
+      applyTx.txId :: bytea,
+      applyTx.metadata :: bytea?,
+      applyTx.invalidBefore :: timestamp,
+      applyTx.invalidHereafter :: timestamp,
+      applyTx.inputs :: bytea,
+      applyTx.outputTxIx :: smallint?,
+      payoutOut.txIx :: smallint?,
+      payoutOut.address :: bytea?,
+      payoutOut.lovelace :: bigint?,
+      payoutOut.policyIds :: bytea[]?,
+      payoutOut.tokenNames :: bytea[]?,
+      payoutOut.quantities :: bigint[]?,
+      payoutOut.rolesCurrency :: bytea?,
+      payoutOut.role :: bytea?,
+      contractOut.address :: bytea?,
+      contractOut.lovelace :: bigint?,
+      contractOut.policyIds :: bytea[]?,
+      contractOut.tokenNames :: bytea[]?,
+      contractOut.quantities :: bigint[]?,
+      contractOut.rolesCurrency :: bytea?,
+      contractOut.state :: bytea?,
+      contractOut.contract :: bytea?
+    FROM marlowe.applyTx
+    JOIN txIds USING (txId)
+    LEFT JOIN
+      ( SELECT
+          payoutTxOut.txId AS txId,
+          payoutTxOut.txIx AS txIx,
+          (ARRAY_AGG(txOut.address))[1] AS address,
+          (ARRAY_AGG(txOut.lovelace))[1] AS lovelace,
+          (ARRAY_REMOVE(ARRAY_AGG(txOutAsset.policyId), NULL)) AS policyIds,
+          (ARRAY_REMOVE(ARRAY_AGG(txOutAsset.name), NULL)) AS tokenNames,
+          (ARRAY_REMOVE(ARRAY_AGG(txOutAsset.quantity), NULL)) AS quantities,
+          (ARRAY_AGG(payoutTxOut.rolesCurrency))[1] AS rolesCurrency,
+          (ARRAY_AGG(payoutTxOut.role))[1] AS role
+        FROM marlowe.payoutTxOut
+        JOIN txIds USING (txId)
+        JOIN marlowe.txOut USING (txId, txIx)
+        LEFT JOIN marlowe.txOutAsset USING (txId, txIx)
+        GROUP BY payoutTxOut.txId, payoutTxOut.txIx
+      ) AS payoutOut USING (txId)
+    LEFT JOIN
+      ( SELECT
+          contractTxOut.txId AS txId,
+          contractTxOut.txIx AS txIx,
+          (ARRAY_AGG(txOut.address))[1] AS address,
+          (ARRAY_AGG(txOut.lovelace))[1] AS lovelace,
+          (ARRAY_REMOVE(ARRAY_AGG(txOutAsset.policyId), NULL)) AS policyIds,
+          (ARRAY_REMOVE(ARRAY_AGG(txOutAsset.name), NULL)) AS tokenNames,
+          (ARRAY_REMOVE(ARRAY_AGG(txOutAsset.quantity), NULL)) AS quantities,
+          (ARRAY_AGG(contractTxOut.rolesCurrency))[1] AS rolesCurrency,
+          (ARRAY_AGG(contractTxOut.state))[1] AS state,
+          (ARRAY_AGG(contractTxOut.contract))[1] AS contract
+        FROM marlowe.contractTxOut
+        JOIN txIds USING (txId)
+        JOIN marlowe.txOut USING (txId, txIx)
+        LEFT JOIN marlowe.txOutAsset USING (txId, txIx)
+        GROUP BY contractTxOut.txId, contractTxOut.txIx
+      ) AS contractOut USING (txId)
+  |] resultFold
+  where
+    resultFold = catMaybes . Map.elems <$> Fold.groupBy extractTxId (mergeWithChildren extractTx addPayouts foldPayouts)
+
+    extractTxId
+      ( txId
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      ) = txId
+
+    extractTx
+      ( txId
+      , metadata
+      , invalidBefore
+      , invalidHereafter
+      , inputs
+      , outputTxIx
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , outputAddress
+      , outputLovelace
+      , outputPolicyIds
+      , outputTokenNames
+      , outputQuantities
+      , outputRolesCurrency
+      , outputState
+      , outputContract
+      ) = Transaction
+        { transactionId = TxId txId
+        , contractId
+        , metadata = maybe mempty (runGet get. fromStrict) metadata
+        , blockHeader
+        , validityLowerBound = localTimeToUTC utc invalidBefore
+        , validityUpperBound = localTimeToUTC utc invalidHereafter
+        , inputs = fromJust $ fromDatum $ runGet get $ fromStrict inputs
+        , output = TransactionOutput
+          { payouts = mempty
+          , scriptOutput = do
+            txIx <- outputTxIx
+            address <- outputAddress
+            lovelace <- outputLovelace
+            policyIds <- V.toList <$> outputPolicyIds
+            tokenNames <- V.toList <$> outputTokenNames
+            quantities <- V.toList <$> outputQuantities
+            rolesCurrency :: ByteString <- outputRolesCurrency
+            state <- outputState
+            contract <- outputContract
+            pure TransactionScriptOutput
+              { address = Address address
+              , assets = Assets
+                { ada = fromIntegral lovelace
+                , tokens = Tokens $ Map.fromList $ zipWith3
+                  (\p t q -> (AssetId (PolicyId p) (TokenName t), fromIntegral q))
+                  policyIds
+                  tokenNames
+                  quantities
+                }
+              , utxo = TxOutRef (TxId txId) (fromIntegral txIx)
+              , datum = MarloweData
+                { marloweParams = MarloweParams $ PV2.CurrencySymbol $ PV2.toBuiltin rolesCurrency
+                , marloweState = fromJust $ fromDatum $ runGet get $ fromStrict state
+                , marloweContract = fromJust $ fromDatum $ runGet get $ fromStrict contract
+                }
+              }
+          }
+        } :: Transaction 'V1
+
+    foldPayouts = Map.fromList . mapMaybe (\row -> (,) <$> extractPayoutTxOutRef row <*> extractPayout row) <$> Fold.list
+
+    extractPayoutTxOutRef
+      ( txId
+      , _
+      , _
+      , _
+      , _
+      , _
+      , txIx
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      ) = TxOutRef (TxId txId) . fromIntegral <$> txIx
+
+    extractPayout
+      ( _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , address
+      , lovelace
+      , policyIds
+      , tokenNames
+      , quantities
+      , rolesCurrency
+      , role
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      , _
+      ) = Payout @'V1
+        <$> (Address <$> address)
+        <*> ( Assets
+              <$> (fromIntegral <$> lovelace)
+              <*> ( Tokens . Map.fromList <$>
+                    ( zipWith3 (\p t q -> (AssetId (PolicyId p) (TokenName t), fromIntegral q))
+                        <$> (V.toList <$> policyIds)
+                        <*> (V.toList <$> tokenNames)
+                        <*> (V.toList <$> quantities)
+                    )
+                  )
+            )
+        <*> ( AssetId
+              <$> (PolicyId <$> rolesCurrency)
+              <*> (TokenName <$> role)
+            )
+
+    addPayouts :: Map TxOutRef (Payout v) -> Transaction v -> Transaction v
+    addPayouts payouts' tx@Transaction{output} = tx
+      { output = output
+          { payouts = payouts output <> payouts'
+          }
+      }
+
+    params = V.fromList $ unTxId <$> txIds
+
+getRedeemSteps :: [TxId] -> T.Transaction [RedeemStep 'V1]
 getRedeemSteps = error "not implemented"
 
-getApplySteps :: BlockHeader -> [TxId] -> T.Transaction [Transaction 'V1]
-getApplySteps = error "not implemented"
+mergeWithChildren :: (a -> r) -> (c -> r -> r) -> Fold.Fold a c -> Fold.Fold a (Maybe r)
+mergeWithChildren extractParent mergeChild (Fold.Fold fChild iChild pChild) = Fold.Fold foldRow (Nothing, iChild) mapResult
+  where
+    foldRow (mParent, children) row = (mParent <|> Just (extractParent row), fChild children row)
+    mapResult (mParent, children) = mergeChild (pChild children) <$> mParent
