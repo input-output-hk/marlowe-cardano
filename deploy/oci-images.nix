e124085f3a369a0f5cfea885e860773cf8e0e19c
Parthiv Seetharaman <pachum99@myrdd.info>
2023-02-09 09:52:12 -0800

    only use dockerhub registry

--- deploy/oci-images.nix
+++ deploy/oci-images.nix
@@ -5,13 +5,7 @@ let
   inherit (self) operables;
   inherit (self.sourceInfo) lastModifiedDate;
 
-  mkImage = name: args: std.lib.ops.mkStandardOCI ({
-    name = "registry.ci.iog.io/dapps-world-${name}";
-    operable = operables.${name};
-    debug = true;
-  } // args);
-
-  mkPublicImage = name: args:
+  mkImage = name: args:
     let
       tagName = removePrefix "marlowe-" name;
       tagDate = builtins.substring 0 8 lastModifiedDate; # pull out just date
@@ -34,10 +28,4 @@ in
   marlowe-history = mkImage "marlowe-history" {};
   marlowe-discovery = mkImage "marlowe-discovery" {};
   marlowe-tx = mkImage "marlowe-tx" {};
-
-  chain-indexer-public = mkPublicImage "chain-indexer" rootConfig;
-  chainseekd-public = mkPublicImage "chainseekd" rootConfig;
-  marlowe-history-public = mkPublicImage "marlowe-history" {};
-  marlowe-discovery-public = mkPublicImage "marlowe-discovery" {};
-  marlowe-tx-public = mkPublicImage "marlowe-tx" {};
 }
