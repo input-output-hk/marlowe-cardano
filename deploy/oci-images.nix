580f5dfc63a75ddf551185a0a53b81ec5a1e1431
Jamie Bertram <jamie.bertram@tweag.io>
2023-02-13 06:37:01 -0700

    Make all containers run as root

--- deploy/oci-images.nix
+++ deploy/oci-images.nix
@@ -5,29 +5,24 @@ let
   inherit (self) operables;
   inherit (self.sourceInfo) lastModifiedDate;
 
-  mkImage = name: args:
+  mkImage = name:
     let
       tagName = removePrefix "marlowe-" name;
       tagDate = builtins.substring 0 8 lastModifiedDate; # pull out just date
     in
-    std.lib.ops.mkStandardOCI
-      ({
-        name = "iohkbuild/marlowe";
-        tag = "${tagName}-${tagDate}";
-        operable = operables.${name};
-      }) // args;
-
-  rootConfig = {
-    uid = "0";
-    gid = "0";
-  };
+    std.lib.ops.mkStandardOCI {
+      name = "iohkbuild/marlowe";
+      tag = "${tagName}-${tagDate}";
+      operable = operables.${name};
+      uid = "0";
+      gid = "0";
+    };
 in
 {
-  # ensure chain-indexer and marlowe-chain-sync have access to node socket
-  chain-indexer = mkImage "chain-indexer" rootConfig;
-  marlowe-chain-sync = mkImage "marlowe-chain-sync" rootConfig;
-  marlowe-indexer = mkImage "marlowe-indexer" { };
-  marlowe-sync = mkImage "marlowe-sync" { };
-  marlowe-tx = mkImage "marlowe-tx" { };
-  marlowe-web-server = mkImage "marlowe-web-server" { };
+  chain-indexer = mkImage "chain-indexer";
+  marlowe-chain-sync = mkImage "marlowe-chain-sync";
+  marlowe-indexer = mkImage "marlowe-indexer";
+  marlowe-sync = mkImage "marlowe-sync";
+  marlowe-tx = mkImage "marlowe-tx";
+  marlowe-web-server = mkImage "marlowe-web-server";
 }
