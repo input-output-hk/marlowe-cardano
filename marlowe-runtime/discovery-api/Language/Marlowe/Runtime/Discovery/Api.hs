9d7a8faecc5d8b4e53bed8a0cb2f078cc42d1ab3
Jamie Bertram <jamie.bertram@tweag.io>
2023-02-21 12:22:23 -0700

    Remove unused protocols

--- marlowe-runtime/discovery-api/Language/Marlowe/Runtime/Discovery/Api.hs
+++ marlowe-runtime/discovery-api/Language/Marlowe/Runtime/Discovery/Api.hs
@@ -4,15 +4,11 @@
 module Language.Marlowe.Runtime.Discovery.Api
   where
 
-import Data.Aeson (ToJSON(..), Value(..), object, (.=))
-import Data.Binary (Binary, get, getWord8, put, putWord8)
-import Data.Type.Equality (type (:~:)(Refl))
-import Data.Void (Void, absurd)
+import Data.Aeson (ToJSON)
+import Data.Binary (Binary)
 import GHC.Generics (Generic)
 import Language.Marlowe.Runtime.ChainSync.Api (Address, BlockHeader, PolicyId, ScriptHash, TransactionMetadata)
 import Language.Marlowe.Runtime.Core.Api (ContractId, SomeMarloweVersion)
-import Network.Protocol.Handshake.Types (HasSignature(..))
-import Network.Protocol.Query.Types (IsQuery(..), QueryToJSON(..), SomeTag(..))
 
 -- | A Marlowe contract header is a compact structure that contains all the
 -- significant metadata related to a Marlowe Contract on chain.
@@ -36,72 +32,3 @@ data ContractHeader = ContractHeader
   } deriving (Show, Eq, Ord, Generic, Binary)
 
 instance ToJSON ContractHeader
-
--- | The queries supported by the discovery service.
-data DiscoveryQuery delimiter err result where
-  -- | Query the full list of contract headers. Results are delivered in pages
-  -- and use a unit value to mark the next page.
-  GetContractHeaders :: DiscoveryQuery () Void [ContractHeader]
-  -- | Query all contract headers that use the given minting policy ID for role
-  -- tokens.
-  GetContractHeadersByRoleTokenCurrency :: PolicyId -> DiscoveryQuery Void Void [ContractHeader]
-
-instance HasSignature DiscoveryQuery where
-  signature _ = "DiscoveryQuery"
-
-instance QueryToJSON DiscoveryQuery where
-  queryToJSON = \case
-    GetContractHeaders -> String "get-contract-headers"
-    GetContractHeadersByRoleTokenCurrency policyId -> object [ "get-contract-headers-by-policy-id" .= policyId ]
-  errToJSON = \case
-    TagGetContractHeaders -> toJSON
-    TagGetContractHeadersByRoleTokenCurrency -> toJSON
-  resultToJSON = \case
-    TagGetContractHeaders -> toJSON
-    TagGetContractHeadersByRoleTokenCurrency -> toJSON
-  delimiterToJSON = \case
-    TagGetContractHeaders -> toJSON
-    TagGetContractHeadersByRoleTokenCurrency -> toJSON
-
-instance IsQuery DiscoveryQuery where
-  data Tag DiscoveryQuery delimiter err result where
-    TagGetContractHeaders :: Tag DiscoveryQuery () Void [ContractHeader]
-    TagGetContractHeadersByRoleTokenCurrency :: Tag DiscoveryQuery Void Void [ContractHeader]
-  tagFromQuery = \case
-    GetContractHeaders -> TagGetContractHeaders
-    GetContractHeadersByRoleTokenCurrency _ -> TagGetContractHeadersByRoleTokenCurrency
-  tagEq TagGetContractHeaders TagGetContractHeaders = Just (Refl, Refl, Refl)
-  tagEq TagGetContractHeaders _ = Nothing
-  tagEq TagGetContractHeadersByRoleTokenCurrency TagGetContractHeadersByRoleTokenCurrency = Just (Refl, Refl, Refl)
-  tagEq TagGetContractHeadersByRoleTokenCurrency _ = Nothing
-  putTag = \case
-    TagGetContractHeaders -> putWord8 0x01
-    TagGetContractHeadersByRoleTokenCurrency -> putWord8 0x02
-  getTag = getWord8 >>= \case
-    0x01 -> pure $ SomeTag TagGetContractHeaders
-    0x02 -> pure $ SomeTag TagGetContractHeadersByRoleTokenCurrency
-    tag -> fail $ "Invalid query tag value " <> show tag
-  putQuery = \case
-    GetContractHeaders -> mempty
-    GetContractHeadersByRoleTokenCurrency policyId -> put policyId
-  getQuery = \case
-    TagGetContractHeaders -> pure GetContractHeaders
-    TagGetContractHeadersByRoleTokenCurrency -> GetContractHeadersByRoleTokenCurrency <$> get
-  putDelimiter = \case
-    TagGetContractHeaders -> put
-    TagGetContractHeadersByRoleTokenCurrency -> absurd
-  getDelimiter = \case
-    TagGetContractHeaders -> get
-    TagGetContractHeadersByRoleTokenCurrency -> fail "No delimiter defined"
-  putErr = \case
-    TagGetContractHeaders -> absurd
-    TagGetContractHeadersByRoleTokenCurrency -> absurd
-  getErr = \case
-    TagGetContractHeaders -> fail "No error defined"
-    TagGetContractHeadersByRoleTokenCurrency -> fail "No error defined"
-  putResult = \case
-    TagGetContractHeaders -> put
-    TagGetContractHeadersByRoleTokenCurrency -> put
-  getResult = \case
-    TagGetContractHeaders -> get
-    TagGetContractHeadersByRoleTokenCurrency -> get
--- marlowe-runtime/history-api/Language/Marlowe/Runtime/History/Api.hs
+++ marlowe-runtime/history-api/Language/Marlowe/Runtime/History/Api.hs
@@ -12,18 +12,15 @@ import Control.Error (listToMaybe, note, runMaybeT)
 import Control.Error.Util (hoistMaybe)
 import Control.Monad (guard, when)
 import Control.Monad.Trans.Class (lift)
-import Data.Aeson (ToJSON, Value(..), object, toJSON, (.=))
+import Data.Aeson (ToJSON, object, toJSON, (.=))
 import Data.Bifunctor (first)
-import Data.Binary (Binary, get, getWord8, put, putWord8)
-import qualified Data.ByteString.Lazy as LBS
+import Data.Binary (Binary, get, put)
 import Data.Foldable (find, for_)
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Maybe (mapMaybe)
 import Data.Set (Set)
 import Data.Traversable (for)
-import Data.Type.Equality (type (:~:)(Refl))
-import Data.Void (Void, absurd)
 import GHC.Generics (Generic)
 import qualified Language.Marlowe.Core.V1.Semantics.Types as V1
 import Language.Marlowe.Runtime.ChainSync.Api (ScriptHash, TxError, TxId, TxOutRef(..), UTxOError)
@@ -31,17 +28,6 @@ import qualified Language.Marlowe.Runtime.ChainSync.Api as Chain
 import Language.Marlowe.Runtime.Core.Api
 import Language.Marlowe.Runtime.Core.ScriptRegistry (MarloweScripts(..), getMarloweVersion)
 import qualified Language.Marlowe.Scripts as V1
-import Network.Protocol.ChainSeek.Codec (DeserializeError)
-import Network.Protocol.Handshake.Types (HasSignature(..))
-import Network.Protocol.Job.Client
-import Network.Protocol.Job.Codec
-import Network.Protocol.Job.Server
-import Network.Protocol.Job.Types
-import Network.Protocol.Query.Client (QueryClient)
-import Network.Protocol.Query.Codec (codecQuery)
-import Network.Protocol.Query.Server (QueryServer)
-import qualified Network.Protocol.Query.Types as Query
-import Network.TypedProtocol.Codec
 import Ouroboros.Consensus.BlockchainTime (SystemStart, fromRelativeTime)
 import Ouroboros.Consensus.HardFork.History (interpretQuery, slotToWallclock)
 import qualified Ouroboros.Network.Block as O
@@ -182,114 +168,6 @@ getOutput (Chain.TxIx i) Chain.Transaction{..} = go i outputs
     go 0 (x : _)   = Right x
     go i' (_ : xs) = go (i' - 1) xs
 
-data HistoryCommand status err result where
-  FollowContract :: ContractId -> HistoryCommand Void ContractHistoryError Bool
-  StopFollowingContract :: ContractId -> HistoryCommand Void Void Bool
-
-instance HasSignature HistoryCommand where
-  signature _ = "HistoryCommand"
-
-instance CommandToJSON HistoryCommand where
-  commandToJSON = \case
-    FollowContract contractId -> object [ "follow-contract" .= contractId ]
-    StopFollowingContract contractId -> object [ "stop-following-contract" .= contractId ]
-  jobIdToJSON = \case
-  errToJSON = \case
-    TagFollowContract -> toJSON
-    TagStopFollowingContract -> toJSON
-  resultToJSON = \case
-    TagFollowContract -> toJSON
-    TagStopFollowingContract -> toJSON
-  statusToJSON = \case
-    TagFollowContract -> toJSON
-    TagStopFollowingContract -> toJSON
-
-instance Command HistoryCommand where
-  data JobId HistoryCommand status err result where
-
-  data Tag HistoryCommand status err result where
-    TagFollowContract :: Tag HistoryCommand Void ContractHistoryError Bool
-    TagStopFollowingContract :: Tag HistoryCommand Void Void Bool
-
-  tagFromCommand = \case
-    FollowContract _        -> TagFollowContract
-    StopFollowingContract _ -> TagStopFollowingContract
-
-  tagFromJobId = \case
-
-  tagEq = curry \case
-    (TagFollowContract, TagFollowContract)               -> Just (Refl, Refl, Refl)
-    (TagFollowContract, _)                               -> Nothing
-    (TagStopFollowingContract, TagStopFollowingContract) -> Just (Refl, Refl, Refl)
-    (TagStopFollowingContract, _)                        -> Nothing
-
-  putTag = \case
-    TagFollowContract        -> putWord8 0x01
-    TagStopFollowingContract -> putWord8 0x02
-
-  getTag = do
-    tag <- getWord8
-    case tag of
-      0x01 -> pure $ SomeTag TagFollowContract
-      0x02 -> pure $ SomeTag TagStopFollowingContract
-      _    -> fail $ "Invalid HistoryCommand tag: " <> show tag
-
-  putCommand = \case
-    FollowContract contractId        -> put contractId
-
-    StopFollowingContract contractId -> put contractId
-
-  getCommand = \case
-    TagFollowContract        -> FollowContract <$> get
-    TagStopFollowingContract -> StopFollowingContract <$> get
-
-  putJobId = \case
-
-  getJobId _ = fail "History commands have no job IDs"
-
-  putStatus = \case
-    TagFollowContract        -> absurd
-    TagStopFollowingContract -> absurd
-
-  getStatus _ = fail "History commands have no statuses"
-
-  putErr = \case
-    TagFollowContract        -> put
-    TagStopFollowingContract -> absurd
-
-  getErr = \case
-    TagFollowContract        -> get
-    TagStopFollowingContract -> fail "StopFollowingContract has no error"
-
-  putResult = \case
-    TagFollowContract        -> put
-    TagStopFollowingContract -> put
-
-  getResult = \case
-    TagFollowContract        -> get
-    TagStopFollowingContract -> get
-
-type RuntimeHistoryJob = Job HistoryCommand
-
-type RuntimeHistoryJobClient = JobClient HistoryCommand
-
-type RuntimeHistoryJobServer = JobServer HistoryCommand
-
-type RuntimeHistoryJobCodec m = Codec RuntimeHistoryJob DeserializeError m LBS.ByteString
-
-historyJobCodec :: Applicative m => RuntimeHistoryJobCodec m
-historyJobCodec = codecJob
-
-data History v = History
-  { create      :: CreateStep v
-  , createBlock :: Chain.BlockHeader
-  , steps       :: Map Chain.BlockHeader [ContractStep v]
-  } deriving Generic
-
-deriving instance Show (History 'V1)
-deriving instance Eq (History 'V1)
-instance Binary (History 'V1)
-
 extractMarloweTransaction
   :: MarloweVersion v
   -> SystemStart
@@ -372,94 +250,3 @@ isToAddress toAddress Chain.TransactionOutput{..} = address == toAddress
 consumesUTxO :: TxOutRef -> Chain.TransactionInput -> Bool
 consumesUTxO TxOutRef{..} Chain.TransactionInput { txId = txInId, txIx = txInIx } =
   txId == txInId && txIx == txInIx
-
-data SomeHistory = forall v. SomeHistory (MarloweVersion v) (History v)
-
-data HistoryQuery delimiter err results where
-  GetFollowedContracts :: HistoryQuery ContractId Void (Map ContractId FollowerStatus)
-  GetStatuses :: Set ContractId -> HistoryQuery Void Void (Map ContractId FollowerStatus)
-
-instance HasSignature HistoryQuery where
-  signature _ = "HistoryQuery"
-
-instance Query.QueryToJSON HistoryQuery where
-  queryToJSON = \case
-    GetFollowedContracts -> String "get-followed-contracts"
-    GetStatuses contractIds -> object [ "get-statuses" .= contractIds ]
-  errToJSON = \case
-    TagGetFollowedContracts -> toJSON
-    TagGetStatuses -> toJSON
-  resultToJSON = \case
-    TagGetFollowedContracts -> toJSON
-    TagGetStatuses -> toJSON
-  delimiterToJSON = \case
-    TagGetFollowedContracts -> toJSON
-    TagGetStatuses -> toJSON
-
-instance Query.IsQuery HistoryQuery where
-  data Tag HistoryQuery delimiter err result where
-    TagGetFollowedContracts :: Query.Tag HistoryQuery ContractId Void (Map ContractId FollowerStatus)
-    TagGetStatuses :: Query.Tag HistoryQuery Void Void (Map ContractId FollowerStatus)
-
-  tagFromQuery = \case
-    GetFollowedContracts -> TagGetFollowedContracts
-    GetStatuses _ -> TagGetStatuses
-
-  tagEq TagGetFollowedContracts TagGetFollowedContracts = Just (Refl, Refl, Refl)
-  tagEq TagGetFollowedContracts _ = Nothing
-  tagEq TagGetStatuses TagGetStatuses = Just (Refl, Refl, Refl)
-  tagEq TagGetStatuses _ = Nothing
-
-  putTag = \case
-    TagGetFollowedContracts -> putWord8 0x01
-    TagGetStatuses -> putWord8 0x02
-
-  getTag = do
-    tagWord <- getWord8
-    case tagWord of
-      0x01 -> pure $ Query.SomeTag TagGetFollowedContracts
-      0x02 -> pure $ Query.SomeTag TagGetStatuses
-      _    -> fail "invalid tag bytes"
-
-  putQuery = \case
-    GetFollowedContracts  -> mempty
-    GetStatuses contractIds  -> put contractIds
-
-  getQuery = \case
-    TagGetFollowedContracts -> pure GetFollowedContracts
-    TagGetStatuses -> GetStatuses <$> get
-
-  putDelimiter = \case
-    TagGetFollowedContracts -> put
-    TagGetStatuses -> put
-
-  getDelimiter = \case
-    TagGetFollowedContracts -> get
-    TagGetStatuses -> get
-
-  putErr = \case
-    TagGetFollowedContracts -> put
-    TagGetStatuses -> put
-
-  getErr = \case
-    TagGetFollowedContracts -> get
-    TagGetStatuses -> get
-
-  putResult = \case
-    TagGetFollowedContracts -> put
-    TagGetStatuses -> put
-
-  getResult = \case
-    TagGetFollowedContracts -> get
-    TagGetStatuses -> get
-
-type RuntimeHistoryQuery = Query.Query HistoryQuery
-
-type RuntimeHistoryQueryClient = QueryClient HistoryQuery
-
-type RuntimeHistoryQueryServer = QueryServer HistoryQuery
-
-type RuntimeHistoryQueryCodec m = Codec RuntimeHistoryQuery DeserializeError m LBS.ByteString
-
-historyQueryCodec :: Applicative m => RuntimeHistoryQueryCodec m
-historyQueryCodec = codecQuery
