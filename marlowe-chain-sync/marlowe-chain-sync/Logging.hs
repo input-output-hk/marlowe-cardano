15ae96df92b123f3656d3487cf8f1faf8fcba6e6
Jamie Bertram <jamie.bertram@tweag.io>
2023-02-21 08:28:53 -0700

    Update marlowe-chain-sync

--- marlowe-chain-sync/marlowe-chain-sync/Logging.hs
+++ marlowe-chain-sync/marlowe-chain-sync/Logging.hs
@@ -9,7 +9,7 @@ module Logging
   ) where
 
 import Language.Marlowe.Runtime.ChainSync.Api (ChainSyncCommand, ChainSyncQuery, RuntimeChainSeek)
-import Network.Protocol.Driver (ConnectionSourceSelector, getConnectionSourceSelectorConfig)
+import Network.Protocol.Driver (ConnectorSelector, getConnectorSelectorConfig)
 import Network.Protocol.Handshake.Types (Handshake)
 import Network.Protocol.Job.Types (Job)
 import Network.Protocol.Query.Types (Query)
@@ -17,16 +17,16 @@ import Observe.Event.Component
   (ConfigWatcherSelector(..), GetSelectorConfig, SelectorConfig(..), prependKey, singletonFieldConfig)
 
 data RootSelector f where
-  ChainSeekServer :: ConnectionSourceSelector (Handshake RuntimeChainSeek) f -> RootSelector f
-  QueryServer :: ConnectionSourceSelector (Handshake (Query ChainSyncQuery)) f -> RootSelector f
-  JobServer :: ConnectionSourceSelector (Handshake (Job ChainSyncCommand)) f -> RootSelector f
+  ChainSeekServer :: ConnectorSelector (Handshake RuntimeChainSeek) f -> RootSelector f
+  QueryServer :: ConnectorSelector (Handshake (Query ChainSyncQuery)) f -> RootSelector f
+  JobServer :: ConnectorSelector (Handshake (Job ChainSyncCommand)) f -> RootSelector f
   ConfigWatcher :: ConfigWatcherSelector f -> RootSelector f
 
 -- TODO automate this boilerplate with Template Haskell
 getRootSelectorConfig :: GetSelectorConfig RootSelector
 getRootSelectorConfig = \case
-  ChainSeekServer sel -> prependKey "chain-sync" $ getConnectionSourceSelectorConfig True False sel
-  QueryServer sel -> prependKey "query" $ getConnectionSourceSelectorConfig True True sel
-  JobServer sel -> prependKey "job" $ getConnectionSourceSelectorConfig True True sel
+  ChainSeekServer sel -> prependKey "chain-sync" $ getConnectorSelectorConfig True False sel
+  QueryServer sel -> prependKey "query" $ getConnectorSelectorConfig True True sel
+  JobServer sel -> prependKey "job" $ getConnectorSelectorConfig True True sel
   ConfigWatcher ReloadConfig -> SelectorConfig "reload-log-config" True
     $ singletonFieldConfig "config" True
