32008d2d76ec29786d0f05c46ef89a9b377139d3
Jamie Bertram <jamie.bertram@tweag.io>
2023-03-09 05:47:25 -0700

    Fix CliSpec test

--- marlowe-integration-tests/test/Language/Marlowe/Runtime/CliSpec.hs
+++ marlowe-integration-tests/test/Language/Marlowe/Runtime/CliSpec.hs
@@ -9,7 +9,6 @@ import Cardano.Api (AsType(AsTxBody), BabbageEra, CardanoEra(BabbageEra), TxBody
 import Control.Concurrent.Async.Lifted (concurrently)
 import qualified Control.Monad.Base as Trans
 import qualified Control.Monad.Reader as Reader
-import qualified Data.Aeson as Aeson
 import qualified Data.Either as Either
 import qualified Data.Maybe as Maybe
 import qualified Data.Set as Set
@@ -17,22 +16,18 @@ import qualified Data.Text as Text
 import Data.Void (Void)
 import qualified Language.Marlowe.Core.V1.Semantics.Types as V1
 import Language.Marlowe.Runtime.Cardano.Api (cardanoEraToAsType)
-import Language.Marlowe.Runtime.ChainSync.Api (Address(..), Lovelace(Lovelace), toBech32)
+import Language.Marlowe.Runtime.ChainSync.Api (Address(..), Lovelace(Lovelace), TransactionMetadata, toBech32)
 import Language.Marlowe.Runtime.Client (runMarloweTxClient)
 import Language.Marlowe.Runtime.Core.Api
-  ( MarloweTransactionMetadata
-  , MarloweVersion(MarloweV1)
-  , MarloweVersionTag(V1)
-  , contractToJSON
-  , emptyMarloweTransactionMetadata
-  )
+  (MarloweTransactionMetadata(..), MarloweVersion(MarloweV1), MarloweVersionTag(V1), encodeMarloweTransactionMetadata)
 import Language.Marlowe.Runtime.Integration.Common
   (Integration, Wallet(..), execMarlowe_, getGenesisWallet, runIntegrationTest)
 import Language.Marlowe.Runtime.Transaction.Api
   (ContractCreated(..), CreateError, MarloweTxCommand(..), RoleTokensConfig(RoleTokensNone), WalletAddresses(..))
 import qualified Network.Protocol.Job.Client as JobClient
 import Test.Hspec (Spec, describe, it, shouldBe)
-import Test.Integration.Marlowe (LocalTestnet(..), Workspace(Workspace, workspaceDir), testnet, withLocalMarloweRuntime)
+import Test.Integration.Marlowe
+  (LocalTestnet(..), resolveWorkspacePath, testnet, withLocalMarloweRuntime, writeWorkspaceFileJSON)
 
 serializeAddress :: Address -> String
 serializeAddress = Text.unpack . Maybe.fromJust . toBech32
@@ -55,29 +50,22 @@ spec = describe "Marlowe runtime CLI" do
       withLocalMarloweRuntime $ runIntegrationTest do
         Wallet {addresses} <- getGenesisWallet 0
 
-        LocalTestnet {workspace = Workspace {workspaceDir}} <- Reader.asks testnet
+        workspace <- Reader.asks $ workspace . testnet
 
         let roleTokensConfig :: RoleTokensConfig
             roleTokensConfig = RoleTokensNone
 
-            transactionMetadata :: MarloweTransactionMetadata
-            transactionMetadata = emptyMarloweTransactionMetadata
+            transactionMetadata :: TransactionMetadata
+            transactionMetadata = mempty
 
             contract :: V1.Contract
             contract = V1.Close
 
-            transactionMetadataFilePath :: FilePath
-            transactionMetadataFilePath = workspaceDir <> "/transaction-metadata"
-
-            contractFilePath :: FilePath
-            contractFilePath = workspaceDir <> "/close-contract"
-
             txBodyEnvelopeFilePath :: FilePath
-            txBodyEnvelopeFilePath = workspaceDir <> "/tx-body.envelope"
+            txBodyEnvelopeFilePath = resolveWorkspacePath workspace "tx-body.json"
 
-        Trans.liftBase do
-          Aeson.encodeFile contractFilePath $ contractToJSON MarloweV1 contract
-          Aeson.encodeFile transactionMetadataFilePath transactionMetadata
+        contractFilePath <- writeWorkspaceFileJSON workspace "close-contract.json" contract
+        transactionMetadataFilePath <- writeWorkspaceFileJSON workspace "transaction-metadata.json" transactionMetadata
 
         let creationCommand :: MarloweTxCommand Void (CreateError 'V1) (ContractCreated BabbageEra 'V1)
             creationCommand =
@@ -86,7 +74,7 @@ spec = describe "Marlowe runtime CLI" do
                 MarloweV1
                 addresses
                 roleTokensConfig
-                transactionMetadata
+                (MarloweTransactionMetadata Nothing transactionMetadata)
                 (Lovelace 2_000_000)
                 contract
 
