09e010e1fcbbd07d2789e050df807784fa8d406f
Jamie Bertram <jamie.bertram@tweag.io>
2023-03-17 12:49:30 -0400

    Fix OCI image targets in Hydra:

--- ci.nix
+++ ci.nix
@@ -80,8 +80,7 @@ let
           inherit (packages) compose-spec;
 
           # Build all oci image uploaders
-          copyAllOciImagesToRegistry = oci-images.all.copyToRegistry;
-          copyAllOciImagesToDockerDaemon = oci-images.all.copyToDockerDaemon;
+          copyAllOciImagesToRegistryLatest = oci-images.latest.all.copyToRegistry;
         } // pkgs.lib.optionalAttrs (!rootsOnly) (filterCross {
           # build relevant top level attributes from flake.nix
           inherit (packages) tests;
