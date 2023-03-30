3d8c72678ed2166506011cff231750ac600fe982
Larry Adames <adames.larry@gmail.com>
2023-03-30 14:23:47 -0400

    PLT-4186 post and put withdrawal endpoint testing

--- marlowe-integration-tests/test/Language/Marlowe/Runtime/Web/PutWithdrawal.hs
+++ marlowe-integration-tests/test/Language/Marlowe/Runtime/Web/PutWithdrawal.hs
@@ -2,10 +2,13 @@ module Language.Marlowe.Runtime.Web.PutWithdrawal
   where
 
 import Control.Monad.IO.Class (MonadIO(liftIO))
+import qualified Data.Set as Set
 import Language.Marlowe.Runtime.Integration.Common
+import Language.Marlowe.Runtime.Transaction.Api (WalletAddresses(..))
 import qualified Language.Marlowe.Runtime.Web as Web
-import Language.Marlowe.Runtime.Web.Client (putWithdrawal)
+import Language.Marlowe.Runtime.Web.Client (postWithdrawal, putWithdrawal)
 import Language.Marlowe.Runtime.Web.Common (signShelleyTransaction')
+import Language.Marlowe.Runtime.Web.Server.DTO (ToDTO(toDTO))
 import Language.Marlowe.Runtime.Web.StandardContract
   ( StandardContractChoiceMade(..)
   , StandardContractClosed(..)
@@ -14,25 +17,40 @@ import Language.Marlowe.Runtime.Web.StandardContract
   , StandardContractNotified(..)
   , createStandardContract
   )
-import Test.Hspec (Spec, describe, focus, it)
+import Test.Hspec (Spec, describe, it)
 import Test.Integration.Marlowe.Local (withLocalMarloweRuntime)
 
 spec :: Spec
-spec = focus $ describe "PUT /contracts/{contractId}/withdrawals/{withdrawalId}" do
-  it "processes a withdrawal successfully with a valid Withdrawal Id" $ withLocalMarloweRuntime $ runIntegrationTest do
+spec = describe "PUT /contracts/{contractId}/withdrawals/{withdrawalId}" do
+  it "successfully submits a withdrawal" $ withLocalMarloweRuntime $ runIntegrationTest do
     partyAWallet@Wallet{signingKeys} <- getGenesisWallet 0
     partyBWallet <- getGenesisWallet 1
 
     result <- runWebClient do
-      StandardContractInit{makeInitialDeposit} <- createStandardContract partyAWallet partyBWallet
+      let WalletAddresses{..} = addresses partyAWallet
+      let webChangeAddress = toDTO changeAddress
+      let webExtraAddresses = Set.map toDTO extraAddresses
+      let webCollataralUtxos = Set.map toDTO collateralUtxos
+      StandardContractInit{contractCreated, makeInitialDeposit} <- createStandardContract partyAWallet partyBWallet
       StandardContractFundsDeposited{chooseGimmeTheMoney} <- makeInitialDeposit
       StandardContractChoiceMade{sendNotify} <- chooseGimmeTheMoney
       StandardContractNotified{makeReturnDeposit} <- sendNotify
-      StandardContractClosed{withdrawPartyAFunds} <- makeReturnDeposit
+      StandardContractClosed{} <- makeReturnDeposit
 
-      (Web.WithdrawTxBody{txBody = withdrawTxBody, withdrawalId}, _) <- withdrawPartyAFunds
+      contractId <- case contractCreated of
+        Web.CreateTxBody{contractId} -> pure contractId
+
+      Web.WithdrawTxBody{withdrawalId, txBody = withdrawTxBody} <- postWithdrawal
+        webChangeAddress
+        (Just webExtraAddresses)
+        (Just webCollataralUtxos)
+        Web.PostWithdrawalsRequest
+          { role = "Party A"
+          , contractId
+          }
       signedWithdrawalTx <- liftIO $ signShelleyTransaction' withdrawTxBody signingKeys
       putWithdrawal withdrawalId signedWithdrawalTx
+
     case result of
       Left _ ->  fail $ "Expected 200 response code - got " <> show result
       Right () ->  pure ()
