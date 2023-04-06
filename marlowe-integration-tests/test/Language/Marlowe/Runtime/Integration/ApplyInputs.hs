c9e4ee8a297aace6ed4d74ca0d2c57bbb7d56b91
Jamie Bertram <jamie.bertram@tweag.io>
2023-04-06 11:05:28 -0400

    Add Pay tests

--- marlowe-integration-tests/test/Language/Marlowe/Runtime/Integration/ApplyInputs.hs
+++ marlowe-integration-tests/test/Language/Marlowe/Runtime/Integration/ApplyInputs.hs
@@ -1,27 +1,40 @@
+{-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE DataKinds #-}
+
 module Language.Marlowe.Runtime.Integration.ApplyInputs
   where
 
 import Cardano.Api (BabbageEra, CardanoEra(..), TxBody(..), TxBodyContent(..), TxOut(..), getTxId)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Reader (ask)
+import Data.Functor (void)
+import qualified Data.List.NonEmpty as NE
+import Data.Maybe (fromJust)
 import qualified Data.Set as Set
-import Data.Time (getCurrentTime)
+import Data.Time (UTCTime, addUTCTime, getCurrentTime, secondsToNominalDiffTime)
+import Data.Time.Clock.POSIX (utcTimeToPOSIXSeconds)
+import Language.Marlowe.Core.V1.Semantics (MarloweData(..))
 import Language.Marlowe.Core.V1.Semantics.Types
-import Language.Marlowe.Runtime.Cardano.Api (fromCardanoAddressInEra, fromCardanoTxId)
-import Language.Marlowe.Runtime.ChainSync.Api (TxOutRef(..))
+import qualified Language.Marlowe.Core.V1.Semantics.Types.Address as Address
+import Language.Marlowe.Extended.V1 (ada)
+import Language.Marlowe.Runtime.Cardano.Api (fromCardanoAddressInEra, fromCardanoTxId, fromCardanoTxOutValue)
+import Language.Marlowe.Runtime.ChainSync.Api (Assets(Assets), TokenName, TxOutRef(..))
 import Language.Marlowe.Runtime.Client (applyInputs, createContract)
 import Language.Marlowe.Runtime.Core.Api hiding (Contract)
 import Language.Marlowe.Runtime.Integration.Common
-  (Integration, Wallet(..), expectRight, getGenesisWallet, runIntegrationTest, submit)
+  (Integration, Wallet(..), expectJust, expectRight, getGenesisWallet, runIntegrationTest, submit)
+import Language.Marlowe.Runtime.Plutus.V2.Api (toPlutusAddress)
 import Language.Marlowe.Runtime.Transaction.Api
+import Plutus.V2.Ledger.Api (POSIXTime(POSIXTime))
 import Test.Hspec
 import Test.Integration.Marlowe (MarloweRuntime(..), withLocalMarloweRuntime)
+import UnliftIO (Concurrently(Concurrently, runConcurrently))
 
 spec :: Spec
-spec = focus $ describe "ApplyInputs" do
+spec = describe "ApplyInputs" do
   closedSpec
   closeSpec
+  paySpec
 
 closedSpec :: Spec
 closedSpec = describe "Closed contract" $ aroundAll setup do
@@ -109,6 +122,284 @@ closeSpec = describe "Close contract" $ aroundAll setup do
         []
       runtime <- ask
       liftIO $ runTests (runtime, (created, inputsApplied))
+
+data PayTestData = PayTestData
+  { payRoleAccountCreated :: ContractCreated BabbageEra 'V1
+  , payRoleAccountApplied :: InputsApplied BabbageEra 'V1
+  , payAddressAccountCreated :: ContractCreated BabbageEra 'V1
+  , payAddressAccountApplied :: InputsApplied BabbageEra 'V1
+  , payRolePartyCreated :: ContractCreated BabbageEra 'V1
+  , payRolePartyApplied :: InputsApplied BabbageEra 'V1
+  , payAddressPartyCreated :: ContractCreated BabbageEra 'V1
+  , payAddressPartyApplied :: InputsApplied BabbageEra 'V1
+  , payDepth1Created :: ContractCreated BabbageEra 'V1
+  , payDepth1Applied :: InputsApplied BabbageEra 'V1
+  , payDepth2AccountCreated :: ContractCreated BabbageEra 'V1
+  , payDepth2AccountApplied :: InputsApplied BabbageEra 'V1
+  , payDepth2PartyCreated :: ContractCreated BabbageEra 'V1
+  , payDepth2PartyApplied :: InputsApplied BabbageEra 'V1
+  , startTime :: UTCTime
+  }
+
+paySpec :: Spec
+paySpec = focus $ describe "Pay contracts" $ aroundAll setup do
+  describe "Pay to role account" do
+    it "should contain no output" $ runAsIntegration \PayTestData{..} -> do
+      let InputsApplied{..} = payRoleAccountApplied
+      liftIO $ output `shouldBe` Nothing
+    it "should send a payout to the role validator" $ runAsIntegration \PayTestData{..} -> do
+      let ContractCreated{payoutScriptAddress} = payRoleAccountCreated
+      let InputsApplied{..} = payRoleAccountApplied
+      let isPayout (TxOut address _ _ _) = fromCardanoAddressInEra BabbageEra address == payoutScriptAddress
+      let getValue (TxOut _ value _ _) = fromCardanoTxOutValue value
+      let payoutOutputs = getValue <$> case txBody of TxBody TxBodyContent{..} -> filter isPayout txOuts
+      liftIO $ payoutOutputs `shouldBe` [Assets 2_000_000 mempty]
+  describe "Pay to address account" do
+    it "should contain no output" $ runAsIntegration \PayTestData{..} -> do
+      let InputsApplied{..} = payAddressAccountApplied
+      liftIO $ output `shouldBe` Nothing
+    it "should send a payout to the wallet" $ runAsIntegration \PayTestData{..} -> do
+      wallet2 <- getGenesisWallet 1
+      let InputsApplied{..} = payAddressAccountApplied
+      let isPayout (TxOut address _ _ _) = fromCardanoAddressInEra BabbageEra address == changeAddress (addresses wallet2)
+      let getValue (TxOut _ value _ _) = fromCardanoTxOutValue value
+      let payoutOutputs = getValue <$> case txBody of TxBody TxBodyContent{..} -> filter isPayout txOuts
+      liftIO $ payoutOutputs `shouldBe` [Assets 2_000_000 mempty]
+  describe "Pay to role party" do
+    it "should contain no output" $ runAsIntegration \PayTestData{..} -> do
+      let InputsApplied{..} = payRolePartyApplied
+      liftIO $ output `shouldBe` Nothing
+    it "should send a payout to the role validator" $ runAsIntegration \PayTestData{..} -> do
+      let ContractCreated{payoutScriptAddress} = payRolePartyCreated
+      let InputsApplied{..} = payRolePartyApplied
+      let isPayout (TxOut address _ _ _) = fromCardanoAddressInEra BabbageEra address == payoutScriptAddress
+      let getValue (TxOut _ value _ _) = fromCardanoTxOutValue value
+      let payoutOutputs = getValue <$> case txBody of TxBody TxBodyContent{..} -> filter isPayout txOuts
+      liftIO $ payoutOutputs `shouldBe` [Assets 2_000_000 mempty]
+  describe "Pay to address party" do
+    it "should contain no output" $ runAsIntegration \PayTestData{..} -> do
+      let InputsApplied{..} = payAddressPartyApplied
+      liftIO $ output `shouldBe` Nothing
+    it "should send a payout to the wallet" $ runAsIntegration \PayTestData{..} -> do
+      wallet2 <- getGenesisWallet 1
+      let InputsApplied{..} = payAddressPartyApplied
+      let isPayout (TxOut address _ _ _) = fromCardanoAddressInEra BabbageEra address == changeAddress (addresses wallet2)
+      let getValue (TxOut _ value _ _) = fromCardanoTxOutValue value
+      let payoutOutputs = getValue <$> case txBody of TxBody TxBodyContent{..} -> filter isPayout txOuts
+      liftIO $ payoutOutputs `shouldBe` [Assets 2_000_000 mempty]
+  describe "Pay with input inside" do
+    it "should contain no output" $ runAsIntegration \PayTestData{..} -> do
+      let InputsApplied{..} = payDepth1Applied
+      liftIO $ output `shouldBe` Nothing
+    it "should send a payout to the role validator" $ runAsIntegration \PayTestData{..} -> do
+      let ContractCreated{payoutScriptAddress} = payDepth1Created
+      let InputsApplied{..} = payDepth1Applied
+      let isPayout (TxOut address _ _ _) = fromCardanoAddressInEra BabbageEra address == payoutScriptAddress
+      let getValue (TxOut _ value _ _) = fromCardanoTxOutValue value
+      let payoutOutputs = getValue <$> case txBody of TxBody TxBodyContent{..} -> filter isPayout txOuts
+      liftIO $ payoutOutputs `shouldBe` [Assets 2_000_000 mempty]
+  describe "Pay to account with two inputs inside" do
+    it "should contain the correct output" $ runAsIntegration \PayTestData{..} -> do
+      let ContractCreated{marloweScriptAddress, assets} = payDepth2AccountCreated
+      let InputsApplied{..} = payDepth2AccountApplied
+      TransactionScriptOutput address assets' utxo' MarloweData{..} <- expectJust "Expected an output" output
+      liftIO $ address `shouldBe` marloweScriptAddress
+      liftIO $ assets' `shouldBe` assets
+      liftIO $ utxo' `shouldBe` TxOutRef (fromCardanoTxId $ getTxId txBody) 1
+      liftIO $ marloweContract `shouldBe` When
+        [ Case (Notify TrueObs) Close
+        ]
+        (utcTimeToPOSIXTime $ addUTCTime (secondsToNominalDiffTime 200) startTime)
+        Close
+    it "should send no payout" $ runAsIntegration \PayTestData{..} -> do
+      let ContractCreated{payoutScriptAddress} = payDepth2AccountCreated
+      let InputsApplied{..} = payDepth2AccountApplied
+      let isPayout (TxOut address _ _ _) = fromCardanoAddressInEra BabbageEra address == payoutScriptAddress
+      let getValue (TxOut _ value _ _) = fromCardanoTxOutValue value
+      let payoutOutputs = getValue <$> case txBody of TxBody TxBodyContent{..} -> filter isPayout txOuts
+      liftIO $ payoutOutputs `shouldBe` []
+  describe "Pay to party with two inputs inside" do
+    it "should contain the correct output" $ runAsIntegration \PayTestData{..} -> do
+      let ContractCreated{marloweScriptAddress} = payDepth2PartyCreated
+      let InputsApplied{..} = payDepth2PartyApplied
+      TransactionScriptOutput address assets' utxo' MarloweData{..} <- expectJust "Expected an output" output
+      liftIO $ address `shouldBe` marloweScriptAddress
+      liftIO $ assets' `shouldBe` Assets 8_000_000 mempty
+      liftIO $ utxo' `shouldBe` TxOutRef (fromCardanoTxId $ getTxId txBody) 1
+      liftIO $ marloweContract `shouldBe` When
+        [ Case (Notify TrueObs) Close
+        ]
+        (utcTimeToPOSIXTime $ addUTCTime (secondsToNominalDiffTime 200) startTime)
+        Close
+    it "should send a payout to the role validator" $ runAsIntegration \PayTestData{..} -> do
+      let ContractCreated{payoutScriptAddress} = payDepth2PartyCreated
+      let InputsApplied{..} = payDepth2PartyApplied
+      let isPayout (TxOut address _ _ _) = fromCardanoAddressInEra BabbageEra address == payoutScriptAddress
+      let getValue (TxOut _ value _ _) = fromCardanoTxOutValue value
+      let payoutOutputs = getValue <$> case txBody of TxBody TxBodyContent{..} -> filter isPayout txOuts
+      liftIO $ payoutOutputs `shouldBe` [Assets 2_000_000 mempty]
+  where
+    setup :: ActionWith (MarloweRuntime, PayTestData) -> IO ()
+    setup runTests = withLocalMarloweRuntime $ runIntegrationTest do
+      startTime <- liftIO getCurrentTime
+      wallet1 <- getGenesisWallet 0
+      wallet2 <- getGenesisWallet 1
+      let timeout1 = addUTCTime (secondsToNominalDiffTime 100) startTime
+      let timeout2 = addUTCTime (secondsToNominalDiffTime 200) startTime
+      let walletParty = Address Address.testnet . fromJust . toPlutusAddress . changeAddress . addresses
+      let mkPay = Pay $ walletParty wallet1
+      payRoleAccountCreated <-
+        expectRight "Failed to create pay role account contract" =<< createContract
+          Nothing
+          MarloweV1
+          (addresses wallet1)
+          (mkRoleTokens [("Role", wallet2)])
+          emptyMarloweTransactionMetadata
+          2_000_000
+          (mkPay (Account $ Role "Role") ada (Constant 2_000_000) Close)
+      submitCreate wallet1 payRoleAccountCreated
+      payAddressAccountCreated <-
+        expectRight "Failed to create pay address account contract" =<< createContract
+          Nothing
+          MarloweV1
+          (addresses wallet1)
+          RoleTokensNone
+          emptyMarloweTransactionMetadata
+          2_000_000
+          (mkPay (Account $ walletParty wallet2) ada (Constant 2_000_000) Close)
+      submitCreate wallet1 payAddressAccountCreated
+      payRolePartyCreated <-
+        expectRight "Failed to create pay role party contract" =<< createContract
+          Nothing
+          MarloweV1
+          (addresses wallet1)
+          (mkRoleTokens [("Role", wallet2)])
+          emptyMarloweTransactionMetadata
+          2_000_000
+          (mkPay (Party $ Role "Role") ada (Constant 2_000_000) Close)
+      submitCreate wallet1 payRolePartyCreated
+      payAddressPartyCreated <-
+        expectRight "Failed to create pay address party contract" =<< createContract
+          Nothing
+          MarloweV1
+          (addresses wallet1)
+          RoleTokensNone
+          emptyMarloweTransactionMetadata
+          2_000_000
+          (mkPay (Party $ walletParty wallet2) ada (Constant 2_000_000) Close)
+      submitCreate wallet1 payAddressPartyCreated
+      payDepth1Created <-
+        expectRight "Failed to create pay depth 1 contract" =<< createContract
+          Nothing
+          MarloweV1
+          (addresses wallet1)
+          (mkRoleTokens [("Role", wallet2)])
+          emptyMarloweTransactionMetadata
+          2_000_000
+          (mkPay (Account $ Role "Role") ada (Constant 2_000_000) $
+            When
+              [ Case (Notify TrueObs) Close
+              ]
+              (utcTimeToPOSIXTime timeout1)
+              Close
+          )
+      submitCreate wallet1 payDepth1Created
+      payDepth2AccountCreated <-
+        expectRight "Failed to create pay to account depth 2 contract" =<< createContract
+          Nothing
+          MarloweV1
+          (addresses wallet1)
+          (mkRoleTokens [("Role", wallet2)])
+          emptyMarloweTransactionMetadata
+          10_000_000
+          (mkPay (Account $ Role "Role") ada (Constant 2_000_000) $
+            When
+              [ Case (Notify TrueObs) $
+                  When
+                    [ Case (Notify TrueObs) Close
+                    ]
+                    (utcTimeToPOSIXTime timeout2)
+                    Close
+              ]
+              (utcTimeToPOSIXTime timeout1)
+              Close
+          )
+      submitCreate wallet1 payDepth2AccountCreated
+      payDepth2PartyCreated <-
+        expectRight "Failed to create pay to party depth 2 contract" =<< createContract
+          Nothing
+          MarloweV1
+          (addresses wallet1)
+          (mkRoleTokens [("Role", wallet2)])
+          emptyMarloweTransactionMetadata
+          10_000_000
+          (mkPay (Party $ Role "Role") ada (Constant 2_000_000) $
+            When
+              [ Case (Notify TrueObs) $
+                  When
+                    [ Case (Notify TrueObs) Close
+                    ]
+                    (utcTimeToPOSIXTime timeout2)
+                    Close
+              ]
+              (utcTimeToPOSIXTime timeout1)
+              Close
+          )
+      submitCreate wallet1 payDepth2PartyCreated
+      runtime <- ask
+      liftIO =<< runConcurrently do
+        payRoleAccountApplied <- Concurrently $ expectRight "Failed to apply inputs" =<< applyInputs
+          MarloweV1
+          (addresses wallet1)
+          (let ContractCreated{..} = payRoleAccountCreated in contractId)
+          emptyMarloweTransactionMetadata
+          []
+        payAddressAccountApplied <- Concurrently $ expectRight "Failed to apply inputs" =<< applyInputs
+          MarloweV1
+          (addresses wallet1)
+          (let ContractCreated{..} = payAddressAccountCreated in contractId)
+          emptyMarloweTransactionMetadata
+          []
+        payRolePartyApplied <- Concurrently $ expectRight "Failed to apply inputs" =<< applyInputs
+          MarloweV1
+          (addresses wallet1)
+          (let ContractCreated{..} = payRolePartyCreated in contractId)
+          emptyMarloweTransactionMetadata
+          []
+        payAddressPartyApplied <- Concurrently $ expectRight "Failed to apply inputs" =<< applyInputs
+          MarloweV1
+          (addresses wallet1)
+          (let ContractCreated{..} = payAddressPartyCreated in contractId)
+          emptyMarloweTransactionMetadata
+          []
+        payDepth1Applied <- Concurrently $ expectRight "Failed to apply inputs" =<< applyInputs
+          MarloweV1
+          (addresses wallet1)
+          (let ContractCreated{..} = payDepth1Created in contractId)
+          emptyMarloweTransactionMetadata
+          [NormalInput INotify]
+        payDepth2AccountApplied <- Concurrently $ expectRight "Failed to apply inputs" =<< applyInputs
+          MarloweV1
+          (addresses wallet1)
+          (let ContractCreated{..} = payDepth2AccountCreated in contractId)
+          emptyMarloweTransactionMetadata
+          [NormalInput INotify]
+        payDepth2PartyApplied <- Concurrently $ expectRight "Failed to apply inputs" =<< applyInputs
+          MarloweV1
+          (addresses wallet1)
+          (let ContractCreated{..} = payDepth2PartyCreated in contractId)
+          emptyMarloweTransactionMetadata
+          [NormalInput INotify]
+        pure $ runTests (runtime, PayTestData{..})
+
+utcTimeToPOSIXTime :: UTCTime -> POSIXTime
+utcTimeToPOSIXTime = POSIXTime . floor . (* 1000) . utcTimeToPOSIXSeconds
+
+mkRoleTokens :: [(TokenName, Wallet)] -> RoleTokensConfig
+mkRoleTokens = RoleTokensMint . mkMint . (fmap . fmap) ((,Nothing) . changeAddress . addresses) . NE.fromList
+
+submitCreate :: Wallet -> ContractCreated BabbageEra 'V1 -> Integration ()
+submitCreate wallet ContractCreated{..} = void $ submit wallet txBody
 
 runAsIntegration :: (a -> Integration ()) -> ActionWith (MarloweRuntime, a)
 runAsIntegration action (runtime, a) = runIntegrationTest (action a) runtime
