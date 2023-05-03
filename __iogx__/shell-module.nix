86f4d0020d09c03a6eb7b3e217ece207f1169fd7
zeme <zem1@pm.me>
2023-05-03 12:32:25 +0700

    WIP

--- __iogx__/shell-module.nix
+++ __iogx__/shell-module.nix
@@ -1,5 +1,6 @@
 # Your development shell is defined here.
-# You can add packages, custom scripts and a shell hook.
+# You can add packages, scripts, envvars, and a shell hook.
+
 {
   # Desystemized merged inputs.
   # All the inputs from iogx (e.g. CHaP, haskell-nix, etc..) unioned with the 
@@ -14,13 +15,22 @@
   # iogx and your `flake.nix`.
 , systemized-inputs
 
-  # The very config passed as second argument to `inputs.iogx.mkFlake` in your 
-  # `flake.nix`.
+  # The very attrset passed to `inputs.iogx.mkFlake` in your `flake.nix`.
 , flakeopts
 
   # Desystemized legacy nix packages configured against `haskell.nix`.
   # NEVER use the `nixpkgs` coming from `inputs` or `systemized-inputs`!
 , pkgs
+
+  # A reference to the `haskell.nix` project on top of which this shell will be 
+  # built. This can be used for example to bring some haskell executables into 
+  # the shell:
+  # packages = [
+  #   haskell-nix-project.hsPkgs.cardano-cli.components.exes.cardano-cli
+  #   haskell-nix-project.hsPkgs.cardano-node.components.exes.cardano-node
+  # ];
+  # Be careful not to refence the project's own haskell packages.
+, haskell-nix-project
 }:
 
 let
