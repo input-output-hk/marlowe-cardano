1fd10f1e294390a21d89c460882cc851dfb0bdfa
zeme <lorenzo.calegari@iohk.io>
2023-06-19 08:52:11 +0200

    Stop pre-commit check

--- nix/hydra-jobs.nix
+++ nix/hydra-jobs.nix
@@ -2,16 +2,23 @@
 
 {
   excludedPaths = [
+    "packages.ghc8107.pre-commit-check"
+    "packages.ghc8107-mingwW64.pre-commit-check"
+
     "packages.ghc8107-profiled"
     "packages.ghc8107-mingwW64-profiled"
+
     "devShells.ghc8107-profiled"
     "devShells.ghc8107-mingwW64-profiled"
+
     "checks.ghc8107-profiled"
     "checks.ghc8107-mingwW64-profiled"
+
     "networks"
     "nomadTasks"
     "operables"
   ];
+
 
   extraJobs = { };
 }
