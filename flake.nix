a3192473bde3c41c14d04629c1482548a94f3874
zeme-wana <15709674+zeme-wana@users.noreply.github.com>
2023-06-28 13:10:21 +0200

    Bump IOGX (#632)
    - Updates to the nix interface files as per the lastest IOGX version.
    - x-builds on windows still a WIP

--- flake.nix
+++ flake.nix
@@ -19,6 +19,6 @@
     extra-trusted-public-keys = [
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
     ];
-    allow-import-from-derivation = "true";
+    allow-import-from-derivation = true;
   };
 }
--- nix/iogx-config.nix
+++ nix/iogx-config.nix
@@ -1,7 +1,6 @@
-{ ... }:
-
 {
   repoRoot = ../.;
   systems = [ "x86_64-darwin" "x86_64-linux" ];
   haskellCompilers = [ "ghc8107" ];
+  shouldCrossCompile = false;
 }
