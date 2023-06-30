17ab6f3f0ec7665a3e0616a14c4c9321b8175587
zeme-wana <15709674+zeme-wana@users.noreply.github.com>
2023-06-30 10:34:31 +0200

    Bump IOGX to bc5f588 (#642)
    Bump IOGX to
    [bc5f588a5c706555a73431d6871bc3858d651cf6](https://www.github.com/input-output-hk/iogx/commit/bc5f588a5c706555a73431d6871bc3858d651cf6)

--- nix/iogx-config.nix
+++ nix/iogx-config.nix
@@ -1,5 +1,7 @@
+# This file is part of the IOGX template and is documented at the link below:
+# https://www.github.com/input-output-hk/iogx#32-nixiogx-confignix
+
 {
-  repoRoot = ../.;
   systems = [ "x86_64-darwin" "x86_64-linux" ];
   haskellCompilers = [ "ghc8107" ];
   shouldCrossCompile = false;
