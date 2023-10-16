ae33d704b72e6c337919f64315691efba194722d
Jamie Bertram <jamie.bertram@tweag.io>
2023-10-16 07:50:16 -0400

    Revert "Remove unused FrameHeader struct"
    This reverts commit e836328e23d9930ac7fcec2eca8d12277e726093.

--- marlowe-protocols/src/Network/Channel.hs
+++ marlowe-protocols/src/Network/Channel.hs
@@ -10,6 +10,7 @@ import Data.Binary.Get (getInt64be, runGet)
 import Data.Binary.Put (putInt64be, runPut)
 import qualified Data.ByteString.Lazy as LBS
 import Data.Functor (($>))
+import Data.Int (Int64)
 import GHC.Generics (Generic)
 import GHC.IO (mkUserError)
 import Network.Socket (Socket)
@@ -48,6 +49,12 @@ data FrameStatus
   = OkStatus
   | ErrorStatus
   deriving stock (Show, Read, Eq, Ord, Bounded, Enum, Generic)
+
+data FrameHeader = FrameHeader
+  { frameHeaderStatus :: FrameStatus
+  , frameHeaderLength :: Int64
+  }
+  deriving stock (Show, Read, Eq, Ord, Generic)
 
 data Frame = Frame
   { frameStatus :: FrameStatus
