b6c2ca1626fb0a39bd191b5d211f9bf5dccae742
Jamie Bertram <jamie.bertram@tweag.io>
2023-10-16 15:28:47 -0400

    Add doc comments for untyped driver

--- marlowe-protocols/src/Network/Protocol/Driver/Untyped.hs
+++ marlowe-protocols/src/Network/Protocol/Driver/Untyped.hs
@@ -13,16 +13,29 @@ import Data.Text (Text)
 import GHC.Generics (Generic)
 import Network.Channel
 
+-- | An untyped protocol driver. Sits between a @Network.TypedProtocol.Driver@ which
+-- is a stateful, typed channel for a specific protocol, and a @Network.Channel@ which is an unstructured,
+-- stateless channel for raw bytes.
+--
+-- An untyped driver is able to send or receive arbitrary data, but it does so in a structured manner. Each payload will
+-- be preceded by a status byte which indicates if the message is a normal, expected payload, or if it is an exception.
 data Driver m = Driver
   { sendSuccessMessage :: Put -> m ()
+  -- ^ Send a normal message encoded as a @Data.Binary.Put@
   , sendFailureMessage :: Text -> m ()
+  -- ^ Send an exception message.
   , recvMessageUntyped :: forall a. Maybe BS.ByteString -> Get a -> m (Either RecvError (a, Maybe BS.ByteString))
+  -- ^ Receive a message and attempt to decode it using a @Data.Binary.Get@.
   }
 
+-- | What can go wrong during a recv call.
 data RecvError
-  = PeerDisconnected
-  | PeerCrashed Text
-  | DeserializeError BS.ByteString ByteOffset String
+  = -- | The peer disconnected unexpectedly.
+    PeerDisconnected
+  | -- | The peer crashed and sent an exception message.
+    PeerCrashed Text
+  | -- | The peer sent unexpected binary data.
+    DeserializeError BS.ByteString ByteOffset String
   deriving stock (Show, Read, Eq, Ord)
   deriving anyclass (Exception)
 
@@ -32,6 +45,7 @@ data StatusToken
   deriving stock (Show, Read, Eq, Ord, Enum, Bounded, Generic)
   deriving anyclass (Binary)
 
+-- | Create a driver which will operate over a channel.
 mkDriver :: forall m. (Monad m) => Channel m LBS.ByteString -> Driver m
 mkDriver Channel{..} =
   Driver
