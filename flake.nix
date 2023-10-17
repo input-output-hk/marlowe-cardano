e58e562d904fe53f824835d5c861fef64561571e
zeme <lorenzo.calegari@iohk.io>
2023-10-17 17:17:51 +0200

    WIP

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
     cardano-world.url = "github:input-output-hk/cardano-world/d22f50fc77d23e2612ca2b313a098dd0b48834d4";
 
     iogx = {
-      url = "github:input-output-hk/iogx?ref=v4";
+      url = "github:input-output-hk/iogx";
       inputs.hackage.follows = "hackage";
       inputs.CHaP.follows = "CHaP";
     };
