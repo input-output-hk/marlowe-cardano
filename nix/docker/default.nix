264f8d77236342f5aecac319901a1349881809b3
Rosario Pulella <RosarioPulella@gmail.com>
2023-10-18 18:16:31 -0400

    mkOciImages: Take list of attributes describing images

--- nix/docker/default.nix
+++ nix/docker/default.nix
@@ -4,12 +4,27 @@ let
 
   mkOciImages =
     { defaultImageAttrs ? { }
-    , imageSet
+    , images
     }:
+    assert lib.assertMsg (lib.isList images) "images must be a list of images.";
     let
       builtImageSet = lib.mapAttrs
         (_: imageConfig: mkImage (lib.recursiveUpdate imageConfig defaultImageAttrs))
-        imageSet;
+        (buildSetWith
+          (imageConfig:
+            # TODO: If the user sees this error they will have no clue where
+            # it is comming from. We need to aid the user with attributing
+            # this error to the code that causes it.
+            # Two ideas
+            # 1. Provide more information about the attribute set that is
+            #    missing the "name" attribute.
+            # 2. Provide a location where the attribute is defined
+            #    (Probably more difficult).
+            assert lib.assertMsg (imageConfig ? name) "Attribute \"name\" is missing.";
+            imageConfig.name
+          )
+          images
+        );
 
       allFunctions = [
         "copyToDockerDaemon"
@@ -52,6 +67,30 @@ let
         readinessProbe = std.lib.ops.writeScript readinessProbe;
       };
     };
+
+  /*
+    Builds an attribute set from a list using a selector function to generate
+    attribute keys.
+
+    Asserts that no attribute keys conflict.
+  */
+  buildSetWith = selector: lib.foldl'
+    (currentSet: val:
+      let
+        key = selector val;
+      in
+      # TODO: If the user sees this error they will have no clue where
+        #     it is comming from. We need to aid the user with attributing
+        #     this error to the code that causes it.
+        #     Two ideas
+        #     1. Provide more information about the attribute sets evaulated to
+        #        the same key.
+        #     2. Provide locations where of the attribute sets that evaulated to
+        #        the same key. (Probably more difficult).
+      assert lib.assertMsg (!(currentSet ? ${key})) "${key} is repeated.";
+      currentSet // { ${key} = val; }
+    )
+    { };
 
   mkFuctionCallForImages = builtImages: functionName: pkgs.writeShellScriptBin
     "${functionName}-set"
