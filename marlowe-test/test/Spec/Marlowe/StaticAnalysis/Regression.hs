6a15872a9e8ad4791ac98654328e53029755b7f8
Ramsay Taylor <ramsay@gmail.com>
2023-11-23 09:24:23 +0000

    Update marlowe-test/test/Spec/Marlowe/StaticAnalysis/Regression.hs
    Co-authored-by: Pablo Lamela  <palas87@gmail.com>

--- marlowe-test/test/Spec/Marlowe/StaticAnalysis/Regression.hs
+++ marlowe-test/test/Spec/Marlowe/StaticAnalysis/Regression.hs
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------------
 --
--- Module      :  $Headers
+-- Module      :  Spec.Marlowe.StaticAnalysis.Regression
 -- License     :  Apache 2.0
 --
 -- Stability   :  Experimental
