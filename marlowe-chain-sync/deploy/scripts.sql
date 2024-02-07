94cb0a419bc464776d01f90b96643a12962a982b
Jamie Bertram <jamie.bertram@tweag.io>
2024-02-07 12:32:08 -0500

    Add script table

---
+++ marlowe-chain-sync/deploy/scripts.sql
@@ -0,0 +1,23 @@
+-- Deploy chain:scripts to pg
+-- requires: appschema
+
+BEGIN;
+
+-- NOTE this migration requires a reset of the entire database, as the chain
+-- needs to be re-synchronized.
+TRUNCATE chain.block CASCADE;
+TRUNCATE chain.tx CASCADE;
+TRUNCATE chain.txIn CASCADE;
+TRUNCATE chain.txOut CASCADE;
+TRUNCATE chain.assetOut;
+TRUNCATE chain.assetMint;
+
+CREATE TYPE chain.SCRIPTLANG as ENUM ('SimpleScript', 'PlutusV1', 'PlutusV2', 'PlutusV3');
+
+CREATE TABLE chain.script
+  ( id BYTEA PRIMARY KEY
+  , bytes BYTEA NOT NULL
+  , language chain.SCRIPTLANG NOT NULL
+  );
+
+COMMIT;
